9bda02f2c23305d4b3842da0a5a44a94b9463653
kai <kai@kaideMacBook-Pro.local>
2019-07-30 15:15:37 +0800

    格式化代码

--- packages/console.freelog.com/src/views/main/index.js
+++ packages/console.freelog.com/src/views/main/index.js
@@ -1,95 +1,95 @@
 import LazyListView from '@/components/LazyListView/index.vue'
-import { loadAuthSchemes } from '@/data/scheme/loader'
+import {loadAuthSchemes} from '@/data/scheme/loader'
 import ListItem from './release.vue'
 
 export default {
-  name: 'index-main-view',
-  data() {
-    return {
-      resourceList: [],
-      query: ''
-    }
-  },
-  components: {
-    ListItem,
-    LazyListView
-  },
-
-  watch: {
-    '$route.query': function () {
-      this.query = this.$route.query.q || ''
-      this.queryHandler()
-    }
-  },
-
-  mounted() {
-    const qs = this.$route.query
-    if (qs.q) {
-      this.query = qs.q
-    }
-  },
-  methods: {
-    autoQueryHandler() {
-      if (this.timer) {
-        clearTimeout(this.timer)
-      }
-      this.timer = setTimeout(() => {
-        this.queryHandler()
-      }, 8e2)
-    },
-    queryHandler() {
-      if (this.timer) {
-        clearTimeout(this.timer)
-      }
-
-      this.$refs.resourceList.$emit('reload', {
-        keyWords: encodeURIComponent(this.query)
-      })
-    },
-    fetchReleaseData(page) {
-      const query = {
-        page
-      }
-      if (this.query) {
-        query.keyWords = this.query
-      }
-      return this.loader(query).then((data) => {
-        data.dataList = data.dataList.filter(r => r.policies.length > 0)
-        const releases = data.dataList
-        data.canLoadMore = !(releases.length < data.pageSize)
-
-        if (releases && releases.length) {
-          const releasesMap = {}
-          const rids = releases.map((r) => {
-            releasesMap[r.resourceId] = r
-            return r.resourceId
-          })
-
-          // return loadAuthSchemes({ resourceIds: rids, authSchemeStatus: 1 }).then((schemes) => {
-          //   schemes.forEach((scheme) => {
-          //     const rid = scheme.resourceId
-          //     releasesMap[rid].schemes = releasesMap[rid].schemes || []
-          //     releasesMap[rid].schemes.push(scheme)
-          //   })
-          //   data.dataList = Object.values(releasesMap)
-          //   return data
-          // })
+    name: 'index-main-view',
+    data() {
+        return {
+            resourceList: [],
+            query: ''
         }
-
-        return data
-      })
     },
-    loader(param) {
-      if (typeof param === 'object') {
-        if (param.keyWords) {
-          this.query = param.keyWords
+    components: {
+        ListItem,
+        LazyListView
+    },
+
+    watch: {
+        '$route.query': function () {
+            this.query = this.$route.query.q || ''
+            this.queryHandler()
         }
-        param = {
-          params: param
+    },
+
+    mounted() {
+        const qs = this.$route.query
+        if (qs.q) {
+            this.query = qs.q
         }
-      }
-      return this.$services.ReleaseService.get(param || {}).then(res => res.getData())
-      // return this.$services.allResources.get(param || {}).then(res => res.getData())
+    },
+    methods: {
+        autoQueryHandler() {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+            this.timer = setTimeout(() => {
+                this.queryHandler()
+            }, 8e2)
+        },
+        queryHandler() {
+            if (this.timer) {
+                clearTimeout(this.timer)
+            }
+
+            this.$refs.resourceList.$emit('reload', {
+                keyWords: encodeURIComponent(this.query)
+            })
+        },
+        fetchReleaseData(page) {
+            const query = {
+                page
+            }
+            if (this.query) {
+                query.keyWords = this.query
+            }
+            return this.loader(query).then((data) => {
+                data.dataList = data.dataList.filter(r => r.policies.length > 0)
+                const releases = data.dataList
+                data.canLoadMore = !(releases.length < data.pageSize)
+
+                if (releases && releases.length) {
+                    const releasesMap = {}
+                    const rids = releases.map((r) => {
+                        releasesMap[r.resourceId] = r
+                        return r.resourceId
+                    })
+
+                    // return loadAuthSchemes({ resourceIds: rids, authSchemeStatus: 1 }).then((schemes) => {
+                    //   schemes.forEach((scheme) => {
+                    //     const rid = scheme.resourceId
+                    //     releasesMap[rid].schemes = releasesMap[rid].schemes || []
+                    //     releasesMap[rid].schemes.push(scheme)
+                    //   })
+                    //   data.dataList = Object.values(releasesMap)
+                    //   return data
+                    // })
+                }
+
+                return data
+            })
+        },
+        loader(param) {
+            if (typeof param === 'object') {
+                if (param.keyWords) {
+                    this.query = param.keyWords
+                }
+                param = {
+                    params: param
+                }
+            }
+            return this.$services.ReleaseService.get(param || {}).then(res => res.getData())
+            // return this.$services.allResources.get(param || {}).then(res => res.getData())
+        }
     }
-  }
 }
