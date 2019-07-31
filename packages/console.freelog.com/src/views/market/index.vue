91327ddf563e06bf10398009c050e08d13b04c97
kai <kai@kaideMacBook-Pro.local>
2019-07-31 10:52:17 +0800

    提交代码

--- packages/console.freelog.com/src/views/market/index.vue
+++ packages/console.freelog.com/src/views/market/index.vue
@@ -1,17 +1,20 @@
 <template>
     <div style="display: flex; justify-content: center;">
-        <div style="width: 1200px; display: flex; justify-content: center;">
-            <div style="display: flex; flex-wrap: wrap; justify-content: center;">
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-            </div>
+        <div style="padding: 0 50px; display: flex; justify-content: center; flex-wrap: wrap; padding-top: 20px;">
+            <Card
+                v-for="item in sourceData"
+                :imgSrc="item.previewImages[0]"
+                :title="item.releaseName"
+                :user="item.username"
+                :type="item.resourceType"
+            />
+            <div
+                v-if="!this.dataEnd"
+                ref="loadingRef"
+                v-loading="true"
+                style="width: 220px; height: 234px; background: #fff; margin: 0 10px 20px;"
+            ></div>
+            <div v-for="item in Array(20)" style="width: 220px; margin: 0 10px;"></div>
         </div>
     </div>
 </template>
@@ -23,7 +26,53 @@
         name: "index",
         components: {
             Card,
-        }
+        },
+        data() {
+            return {
+                page: 1,
+                sourceData: [],
+                dataEnd: false,
+                isLoading: false,
+            };
+        },
+        mounted() {
+            //.$el.getBoundingClientRect()
+            // console.log(this.$refs.loadingRef.getBoundingClientRect(), '1234');
+            window.onscroll = this.loadingControl;
+
+            this.loadingControl();
+        },
+        methods: {
+            async handleData() {
+                const res = await this.$axios.get('/v1/releases', {
+                    params: {
+                        page: this.page,
+                        pageSize: 1,
+                    }
+                });
+                const data = res.data.data;
+                console.log(res, 'resresres');
+                this.sourceData = [
+                    ...this.sourceData,
+                    ...data.dataList,
+                ];
+                this.dataEnd = data.page * data.pageSize >= data.totalItem;
+            },
+            async loadingControl() {
+                if (this.isLoading) {
+                    return;
+                }
+                let top = this.$refs.loadingRef.getBoundingClientRect().top;
+                while (top < window.innerHeight && !this.dataEnd) {
+                    this.isLoading = true;
+                    this.page++;
+                    await this.handleData();
+                    top = this.$refs.loadingRef.getBoundingClientRect().top;
+                }
+                this.isLoading = false;
+            }
+
+        },
     }
 </script>
 
