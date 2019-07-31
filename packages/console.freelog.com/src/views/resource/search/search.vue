eec01d1017ce7113ac475ff0724b06427c6e722a
wwzh <790727372@qq.com>
2019-07-31 17:29:07 +0800

    release 搜索 bugfix

--- packages/console.freelog.com/src/views/resource/search/search.vue
+++ packages/console.freelog.com/src/views/resource/search/search.vue
@@ -19,6 +19,7 @@
           <span class="r-e-w-s-r-name">{{scope.data.aliasName}}</span>
           <span class="r-e-w-s-r-type">{{scope.data.resourceType}}</span>
           <span class="r-e-w-s-r-date">{{scope.data.createDate | fmtDate}}</span>
+          <span class="r-e-w-s-r-relase" v-if="resourceMapReleases[scope.data.resourceId]">已有发行</span>
           <span class="r-e-w-s-r-select-btn" @click="selectResource(scope.data)">选择</span>
         </div>
       </template>
@@ -38,7 +39,8 @@ export default {
     return {
       searchInput: '',
       searchResources: [],
-      isFirstSearch: true
+      isFirstSearch: true,
+      resourceMapReleases: {}
     }
   },
   computed: {
@@ -53,6 +55,7 @@ export default {
     searchHandler() {
       this.searchResources = []
       this.$refs.searchView.refresh()
+      console.log(this.resourceMapReleases)
     },
     // 资源搜索
     searchDataHandler(page) {
@@ -81,25 +84,30 @@ export default {
         } else {
           data.canLoadMore = false
         }
+        const resourceIds = data.dataList.map(r => r.resourceId)
+        this.batchFetchReleaseList(resourceIds)
         return data
       })
     },
     selectResource(resource) {
       this.$emit('select-resource', resource)
     },
-    // 获取单个资源"所属发行列表"
-    fetchReleaseList(resourceId) {
-      if(!this.resourceMapReleases[resourceId]) {
-        return this.$services.ResourceService.get(`${resourceId}/releases`)
+    // 批量获取资源"所属发行列表"
+    batchFetchReleaseList(resourceIds) {
+      resourceIds = resourceIds.filter(r => !this.resourceMapReleases[r.resourceId]).join(',')
+      return this.$services.ResourceService.get(`releases?resourceIds=${resourceIds}`)
           .then(res => res.data)
           .then(res => {
             if(res.errcode === 0) {
-              this.resourceMapReleases[resourceId] = res.data
+              const leng = res.data.length
+              const map = {}
+              for(let i = 0; i < leng; i++) {
+                const { resourceId } = res.data[i]
+                map[resourceId] = res.data[i].releases.length > 0
+              }
+              this.resourceMapReleases = Object.assign({}, this.resourceMapReleases, map)
             }
           })
-      }else {
-        return Promise.resolve(this.resourceMapReleases[resourceId])
-      }
     },
   },
 }
@@ -113,10 +121,16 @@ export default {
   .r-e-w-s-r-item { margin: 15px 0; }
   span { display: inline-block; text-overflow: ellipsis; overflow: hidden; white-space: nowrap; }
   .r-e-w-s-r-name { width: 220px; color: #333; }
-  .r-e-w-s-r-type, .r-e-w-s-r-date { width: 90px; font-size: 12px; color: #999; }
+  .r-e-w-s-r-type, .r-e-w-s-r-date { font-size: 12px; color: #999; }
+  .r-e-w-s-r-type{ width: 80px; }
+  .r-e-w-s-r-date{ width: 72px; }
+  .r-e-w-s-r-relase{ 
+    padding: 0 5px; border-radius: 3px;
+    font-size: 12px; background-color: #67C23A; color: #fff;
+  }
   .r-e-w-s-r-select-btn {
     float: right; cursor: pointer;
-    padding: 2px 10px; border-radius: 13px;
+    margin: 2px 0; padding: 2px 10px; border-radius: 13px;
     font-size: 12px; background-color: #409EFF; color: #fff;
   }
 </style>
