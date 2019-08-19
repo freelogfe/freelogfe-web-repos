0ca8181ac7a69c2ee3d3dc9fa8b064b181d7a69a
wwzh <790727372@qq.com>
2019-08-19 19:04:28 +0800

    创建资源 类型参数更改

--- packages/console.freelog.com/src/views/resource/editor/index.js
+++ packages/console.freelog.com/src/views/resource/editor/index.js
@@ -29,7 +29,7 @@ export default {
         return {
             isUpdateResource: !!this.$route.params.resourceId,
             // 资源类型选项
-            resourceTypes: ['json', 'widget', 'image', 'audio', 'markdown', 'page_build', 'revealSlide', 'license', 'video', 'catalog'],
+            resourceTypes: ['json', 'widget', 'image', 'audio', 'markdown', 'page_build', 'reveal_slide', 'license', 'video', 'catalog'],
             // 资源类型值
             resourceType: '',
             // 资源类型，是否提醒
