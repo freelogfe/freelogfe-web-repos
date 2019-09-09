10b2036dbe1725df878ffe0be80ab710342b8d7c
kai <kai@kaideMacBook-Pro.local>
2019-09-09 18:33:51 +0800

    添加 table

--- packages/console.freelog.com/src/views/node/test-management/index.vue
+++ packages/console.freelog.com/src/views/node/test-management/index.vue
@@ -100,26 +100,70 @@
             </div>
 
             <div style="height: 70px;"></div>
+
+            <el-table
+                :data="tableData"
+                style="width: 100%"
+            >
+                <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="address"
+                    label="地址">
+                </el-table-column>
+            </el-table>
         </div>
     </div>
 </template>
 
 <script>
-    import clipboard from '@/components/clipboard/index.vue';
-
-    export default {
-        name: 'test-management',
-        components: {
-            clipboard,
-        },
-        data() {
-            return {
-                styleObject: {
-                    minHeight: (window.innerHeight - 60) + 'px',
-                },
-            };
-        },
-    }
+    import Index from './index';
+    export default Index;
+    // import clipboard from '@/components/clipboard/index.vue';
+    //
+    // export default {
+    //     name: 'test-management',
+    //     components: {
+    //         clipboard,
+    //     },
+    //     data() {
+    //         return {
+    //             styleObject: {
+    //                 minHeight: (window.innerHeight - 60) + 'px',
+    //             },
+    //             tableData: [
+    //                 {
+    //                     date: '2016-05-02',
+    //                     name: '王小虎',
+    //                     address: '上海市普陀区金沙江路 1518 弄'
+    //                 },
+    //                 {
+    //                     date: '2016-05-04',
+    //                     name: '王小虎',
+    //                     address: '上海市普陀区金沙江路 1517 弄'
+    //                 },
+    //                 {
+    //                     date: '2016-05-01',
+    //                     name: '王小虎',
+    //                     address: '上海市普陀区金沙江路 1519 弄'
+    //                 },
+    //                 {
+    //                     date: '2016-05-03',
+    //                     name: '王小虎',
+    //                     address: '上海市普陀区金沙江路 1516 弄'
+    //                 }
+    //             ],
+    //         };
+    //     },
+    // }
 </script>
 
 <style scoped>
