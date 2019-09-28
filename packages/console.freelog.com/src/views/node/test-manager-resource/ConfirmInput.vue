5d7730d529b058088fd73cba2aa47353c9b031ed
kai <kai@MacBook-Pro.local>
2019-09-28 16:43:33 +0800

    编辑页UI和基本逻辑完成

---
+++ packages/console.freelog.com/src/views/node/test-manager-resource/ConfirmInput.vue
@@ -0,0 +1,60 @@
+<template>
+    <div style="height: 40px; width: 700px;">
+        <a
+            v-show="!isEdit"
+            @click="isEdit = true"
+            style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: space-between; background-color: #fafbfb; cursor: pointer; border-radius: 2px; padding: 0 15px; box-sizing: border-box; border: 1px solid #fafbfb;"
+        >
+            <span style="font-weight: 600; font-size: 14px; color: #333;">{{value}}</span>
+            <i class="el-icon-edit" style="color: #888; font-size: 16px;"></i>
+        </a>
+        <div
+            v-show="isEdit"
+            style="width: 100%; height: 100%; display: flex; border: 1px solid #c7c7c7; border-radius: 2px;"
+        >
+            <input
+                v-model="inputValue"
+                style="padding: 0 15px; color: #333; line-height: 20px; font-size: 14px; display: block; width: 100%; border: none; outline: none; flex-shrink: 1; font-weight: 600;"
+            />
+            <div style="display: flex; align-items: center; padding: 0 10px;">
+                <a
+                    @click="isEdit = false"
+                    style="font-size: 12px; color: #999; line-height: 24px; width: 50px; text-align: center; cursor: pointer;"
+                >取消</a>
+                <a
+                    @click="confirmChange"
+                    style="display: inline-block; line-height: 24px; background-color: #409eff; color: #fff; width: 50px; text-align: center; border-radius: 12px; font-weight: 600; cursor: pointer;"
+                >保存</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ConfirmInput',
+        props: {
+            value: {
+                type: String,
+                default: '',
+            },
+        },
+        data() {
+            return {
+                isEdit: false,
+                inputValue: this.value,
+            };
+        },
+        methods: {
+            confirmChange() {
+                this.isEdit = false;
+                // console.log(this.inputValue, 'inputValue');
+                this.$emit('confirmChange', this.inputValue);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ packages/console.freelog.com/src/views/node/test-manager-resource/OverviewHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div
+        style="padding: 20px; display: flex; box-sizing: border-box; box-shadow:0 3px 8px 0 rgba(0,0,0,0.2); border-radius:10px;">
+        <img
+            class="resource-default-preview"
+            style="width: 88px; height: 66px; flex-shrink: 0"
+        />
+
+        <div
+            style="display: flex; flex-shrink: 1; width: 100%; flex-direction: column; justify-content: space-between; box-sizing: border-box; padding-left: 20px;">
+            <div style="display: flex; justify-content: space-between; align-items: center;">
+                <div style="align-items: center; display: flex;">
+                    <span style="font-size: 18px; color: #222; line-height: 1;">数据库应用</span>
+                    <label
+                        style="background-color: #72bb1f; font-size: 12px; font-weight: 600; color: #fff; text-align: center; line-height: 18px; padding: 0 8px; margin-left: 5px; border-radius: 2px; display: inline-block;">发行</label>
+                </div>
+                <div style="font-size: 12px; color: #999;">
+                    <span>类型 markdown</span>
+                    <span> | 签约时间 2018-10-10 12:00</span>
+                    <span> | 最新版本 v1.0.1</span>
+                </div>
+            </div>
+            <div style="font-size: 12px; color: #333;">这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…这是一段发行介绍…
+                这是一段发行介绍……这是一段发行介绍……这是一段发行介绍……</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OverviewHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
