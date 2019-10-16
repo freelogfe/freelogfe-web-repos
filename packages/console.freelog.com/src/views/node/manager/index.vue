4f232df670528f22099d238acb19ffe98e840856
kai <kai@MacBook-Pro.local>
2019-10-16 17:51:21 +0800

    提取 节点管理 css

--- packages/console.freelog.com/src/views/node/manager/index.vue
+++ packages/console.freelog.com/src/views/node/manager/index.vue
@@ -1,36 +1,41 @@
 <template>
-    <div class="node-manager" style="display: flex;">
+    <div class="node-manager">
 
-        <div :style="styleObject" style="width: 280px; flex-shrink: 0; background-color: #fafbfb;">
+        <div
+            :style="styleObject"
+            class="node-manager__aside"
+        >
             <div style="height: 30px;"></div>
-            <div style="color: #333; font-size: 20px; font-weight: 600; padding: 0 20px;">{{nodeInfo.name}}</div>
+            <div class="node-manager__aside__name">{{nodeInfo.name}}</div>
             <div style="height: 10px;"></div>
-            <div style="padding: 0 20px;">
+            <div class="node-manager__aside__url">
                 <a
-                    style="font-size: 14px; color: #333; text-decoration: underline; margin-right: 5px;"
+                    class="node-manager__aside__url__link"
                     :href="'//' + nodeInfo.origin"
                     target="_blank"
                 >{{nodeInfo.origin}}</a>
                 <clipboard
+                    class="node-manager__aside__url__clipboard"
                     :value="nodeInfo.origin"
-                    style="display: inline-block;"
                     @copyDone="$message.success($t('copySuccess'))"
                 >
-                    <el-button style="padding: 3px 6px;" type="primary" plain size="mini">copy</el-button>
+                    <el-button
+                        class="node-manager__aside__url__clipboard__button"
+                        type="primary"
+                        plain
+                        size="mini"
+                    >copy
+                    </el-button>
                 </clipboard>
             </div>
 
             <div style="height: 79px;"></div>
 
-            <div style="border-top: 1px solid #ebebeb; cursor: pointer;">
+            <div class="node-manager__aside__navs">
                 <a
                     @click="switchIsPageStyle(false)"
-                    :style="{
-                        backgroundColor: !isPageStyle ? '#fff':'transparent',
-                        borderRight: !isPageStyle ? '3px solid #409eff': 'none',
-                        color: !isPageStyle ? '#409eff': '#333',
-                    }"
-                    style="line-height: 54px; display: block; padding-left: 60px; font-size: 14px; font-weight: 600; border-bottom: 1px solid #ebebeb;">{{$t('nodeReleaseList')}}</a>
+                    :class="{'node-manager__aside__navs__a--active': !isPageStyle}"
+                >{{$t('nodeReleaseList')}}</a>
                 <a
                     @click="switchIsPageStyle(true)"
                     :style="{
@@ -38,32 +43,32 @@
                         borderRight: isPageStyle ? '3px solid #409eff': 'none',
                         color: isPageStyle ? '#409eff': '#333',
                     }"
-                    style="line-height: 54px; display: block; padding-left: 60px; font-size: 14px; font-weight: 600; border-bottom: 1px solid #ebebeb;">{{$t('nodePageStyle')}}</a>
+                    :class="{'node-manager__aside__navs__a--active': isPageStyle}"
+                >{{$t('nodePageStyle')}}</a>
             </div>
         </div>
 
 
-        <div style="width: 100%; flex-shrink: 1; background-color: #fff; padding: 0 60px;">
+        <div class="node-manager__main">
             <div style="height: 40px;"></div>
-            <div style="display: flex; align-items: center; justify-content: space-between;">
+            <div class="node-manager__main__header">
                 <el-radio-group
                     v-model="filterTodo">
                     <el-radio-button label="全部">
-                        <span style="display: inline-block; width: 84px; text-align: center;">{{$t('all')}}</span>
+                        <span class="node-manager__main__header__text">{{$t('all')}}</span>
                     </el-radio-button>
                     <el-radio-button label="待处理">
-                        <span style="display: inline-block; width: 84px; text-align: center;">{{$t('pending')}}</span>
+                        <span class="node-manager__main__header__text">{{$t('pending')}}</span>
                     </el-radio-button>
                 </el-radio-group>
 
                 <el-input
-                    style="width: 400px;"
+                    class="node-manager__main__header__input"
                     v-model="filterSearch"
                     debounce="1000"
                 >
                     <i slot="prefix" class="el-input__icon el-icon-search"></i>
                     <i
-                        style="cursor: pointer"
                         @click="filterSearch = ''"
                         v-show="filterSearch && filterSearch.length > 0"
                         slot="suffix"
@@ -76,7 +81,7 @@
 
             <el-table
                 :data="tableData"
-                style="width: 100%"
+                class="node-manager__main__table"
             >
                 <el-table-column
                     prop="publish"
@@ -84,10 +89,9 @@
                     min-width="20%"
                 >
                     <template slot-scope="scope">
-                        <div style="display: flex; align-items: center; padding-left: 10px;">
+                        <div class="node-manager__main__table__release">
                             <div
-                                style="width: 40px; height: 30px; flex-shrink: 0;"
-                                class="resource-default-preview"
+                                class="resource-default-preview node-manager__main__table__release__preview"
                             >
                                 <img
                                     style="width: 100%; height: 100%;"
@@ -96,12 +100,11 @@
                                     class="resource-default-preview"
                                 />
                             </div>
-                            <div style="padding-left: 10px; overflow: hidden; flex-shrink: 1;">
-                                <div class="text-overflow-ellipsis"
-                                     style="color: #000; font-size: 14px; font-weight: 600; line-height: 20px; width: 100%;">
+                            <div class="node-manager__main__table__release__content">
+                                <div class="text-overflow-ellipsis node-manager__main__table__release__content__name">
                                     {{scope.row.releaseInfo.releaseName}}
                                 </div>
-                                <div style="line-height: 17px; color: #999; font-size: 12px;">
+                                <div class="node-manager__main__table__release__content__version">
                                     {{scope.row.releaseInfo.version}}
                                 </div>
                             </div>
@@ -114,7 +117,7 @@
                     min-width="18%"
                 >
                     <template slot-scope="scope">
-                        <div class="text-overflow-ellipsis" style="color: #000; font-size: 14px;">
+                        <div class="text-overflow-ellipsis node-manager__main__table__name">
                             {{scope.row.presentableName}}
                         </div>
                     </template>
@@ -142,7 +145,7 @@
                         </el-dropdown>
                     </template>
                     <template slot-scope="scope">
-                        <div style="color: #000; font-size: 14px;">
+                        <div class="node-manager__main__table__type">
                             {{scope.row.releaseInfo.resourceType}}
                         </div>
                     </template>
@@ -153,9 +156,8 @@
                     min-width="14%"
                 >
                     <div
-                        class="table-policies"
+                        class="table-policies node-manager__main__table__policy"
                         slot-scope="scope"
-                        style="display: flex; align-items: center; justify-content: space-between; width: 120px;"
                     >
                         <el-popover
                             placement="bottom-start"
@@ -168,16 +170,24 @@
                                 slot="reference"
                             >
                                 <div
-                                    style="padding-right: 10px; text-align: left; width: 80px; flex-shrink: 1;"
+                                    class="node-manager__main__table__policy__box"
                                 >
-                                    <div v-if="scope.row.policies.length > 0"
-                                         style="color: #409eff; font-size: 14px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 80px;">
+                                    <div
+                                        v-if="scope.row.policies.length > 0"
+                                        class="node-manager__main__table__policy__box--then-zero"
+                                    >
                                         {{scope.row.policies[0].policyName}}
                                     </div>
-                                    <div v-if="scope.row.policies.length === 0" style="color: #999; font-size: 14px;">
+                                    <div
+                                        v-if="scope.row.policies.length === 0"
+                                        class="node-manager__main__table__policy__box--zero"
+                                    >
                                         {{$t('noPolicy')}}
                                     </div>
-                                    <div v-if="scope.row.policies.length > 1" style="font-size: 12px; word-break: normal;">
+                                    <div
+                                        v-if="scope.row.policies.length > 1"
+                                        class="node-manager__main__table__policy__box--then-one"
+                                    >
                                         {{$t('suchAs')}}{{scope.row.policies.length}}{{$t('policies')}}…
                                     </div>
                                 </div>
@@ -185,9 +195,9 @@
                         </el-popover>
                         <a
                             @click="goToAddPolicyPage(scope.row.presentableId)"
-                            style="width: 26px; height: 20px; border-radius: 10px; background-color: #409eff; align-items: center; display: flex; justify-content: center; cursor: pointer; flex-shrink: 0;"
+                            class="node-manager__main__table__policy__add"
                         >
-                            <i style="color: #fff; font-size: 12px; font-weight: 600;" class="el-icon-plus"></i>
+                            <i class="el-icon-plus"></i>
                         </a>
                     </div>
                 </el-table-column>
@@ -197,10 +207,11 @@
                     min-width="18%"
                 >
                     <template slot-scope="scope">
-                        <div>
-                            <div style="font-size: 14px; color: #000;">{{dateStringToFormat(scope.row.updateDate)}}
+                        <div class="node-manager__main__table__date">
+                            <div class="node-manager__main__table__date__update">
+                                {{dateStringToFormat(scope.row.updateDate)}}
                             </div>
-                            <div style="color: #bfbfbf; font-size: 12px;">{{$t('joined')}}
+                            <div class="node-manager__main__table__date__create">{{$t('joined')}}
                                 {{dateStringToFormat(scope.row.createDate)}}
                             </div>
                         </div>
@@ -212,7 +223,9 @@
                     min-width="12%"
                 >
                     <template slot="header" slot-scope="scope">
-                        <el-dropdown style="height: 32px">
+                        <el-dropdown
+                            style="height: 32px"
+                        >
                             <div>
                                 {{selectedState}} <i class="el-icon-caret-bottom"></i>
                             </div>
@@ -220,7 +233,7 @@
                                 <el-dropdown-item v-for="item in allState">
                                     <a
                                         @click="onChangeState(item)"
-                                        style="display: block; width: 100%; height: 100%;"
+                                        class="node-manager__main__table__state__button"
                                     >{{item}}</a>
                                 </el-dropdown-item>
                             </el-dropdown-menu>
@@ -228,9 +241,15 @@
                     </template>
 
                     <template slot-scope="scope">
-                        <div style="font-size: 14px; display: flex; align-items: center;">
-                            <span v-if="scope.row.isOnline === 1" style="color: #000;">{{$t('online')}}</span>
-                            <span v-if="scope.row.isOnline === 0" style="color: #bfbfbf;">{{$t('noOnline')}}</span>
+                        <div class="node-manager__main__table__state__text">
+                            <span
+                                v-if="scope.row.isOnline === 1"
+                                class="node-manager__main__table__state__text--online"
+                            >{{$t('online')}}</span>
+                            <span
+                                v-if="scope.row.isOnline === 0"
+                                class="node-manager__main__table__state__text--no-online"
+                            >{{$t('noOnline')}}</span>
                             <template v-if="!scope.row.isAuth">
                                 <el-popover
                                     placement="top"
@@ -241,7 +260,6 @@
                                     <i
                                         slot="reference"
                                         class="el-icon-warning"
-                                        style="font-size: 20px; color: #ffc210; margin-left: 8px;"
                                     ></i>
                                 </el-popover>
                             </template>
@@ -262,23 +280,25 @@
                                 icon="el-icon-more"
                                 type="small"
                                 circle
-                                style="background-color: #fafbfb;"
+                                class="node-manager__main__table__operation"
                             ></el-button>
 
                             <el-dropdown-menu slot="dropdown">
                                 <el-dropdown-item>
                                     <a
                                         @click="goToEditPage(scope.row.presentableId)"
-                                        style="display: block; width: 100%; height: 100%; color: #333;"
+                                        class="node-manager__main__table__operation__dropdown-link"
                                     >{{$t('action.edit')}}</a>
                                 </el-dropdown-item>
                                 <el-dropdown-item>
                                     <a
-                                        style="display: block; width: 100%; height: 100%; color: #333;"
+                                        class="node-manager__main__table__operation__dropdown-link"
                                     >{{$t('action.top')}}</a>
                                 </el-dropdown-item>
                                 <el-dropdown-item>
-                                    <a @click="upgradePresentable(scope.row)">{{$t('action.upgrade')}}</a>
+                                    <a class="node-manager__main__table__operation__dropdown-link"
+                                       @click="upgradePresentable(scope.row)"
+                                    >{{$t('action.upgrade')}}</a>
                                 </el-dropdown-item>
                                 <el-dropdown-item>
                                     <a
@@ -296,11 +316,9 @@
             </el-table>
 
             <div
-                style="padding: 10px 0; display: flex; justify-content: flex-end;"
+                class="node-manager__main__pagination"
                 v-if="totalQuantity > pageSize"
             >
-                <!--                @current-change="onCurrentPageChange"-->
-                <!--                @size-change="onPageSizeChange"-->
                 <el-pagination
                     :current-page="currentPage"
                     :page-size="pageSize"
@@ -321,6 +339,10 @@
 
     export default manager;
 </script>
+
+<style lang="less" scoped>
+    @import 'index.less';
+</style>
 
 <style lang="less">
     .node-manager {
