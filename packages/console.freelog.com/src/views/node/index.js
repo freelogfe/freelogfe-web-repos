50cc0275f4fe9f30405a1b9b551d8b3f7aff9e52
kai <kai@MacBook-Pro.local>
2019-10-18 14:46:20 +0800

    清除节点无用代码

--- packages/console.freelog.com/src/views/node/index.js
+++ packages/console.freelog.com/src/views/node/index.js
@@ -1,10 +1,10 @@
 const nodeCreator = resolve => require.ensure([], () => resolve(require('./create/index.vue')), 'node')
-const nodeList = resolve => require.ensure([], () => resolve(require('./list/index.vue')), 'node')
-const nodeDetail = resolve => require.ensure([], () => resolve(require('./detail/index.vue')), 'node')
-const presentableList = resolve => require.ensure([], () => resolve(require('./presentables-V4/index.vue')), 'presentable')
-
-const nodePreview = resolve => require.ensure([], () => resolve(require('./preview/index.vue')), 'node')
-const presentableDetail = resolve => require.ensure([], () => resolve(require('./presentable-V4/detail/index.vue')), 'presentable')
+// const nodeList = resolve => require.ensure([], () => resolve(require('./list/index.vue')), 'node')
+// const nodeDetail = resolve => require.ensure([], () => resolve(require('./detail/index.vue')), 'node')
+// const presentableList = resolve => require.ensure([], () => resolve(require('./presentables-V4/index.vue')), 'presentable')
+//
+// const nodePreview = resolve => require.ensure([], () => resolve(require('./preview/index.vue')), 'node')
+// const presentableDetail = resolve => require.ensure([], () => resolve(require('./presentable-V4/detail/index.vue')), 'presentable')
 
 const nodeManager = resolve => require.ensure([], () => resolve(require('./manager/index.vue')), 'node');
 const nodeManagerRelease = resolve => require.ensure([], () => resolve(require('./manager-release/index.vue')), 'node');
@@ -12,13 +12,13 @@ const testNodeManager = resolve => require.ensure([], () => resolve(require('./t
 const testManagerResource = resolve => require.ensure([], () => resolve(require('./test-manager-resource/index.vue')), 'node');
 
 export default {
-    nodeCreator,
-    nodeList,
-    nodeDetail,
-    presentableList,
-    nodePreview,
-    presentableDetail,
+    // nodeList,
+    // nodeDetail,
+    // presentableList,
+    // nodePreview,
+    // presentableDetail,
 
+    nodeCreator,
     nodeManager,
     nodeManagerRelease,
     testNodeManager,
