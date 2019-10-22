32dc1a3a82d1c669d385fb10b5275a4308959b18
kai <kai@MacBook-Pro.local>
2019-10-22 16:47:14 +0800

    裁图基本完成

--- packages/console.freelog.com/src/components/UploadCover/index.js
+++ packages/console.freelog.com/src/components/UploadCover/index.js
@@ -141,7 +141,7 @@ export default {
                     ready() {
                     },
                     // preview: '.crop-img-preview'
-                })
+                });
             // }
         },
         /**
