a34a927dab34e324f3e07ac3620b91f27dbdd0b4
kai <kai@MacBook-Pro.local>
2019-11-06 14:35:54 +0800

    Update index.js

--- packages/console.freelog.com/src/views/node/test-manager/ReleaseList/index.js
+++ packages/console.freelog.com/src/views/node/test-manager/ReleaseList/index.js
@@ -1,5 +1,6 @@
 import AddAndReplace from '../AddAndReplace/index.vue';
 import RulesBar from '../../components/RulesBar';
+import {decompile} from '@freelog/nmr_translator';
 
 let searchInputDelay = null;
 
@@ -83,7 +84,7 @@ export default {
             // console.log(data.dataList, 'ddddddddddddDDDDDD');
         },
         /**
-         * 追加新加规则成功
+         * 修改规则成功，并且重新生成匹配规则
          */
         pushRuleSuccess(result) {
             this.matchTestResult = {
@@ -194,37 +195,42 @@ export default {
          * @param row
          */
         async onLineAndOffLine(row) {
+            const testRules = [...this.matchTestResult.testRules];
             const {nodeId} = this.$route.params;
-            const res = await this.$axios.get(`/v1/testNodes/${nodeId}`);
+            // const res = await this.$axios.get(`/v1/testNodes/${nodeId}`);
             const testResourceName = row.testResourceName;
             const isOnline = row.differenceInfo.onlineStatusInfo.isOnline === 1;
-            const ruleText = res.data.data ? res.data.data.ruleText : '';
-            if (isOnline) {
-                // 需要下线
-                const testRuleText = ruleText.replace(`^ ${testResourceName}`, `- ${testResourceName}`);
-                const response = await this.$axios.post(`/v1/testNodes`, {
-                    nodeId,
-                    testRuleText: Buffer.from(testRuleText).toString('base64'),
-                });
-                this.$message.success('下线成功');
+            const oldRulesText = this.matchTestResult.ruleText;
+            const rule = testRules.find(i => i.presentableName === testResourceName);
+            console.log(rule, 'rulerulerulerule');
+            let newRulesText;
+            if (rule) {
+                rule.online = !isOnline;
+                const ruleText = decompile([rule]);
+                newRulesText = oldRulesText.replace(rule.text, ruleText);
             } else {
-                // 需要上线
-                if (ruleText.includes(`- ${testResourceName}`)) {
-                    const testRuleText = ruleText.replace(`- ${testResourceName}`, `^ ${testResourceName}`);
-                    const response = await this.$axios.post(`/v1/testNodes`, {
-                        nodeId,
-                        testRuleText: Buffer.from(testRuleText).toString('base64'),
-                    });
-                } else {
-                    const testRuleText = `^ ${testResourceName}`;
-                    const params = {
-                        testRuleText: Buffer.from(testRuleText).toString('base64'),
-                    };
-                    const response = await this.$axios.put(`/v1/testNodes/${nodeId}/additionalTestRule`, params);
-                }
-                this.$message.success('上线成功');
+                const ruleObj = {
+                    "tags": null,
+                    "replaces": [],
+                    "online": !isOnline,
+                    "operation": "alter",
+                    "presentableName": testResourceName,
+                };
+                console.log(decompile([ruleObj]), 'decompile([ruleObj])');
+                newRulesText = oldRulesText + '\n' + decompile([ruleObj]);
             }
-            this.handleTableData();
+
+            const res = await this.$axios.post(`/v1/testNodes`, {
+                nodeId,
+                testRuleText: Buffer.from(newRulesText).toString('base64'),
+            });
+
+            if (res.data.errcode !== 0 || res.data.ret !== 0) {
+                return this.$message.error(JSON.stringify(res.data.data.errors));
+            }
+            isOnline ? this.$message.success('下线成功') : this.$message.success('上线成功');
+            // this.handleTableData();
+            this.pushRuleSuccess(res.data.data);
         }
     },
     watch: {
