373a75de6b7c82a25376c726a1bd4e2daf725399
kai <kai@MacBook-Pro.local>
2019-11-19 10:45:07 +0800

    添加 匹配错误 提示

--- packages/console.freelog.com/src/views/node/test-manager/StylePage/index.js
+++ packages/console.freelog.com/src/views/node/test-manager/StylePage/index.js
@@ -41,6 +41,11 @@ export default {
         async matchTestResources() {
             const {nodeId} = this.$route.params;
             const res = await this.$axios.post(`/v1/testNodes/${nodeId}/matchTestResources`);
+            
+            if (res.data.errcode !== 0 || res.data.ret !== 0) {
+                return this.$message.error(JSON.stringify(res.data.data.errors));
+            }
+
             const result = res.data.data;
             this.matchTestResult = {
                 ruleText: result.ruleText,
