4e75c064f7f12c4bb96ad530df37fb8be00da254
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2019-11-25 15:07:42 +0800

    Update index.js

--- packages/console.freelog.com/src/components/Tags/index.js
+++ packages/console.freelog.com/src/components/Tags/index.js
@@ -1,67 +1,68 @@
 export default {
-  name: 'freelog-tags',
-  data() {
-    return {
-      tags: [],
-      inputVisible: false,
-      inputValue: ''
-    }
-  },
-  props: {
-    value: {
-      type: Array,
-      default() {
-        return []
-      }
+    name: 'freelog-tags',
+    data() {
+        return {
+            tags: [],
+            inputVisible: false,
+            inputValue: ''
+        }
     },
-    actionText: {
-      type: String,
-      default() {
-        return 'New Tag'
-      }
+    props: {
+        value: {
+            type: Array,
+            default() {
+                return []
+            }
+        },
+        actionText: {
+            type: String,
+            default() {
+                return 'New Tag'
+            }
+        },
+        isCanCURD: {
+            type: Boolean,
+            default: true
+        },
     },
-    isCanCURD: {
-      type: Boolean,
-      default: true
+    watch: {
+        value(val) {
+            this.setCurrentValue(val)
+        }
     },
-  },
-  watch: {
-    value(val) {
-      this.setCurrentValue(val)
-    }
-  },
-  mounted() {
-    this.setCurrentValue(this.value)
-  },
-  methods: {
-    setCurrentValue(value) {
-      this.tags = value
+    mounted() {
+        this.setCurrentValue(this.value)
     },
-    handleClose(tag) {
-      // this.tags.splice(this.tags.indexOf(tag), 1)
+    methods: {
+        setCurrentValue(value) {
+            this.tags = value
+        },
+        handleClose(tag) {
+            // this.tags.splice(this.tags.indexOf(tag), 1)
 
-        this.tags = this.tags.filter(i => i !== tag);
-      this.$emit('input', this.tags);
-    },
-    showInput() {
-      this.inputVisible = true
-      this.$nextTick(() => {
-        this.$refs.saveTagInput.$refs.input.focus()
-      })
-    },
-    handleInputConfirm() {
-      const inputValue = this.inputValue
-      if (inputValue) {
-        // this.tags.push(inputValue)
-          const tags = [
-              ...this.tags,
-              inputValue,
-          ];
-          this.tags = Array.from(new Set(tags));
-        this.$emit('input', this.tags)
-      }
-      this.inputVisible = false;
-      this.inputValue = '';
+            this.tags = this.tags.filter(i => i !== tag);
+            this.$emit('input', this.tags);
+        },
+        showInput() {
+            this.inputVisible = true
+            this.$nextTick(() => {
+                this.$refs.saveTagInput.$refs.input.focus()
+            })
+        },
+        handleInputConfirm() {
+            const inputValue = this.inputValue
+        
+            if (inputValue) {
+                // this.tags.push(inputValue)
+                const tags = [
+                    ...this.tags,
+                    inputValue,
+                ];
+                this.tags = Array.from(new Set(tags));
+                this.$emit('input', this.tags)
+            }
+            this.inputVisible = false;
+            this.inputValue = '';
+        }
     }
-  }
 }
