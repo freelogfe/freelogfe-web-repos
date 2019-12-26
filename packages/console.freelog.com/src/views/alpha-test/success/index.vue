76a36a7008a4eafbe8ad56c34bce30d2d5a802f9
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2019-12-26 17:08:21 +0800

    修改图标

--- packages/console.freelog.com/src/views/alpha-test/success/index.vue
+++ packages/console.freelog.com/src/views/alpha-test/success/index.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>申请成功</div>
+    <div>
+        <div style="height: 160px;"/>
+        <div style="margin: 0 auto; text-align: center;">
+            <i
+                class="freelog fl-icon-shenqingchenggong"
+                style="color: #7ed321; font-size: 76px;"
+            />
+            <div style="height: 32px;"/>
+            <div style="color: #000; font-size: 18px;">申请提交成功</div>
+            <div style="height: 50px;"/>
+            <div style="color: #000; font-size: 14px;">我们会尽快审核您的申请，审核结果会通过您的注册邮箱或者手机号发送给你，敬请留意。</div>
+            <div style="height: 70px;"/>
+            <el-button type="primary">返回个人中心</el-button>
+        </div>
+    </div>
 </template>
 
 <script>
