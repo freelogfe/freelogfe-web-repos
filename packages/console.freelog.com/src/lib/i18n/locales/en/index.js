b3b864a97a74bc46fdda701aa891315a0274d547
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-01-15 14:41:36 +0800

    拆分 components i18n

--- packages/console.freelog.com/src/lib/i18n/locales/en/index.js
+++ packages/console.freelog.com/src/lib/i18n/locales/en/index.js
@@ -2,6 +2,7 @@ import mock from './mock';
 import node from './node';
 import layout from './layout';
 import resource from './resource';
+import components from './components';
 
 export default {
     common: {
@@ -242,124 +243,124 @@ export default {
     },
 
 
-    components: {
-        authScheme: {
-            checkMessages: ['The authorization scheme has been released and the current operation cannot be performed.', 'The authorization scheme has been discarded and the current operation cannot be performed.'],
-            signContractError: '{resources} did not select authorization policy',
-            update: 'update',
-            generate: 'generate',
-            signSuccessMsg: 'The presentable {presentableName} authorization contract {msg} succeeded!',
-            notHandleResource: 'Do not process this resource',
-            updateContract: 'Update contract',
-            createContract: 'Create contract',
-            schemeTitle: 'Authorization scheme',
-            unsignedResources: 'Pending resources',
-            policyTitle: 'Authorization policy',
-            hadSigned: 'Historical signed',
-            unavailable: 'unavailable',
-            selectPolicyMessages: ['The authorization scheme of the parent resource {resourceName} is not selected', 'The authorization scheme {authSchemeName} of the parent resource {resourceName} is not selected'],
-            switchSchemeTip: 'Switching the authorization scheme will cause the previously selected policies to be invalid.',
-            cancelSelectedSchemeTip: 'Cancelling the current selection will invalidate the selection of some authorization schemes',
-            confirmMsg: '{str}, sure to continue?',
-            signedDepsTip: 'Signed dependencies (total {length})',
-            unsignedDepsTip: 'Dependent resources not processed (total {length})',
-            resourceName: 'resource name',
-            authSchemeName: 'Authorization scheme',
-            policyName: 'Authorization Policy',
-            signState: 'Contract status',
-            noResolvedTip: 'The current scheme chooses not to process dependent resources, is it confirmed to sign the contract?',
-            signConfirmTitle: 'Signing confirmation',
-            signConfirmText: 'ok',
-            dialogTitles: ['Contract switching', 'Signing confirmation'],
-            selectedSchemesTitle: 'Selected authorization scheme',
-            selectTip: 'Please select the appropriate authorization scheme and policy...',
-            unhandledListTitle: 'Upcast Releases'
-        },
-
-        contractDetail: {
-            title: 'contract detail',
-            releaseName: 'release name',
-            releaseType: 'release type',
-            activateContract: 'activate contract',
-            triggerContract: 'activate',
-            authState: 'Authorization status: ',
-            defaultPolicyName: 'Authorization Policy',
-            activateContractSuccess: 'success'
-        },
-
-        contractManager: {
-            resourceList: 'resources',
-            masterResource: 'master resource',
-            subResourceId: 'subresource contract ID: ',
-            bubbleResources: 'bubble resources'
-        },
-
-        cropImage: {
-            reUpload: 're-upload'
-        },
-
-        detailInfo: {
-            createDate: 'creation date',
-            contractId: 'contract ID',
-            partyOne: 'party A',
-            partyTwo: 'party B'
-        },
-        lazyListView: {
-            noContentTip: 'No query results',
-            loadingTip: 'loading'
-        },
-
-        pagination: {
-            first: 'first',
-            previous: 'previous',
-            next: 'next',
-            last: 'last',
-            fromto: '{from}-{to},',
-            total: 'Total {total}'
-        },
-
-        policyEditor: {
-            inputPlaceholder: 'input here',
-            licensePlaceholder: 'input the license ID here',
-            demoTpl: 'Sample templates',
-            myTpl: 'my templates',
-            copyDone: 'Copied',
-            defaultPolicyNames: {
-                free: 'free policy',
-                charge: 'charge policy'
-            },
-            switchTip: 'Determine to {statusText} the policy <{policyName}>?',
-            offline: 'offline',
-            online: 'online',
-            template: 'template',
-            policyPlaceholder: 'input your policy here',
-            selectPolicyTitle: 'select a policy template'
-        },
-
-        policyList: {
-            addPolicy: 'add new policy',
-            unnamedPolicy: 'Unnamed policy'
-        },
-
-        policyTplSelector: {
-            name: 'Template name',
-            operation: 'select'
-        },
-
-        resourceButton: {
-            exception: 'Abnormal resource',
-            publish: 'publish',
-            offline: 'offline',
-            freeze: 'freeze',
-            publishTip: 'Determine to {tip}?',
-            success: 'success',
-            fail: 'fail'
-        },
-
-        richEditor: {
-            uploadTip: 'Click Upload or drag the image here'
-        }
-    },
+    // components: {
+    //     authScheme: {
+    //         checkMessages: ['The authorization scheme has been released and the current operation cannot be performed.', 'The authorization scheme has been discarded and the current operation cannot be performed.'],
+    //         signContractError: '{resources} did not select authorization policy',
+    //         update: 'update',
+    //         generate: 'generate',
+    //         signSuccessMsg: 'The presentable {presentableName} authorization contract {msg} succeeded!',
+    //         notHandleResource: 'Do not process this resource',
+    //         updateContract: 'Update contract',
+    //         createContract: 'Create contract',
+    //         schemeTitle: 'Authorization scheme',
+    //         unsignedResources: 'Pending resources',
+    //         policyTitle: 'Authorization policy',
+    //         hadSigned: 'Historical signed',
+    //         unavailable: 'unavailable',
+    //         selectPolicyMessages: ['The authorization scheme of the parent resource {resourceName} is not selected', 'The authorization scheme {authSchemeName} of the parent resource {resourceName} is not selected'],
+    //         switchSchemeTip: 'Switching the authorization scheme will cause the previously selected policies to be invalid.',
+    //         cancelSelectedSchemeTip: 'Cancelling the current selection will invalidate the selection of some authorization schemes',
+    //         confirmMsg: '{str}, sure to continue?',
+    //         signedDepsTip: 'Signed dependencies (total {length})',
+    //         unsignedDepsTip: 'Dependent resources not processed (total {length})',
+    //         resourceName: 'resource name',
+    //         authSchemeName: 'Authorization scheme',
+    //         policyName: 'Authorization Policy',
+    //         signState: 'Contract status',
+    //         noResolvedTip: 'The current scheme chooses not to process dependent resources, is it confirmed to sign the contract?',
+    //         signConfirmTitle: 'Signing confirmation',
+    //         signConfirmText: 'ok',
+    //         dialogTitles: ['Contract switching', 'Signing confirmation'],
+    //         selectedSchemesTitle: 'Selected authorization scheme',
+    //         selectTip: 'Please select the appropriate authorization scheme and policy...',
+    //         unhandledListTitle: 'Upcast Releases'
+    //     },
+    //
+    //     contractDetail: {
+    //         title: 'contract detail',
+    //         releaseName: 'release name',
+    //         releaseType: 'release type',
+    //         activateContract: 'activate contract',
+    //         triggerContract: 'activate',
+    //         authState: 'Authorization status: ',
+    //         defaultPolicyName: 'Authorization Policy',
+    //         activateContractSuccess: 'success'
+    //     },
+    //
+    //     contractManager: {
+    //         resourceList: 'resources',
+    //         masterResource: 'master resource',
+    //         subResourceId: 'subresource contract ID: ',
+    //         bubbleResources: 'bubble resources'
+    //     },
+    //
+    //     cropImage: {
+    //         reUpload: 're-upload'
+    //     },
+    //
+    //     detailInfo: {
+    //         createDate: 'creation date',
+    //         contractId: 'contract ID',
+    //         partyOne: 'party A',
+    //         partyTwo: 'party B'
+    //     },
+    //     lazyListView: {
+    //         noContentTip: 'No query results',
+    //         loadingTip: 'loading'
+    //     },
+    //
+    //     pagination: {
+    //         first: 'first',
+    //         previous: 'previous',
+    //         next: 'next',
+    //         last: 'last',
+    //         fromto: '{from}-{to},',
+    //         total: 'Total {total}'
+    //     },
+    //
+    //     policyEditor: {
+    //         inputPlaceholder: 'input here',
+    //         licensePlaceholder: 'input the license ID here',
+    //         demoTpl: 'Sample templates',
+    //         myTpl: 'my templates',
+    //         copyDone: 'Copied',
+    //         defaultPolicyNames: {
+    //             free: 'free policy',
+    //             charge: 'charge policy'
+    //         },
+    //         switchTip: 'Determine to {statusText} the policy <{policyName}>?',
+    //         offline: 'offline',
+    //         online: 'online',
+    //         template: 'template',
+    //         policyPlaceholder: 'input your policy here',
+    //         selectPolicyTitle: 'select a policy template'
+    //     },
+    //
+    //     policyList: {
+    //         addPolicy: 'add new policy',
+    //         unnamedPolicy: 'Unnamed policy'
+    //     },
+    //
+    //     policyTplSelector: {
+    //         name: 'Template name',
+    //         operation: 'select'
+    //     },
+    //
+    //     resourceButton: {
+    //         exception: 'Abnormal resource',
+    //         publish: 'publish',
+    //         offline: 'offline',
+    //         freeze: 'freeze',
+    //         publishTip: 'Determine to {tip}?',
+    //         success: 'success',
+    //         fail: 'fail'
+    //     },
+    //
+    //     richEditor: {
+    //         uploadTip: 'Click Upload or drag the image here'
+    //     }
+    // },
 
     resourceDetailView: {
         tabs: ['Resource Introduction', 'Authorization Scheme', 'Resource Description', 'Meta Information'],
@@ -471,4 +472,5 @@ export default {
     node,
     layout,
     resource,
+    components,
 }
--- packages/console.freelog.com/src/lib/i18n/locales/zh-CN/index.js
+++ packages/console.freelog.com/src/lib/i18n/locales/zh-CN/index.js
@@ -2,6 +2,7 @@ import mock from './mock';
 import node from './node';
 import layout from './layout';
 import resource from './resource';
+import components from './components';
 
 export default {
     common: {
@@ -245,124 +246,124 @@ export default {
     },
 
     //组件
-    components: {
-        authScheme: {
-            checkMessages: ['已发布该授权点，当前操作不可执行', '已废弃该授权点，当前操作不可执行'],
-            signContractError: '资源{resources}未选择授权策略',
-            update: '更新',
-            generate: '生成',
-            signSuccessMsg: '节点资源{presentableName}授权合约{msg}成功！',
-            notHandleResource: '不处理此资源',
-            updateContract: '更新合约',
-            createContract: '生成合约',
-            schemeTitle: '授权方案',
-            unsignedResources: '待签资源',
-            policyTitle: '授权策略',
-            hadSigned: '存在历史签约',
-            unavailable: '不可用',
-            selectPolicyMessages: ['父级资源{resourceName}未选中授权方案', '父级资源{resourceName}的授权方案{authSchemeName}未选中'],
-            switchSchemeTip: '切换授权方案，将会导致之前选择的策略都将失效',
-            cancelSelectedSchemeTip: '取消当前选择，将导致部分授权方案的选择失效',
-            confirmMsg: '{str}，确定继续？',
-            signedDepsTip: '签约的依赖资源（共{length}个）',
-            unsignedDepsTip: '不处理的依赖资源（共{length}个）',
-            resourceName: '资源名称',
-            authSchemeName: '授权方案',
-            policyName: '授权策略',
-            signState: '签约状态',
-            noResolvedTip: '当前方案选择不处理依赖资源，是否确认签约？',
-            signConfirmTitle: '签约确认',
-            signConfirmText: '确认',
-            dialogTitles: ['合约切换', '签约确认'],
-            selectedSchemesTitle: '已选中的授权方案',
-            selectTip: '请选择相应授权方案及策略……',
-            unhandledListTitle: '上抛（选择的上抛将会成为发行的基础上抛）'
-        },
-
-        contractDetail: {
-            title: '合约详情',
-            releaseName: '发行名称',
-            releaseType: '发行类型',
-            activateContract: '激活合同',
-            triggerContract: '立即激活',
-            authState: '授权状态：',
-            defaultPolicyName: '授权策略',
-            activateContractSuccess: '成功激活合同'
-        },
-
-        contractManager: {
-            resourceList: '资源列表',
-            masterResource: '主资源',
-            subResourceId: '子资源合同ID：',
-            bubbleResources: '上抛资源'
-        },
-
-        cropImage: {
-            reUpload: '重新上传'
-        },
-
-        detailInfo: {
-            createDate: '创建日期',
-            contractId: '合约ID',
-            partyOne: '甲方',
-            partyTwo: '乙方'
-        },
-        lazyListView: {
-            noContentTip: '暂无查询结果',
-            loadingTip: '拼命加载中'
-        },
-
-        pagination: {
-            first: '首页',
-            previous: '上一页',
-            next: '下一页',
-            last: '尾页',
-            fromto: '{from}-{to}条，',
-            total: '共{total}条'
-        },
-
-        policyEditor: {
-            inputPlaceholder: '请输入内容',
-            licensePlaceholder: '请输入内容License ID',
-            demoTpl: '示例模板',
-            myTpl: '我的模板',
-            copyDone: '已复制',
-            defaultPolicyNames: {
-                free: '免费策略',
-                charge: '收费策略'
-            },
-            switchTip: '确定{statusText}架策略 <{policyName}>？',
-            offline: '下架',
-            online: '上架',
-            template: '模板',
-            policyPlaceholder: '请输入策略',
-            selectPolicyTitle: '选择策略模板'
-        },
-
-        policyList: {
-            addPolicy: '添加新策略',
-            unnamedPolicy: '未命名策略'
-        },
-
-        policyTplSelector: {
-            name: '模板名称',
-            operation: '选择'
-        },
-
-        resourceButton: {
-            exception: '资源异常',
-            publish: '发布资源',
-            offline: '下架资源',
-            freeze: '资源冻结',
-            publishTip: '确定{tip}？',
-            success: '成功',
-            fail: '失败'
-        },
-
-        richEditor: {
-            uploadTip: '点击上传或将图片拖到此处'
-        }
-    },
+    // components: {
+    //     authScheme: {
+    //         checkMessages: ['已发布该授权点，当前操作不可执行', '已废弃该授权点，当前操作不可执行'],
+    //         signContractError: '资源{resources}未选择授权策略',
+    //         update: '更新',
+    //         generate: '生成',
+    //         signSuccessMsg: '节点资源{presentableName}授权合约{msg}成功！',
+    //         notHandleResource: '不处理此资源',
+    //         updateContract: '更新合约',
+    //         createContract: '生成合约',
+    //         schemeTitle: '授权方案',
+    //         unsignedResources: '待签资源',
+    //         policyTitle: '授权策略',
+    //         hadSigned: '存在历史签约',
+    //         unavailable: '不可用',
+    //         selectPolicyMessages: ['父级资源{resourceName}未选中授权方案', '父级资源{resourceName}的授权方案{authSchemeName}未选中'],
+    //         switchSchemeTip: '切换授权方案，将会导致之前选择的策略都将失效',
+    //         cancelSelectedSchemeTip: '取消当前选择，将导致部分授权方案的选择失效',
+    //         confirmMsg: '{str}，确定继续？',
+    //         signedDepsTip: '签约的依赖资源（共{length}个）',
+    //         unsignedDepsTip: '不处理的依赖资源（共{length}个）',
+    //         resourceName: '资源名称',
+    //         authSchemeName: '授权方案',
+    //         policyName: '授权策略',
+    //         signState: '签约状态',
+    //         noResolvedTip: '当前方案选择不处理依赖资源，是否确认签约？',
+    //         signConfirmTitle: '签约确认',
+    //         signConfirmText: '确认',
+    //         dialogTitles: ['合约切换', '签约确认'],
+    //         selectedSchemesTitle: '已选中的授权方案',
+    //         selectTip: '请选择相应授权方案及策略……',
+    //         unhandledListTitle: '上抛（选择的上抛将会成为发行的基础上抛）'
+    //     },
+    //
+    //     contractDetail: {
+    //         title: '合约详情',
+    //         releaseName: '发行名称',
+    //         releaseType: '发行类型',
+    //         activateContract: '激活合同',
+    //         triggerContract: '立即激活',
+    //         authState: '授权状态：',
+    //         defaultPolicyName: '授权策略',
+    //         activateContractSuccess: '成功激活合同'
+    //     },
+    //
+    //     contractManager: {
+    //         resourceList: '资源列表',
+    //         masterResource: '主资源',
+    //         subResourceId: '子资源合同ID：',
+    //         bubbleResources: '上抛资源'
+    //     },
+    //
+    //     cropImage: {
+    //         reUpload: '重新上传'
+    //     },
+    //
+    //     detailInfo: {
+    //         createDate: '创建日期',
+    //         contractId: '合约ID',
+    //         partyOne: '甲方',
+    //         partyTwo: '乙方'
+    //     },
+    //     lazyListView: {
+    //         noContentTip: '暂无查询结果',
+    //         loadingTip: '拼命加载中'
+    //     },
+    //
+    //     pagination: {
+    //         first: '首页',
+    //         previous: '上一页',
+    //         next: '下一页',
+    //         last: '尾页',
+    //         fromto: '{from}-{to}条，',
+    //         total: '共{total}条'
+    //     },
+    //
+    //     policyEditor: {
+    //         inputPlaceholder: '请输入内容',
+    //         licensePlaceholder: '请输入内容License ID',
+    //         demoTpl: '示例模板',
+    //         myTpl: '我的模板',
+    //         copyDone: '已复制',
+    //         defaultPolicyNames: {
+    //             free: '免费策略',
+    //             charge: '收费策略'
+    //         },
+    //         switchTip: '确定{statusText}架策略 <{policyName}>？',
+    //         offline: '下架',
+    //         online: '上架',
+    //         template: '模板',
+    //         policyPlaceholder: '请输入策略',
+    //         selectPolicyTitle: '选择策略模板'
+    //     },
+    //
+    //     policyList: {
+    //         addPolicy: '添加新策略',
+    //         unnamedPolicy: '未命名策略'
+    //     },
+    //
+    //     policyTplSelector: {
+    //         name: '模板名称',
+    //         operation: '选择'
+    //     },
+    //
+    //     resourceButton: {
+    //         exception: '资源异常',
+    //         publish: '发布资源',
+    //         offline: '下架资源',
+    //         freeze: '资源冻结',
+    //         publishTip: '确定{tip}？',
+    //         success: '成功',
+    //         fail: '失败'
+    //     },
+    //
+    //     richEditor: {
+    //         uploadTip: '点击上传或将图片拖到此处'
+    //     }
+    // },
 
     //页面级
     resourceDetailView: {
@@ -476,4 +477,5 @@ export default {
     node,
     layout,
     resource,
+    components,
 }
