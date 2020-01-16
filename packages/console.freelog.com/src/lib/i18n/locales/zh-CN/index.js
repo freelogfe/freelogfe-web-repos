48188599b8793711fcfe75d9af96c6c92d73c5af
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-01-16 15:42:06 +0800

    routes i18n

--- packages/console.freelog.com/src/lib/i18n/locales/zh-CN/index.js
+++ packages/console.freelog.com/src/lib/i18n/locales/zh-CN/index.js
@@ -1,5 +1,7 @@
 import common from './common';
 import components from './components';
+import routes from './routes';
+
 import mock from './mock';
 import node from './node';
 import layout from './layout';
@@ -118,49 +120,6 @@ export default {
         noFavorReleases: '暂无收藏发行',
         historicVersion: '历史版本',
         searchTitle: '全局搜索',
-    },
-
-    //路由标题
-    routes: {
-        accountSetting: '账号设置',
-
-        mockResource: 'mock资源',
-
-        nodes: '节点',
-        releaseMarket: '发行市场',
-        nodeExamples: '示例节点',
-        presentableDetail: '节点资源详情',
-        nodesSystem: '节点管理系统',
-
-        nodeManager: '节点管理',
-        nodeReleaseManagement: '节点发行管理',
-
-        createNode: '创建节点',
-        nodeList: '节点列表',
-        policyList: '策略模板列表',
-        createPolicyTpl: '创建策略模板',
-        policyTplDetail: '策略模板详情',
-        nodeDetail: '节点详情',
-        resourceSystem: '资源管理系统',
-        resourcePolicyTplList: '资源策略模板列表',
-        createResource: '创建资源',
-        updateResource: '更新资源',
-        myResources: '我的资源',
-        myReleases: '我的发行',
-        myCollections: '我的收藏',
-        // resourceDetail: '资源详情',
-        resourceManager: '资源管理',
-        createResourcePolicyTpl: '创建资源策略模板',
-        resourcePolicyTplDetail: '资源策略模板详情',
-        createRelease: '创建发行',
-        releaseDetail: '发行详情',
-        releaseSystem: '',
-        releaseManager: '发行管理',
-        releaseAdd: '添加发行',
-
-        mockResourcePool: '模拟资源池',
-        createMockResource: '创建模拟资源',
-        manageMockResource: '模拟资源管理',
     },
 
     axios: {
@@ -310,6 +269,7 @@ export default {
     //
     common,
     components,
+    routes,
 
     mock,
     node,
