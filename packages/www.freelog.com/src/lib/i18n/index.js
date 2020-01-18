5780bf29c705bd0b4c0deeb432d68582b91939b1
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-01-18 15:43:55 +0800

    www i18n

--- packages/www.freelog.com/src/lib/i18n/index.js
+++ packages/www.freelog.com/src/lib/i18n/index.js
@@ -1,5 +1,5 @@
 import { initI18n } from '@freelog/freelog-common-lib'
-import cn from './locales/cn'
-import en from './locales/en'
+import cn from '../../../../freelog-i18n/www/zh-CN';
+import en from '../../../../freelog-i18n/www/en';
 
 export default initI18n({ cn, en })
