64178cf2ee281f63efb3f4671c1a9dfa78c7536a
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-06-29 14:40:25 +0800

    文字

--- packages/console/src/components/FText/FNav/index.tsx
+++ packages/console/src/components/FText/FNav/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import styles from './index.less';
 
-export default function () {
+interface FNavProps {
+  text: string;
+  type?: 'normal' | 'active';
+}
 
+// TODO:
+export default function ({text, type = 'normal'}: FNavProps) {
+  return (<div className={styles.text + ' ' + styles[type]}>{text}</div>);
 }
--- packages/console/src/components/FText/FTitle/index.tsx
+++ packages/console/src/components/FText/FTitle/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import styles from './index.less';
 
-export default function () {
 
+interface FTitleProps {
+  text: string;
+  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'form';
+}
+
+// TODO:
+export default function ({text, type = 'h1'}: FTitleProps) {
+  return (<div className={styles.text + ' ' + styles[type]}>{text}</div>);
 }
