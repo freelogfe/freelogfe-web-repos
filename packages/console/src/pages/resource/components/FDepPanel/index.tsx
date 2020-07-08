9968a8ade4c591b169845b376c832089bb2f2564
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-07-08 10:41:51 +0800

    创建版本

--- packages/console/src/pages/resource/components/FDepPanel/index.tsx
+++ packages/console/src/pages/resource/components/FDepPanel/index.tsx
@@ -4,7 +4,13 @@ import styles from './index.less';
 import {FContentText} from '@/components/FText';
 import {FCircleButton} from '@/components/FButton';
 import {EditOutlined, InfoCircleFilled} from '@ant-design/icons';
-import {Radio} from 'antd';
+import {Radio, Checkbox, Space} from 'antd';
+
+const code = 'initial:\n' +
+  '    active\n' +
+  '    recontractable\n' +
+  '    presentable\n' +
+  '    terminate';
 
 export default function () {
   return (<div className={styles.DepPanel}>
@@ -56,13 +62,13 @@ export default function () {
     <div className={styles.DepPanelContent}>
       <div className={styles.radios}>
         <div>
-          <Radio checked={true}/>
+          <Radio style={{lineHeight: '16px'}} checked={true}/>
           <span style={{color: '#666'}}>上抛</span>
           <InfoCircleFilled style={{color: '#C7C7C7', fontSize: 16, marginLeft: 20}}/>
         </div>
         <div style={{height: 18}}/>
         <div>
-          <Radio checked={false}/>
+          <Radio style={{lineHeight: '16px'}} checked={false}/>
           <span style={{color: '#666'}}>签约</span>
           <InfoCircleFilled style={{color: '#C7C7C7', fontSize: 16, marginLeft: 20}}/>
         </div>
@@ -77,10 +83,29 @@ export default function () {
           <div style={{height: 10}}/>
           <div className={styles.Policy}>
             <div className={styles.PolicyGrammar}>
-
+              <div className={styles.PolicyGrammarName}>
+                <Checkbox/><span>策略1</span>
+                <div>
+                  <label className={styles.executing}>执行中</label>
+                </div>
+              </div>
+              <div style={{height: 15}}/>
+              <pre className={styles.highlight}>{code}</pre>
             </div>
             <div className={styles.PolicyInfo}>
-
+              <Space size={40}>
+                <FContentText type="additional2" text={'合约ID：adhjtyrghgjhxdfthgasdhdflgkftr'}/>
+                <FContentText type="additional2" text={'签约时间：2019-10-10'}/>
+              </Space>
+              <div style={{height: 9}}/>
+              <div>
+                <FContentText type="additional2">
+                  应用版本：
+                  <Space size={15}>
+                    <span>10.5.2</span><span>10.5.3</span>
+                  </Space>
+                </FContentText>
+              </div>
             </div>
           </div>
         </>
