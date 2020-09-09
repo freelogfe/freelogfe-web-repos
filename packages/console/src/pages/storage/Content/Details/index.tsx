a3afbea4371c9ee2109bf58779a95f4b958bf0a3
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-09-09 17:09:50 +0800

    添加上抛

--- packages/console/src/pages/storage/Content/Details/index.tsx
+++ packages/console/src/pages/storage/Content/Details/index.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import styles from './index.less';
 import {FTitleText, FContentText} from '@/components/FText';
-import {CopyOutlined, DownloadOutlined} from '@ant-design/icons';
+import {CopyOutlined, DownloadOutlined, ArrowUpOutlined} from '@ant-design/icons';
 import {FTextButton, FCircleButton} from '@/components/FButton';
-import {Space, Divider} from 'antd';
+import {Space, Divider, Popover} from 'antd';
 import FEditorCard from '@/components/FEditorCard';
 import FSelect from '@/components/FSelect';
+
 
 interface DetailsProps {
 
 }
 
 function Details({}: DetailsProps) {
-  return (<div>
+  // const divContainer = React.useRef<HTMLDivElement>(null);
+
+  return (<div className={styles.divContainer}>
     <div style={{height: 10}}/>
     <Space size={15}>
       <FTitleText text={'bucket-001/2309737.png'} type="h3"/>
@@ -34,30 +37,8 @@ function Details({}: DetailsProps) {
         <FCircleButton theme="weaken"/>
         <FContentText text={'添加'}/>
       </Space>
-      <div style={{height: 30}}/>
-      <FContentText text={'添加'}/>
-      <div className={styles.resources}>
-        <div className={styles.resource}>
-          <div className={styles.resourceLeft}>
-            <div className={styles.resourceTitle}>
-              <FContentText
-                singleRow={true}
-                text={'stefan/image2image2image2image2image2'}
-                className={styles.resourceName}
-              />
-              <span className={styles.notOnline}>未上线</span>
-            </div>
-            <div style={{height: 9}}/>
-            <div className={styles.resourceInfo}>
-              <FContentText type="additional2">image</FContentText>
-              <Divider type="vertical"/>
-              <FContentText type="additional2">版本范围：xxx</FContentText>
-              <Divider type="vertical"/>
-              <FContentText type="additional2">3个基础上抛</FContentText>
-            </div>
-          </div>
-        </div>
-      </div>
+      <DepsCards/>
+      <DepsCards/>
     </FEditorCard>
     <FEditorCard title={'自定义属性'}>
       <Space size={10}>
@@ -69,3 +50,78 @@ function Details({}: DetailsProps) {
 }
 
 export default Details;
+
+function BasisUpthrows() {
+  return (
+    <Space direction="vertical" size={10}>
+      <div>
+        <Space size={10}>
+          <ArrowUpOutlined style={{color: '#EA7171'}}/>
+          <span>stefan/image9</span>
+        </Space>
+      </div>
+      <div>
+        <Space size={10}>
+          <ArrowUpOutlined style={{color: '#EA7171'}}/>
+          <span>stefan/image9</span>
+        </Space>
+      </div>
+    </Space>
+  );
+}
+
+interface Interface {
+
+}
+
+function DepsCards() {
+  const [ref, setRef] = React.useState<HTMLDivElement | null>(null);
+
+  const [visibleIndex, setVisibleIndex] = React.useState<number>(-1);
+  // React.useEffect(() => {
+  //   console.log(ref, 'RRRRRR');
+  // }, [ref]);
+
+  return (<div
+    className={styles.DepsCards}
+    ref={(div) => setRef(div)}
+  >
+    <div style={{height: 30}}/>
+    <FContentText text={'添加'}/>
+    <div style={{height: 15}}/>
+    <div className={styles.resources}>
+      <div className={styles.resource}>
+        <div className={styles.resourceLeft}>
+          <div className={styles.resourceTitle}>
+            <FContentText
+              singleRow={true}
+              text={'stefan/image2image2image2image2image2'}
+              className={styles.resourceName}
+            />
+            <span className={styles.notOnline}>未上线</span>
+          </div>
+          <div style={{height: 9}}/>
+          <div className={styles.resourceInfo}>
+            <FContentText type="additional2">image</FContentText>
+            <Divider type="vertical"/>
+            <FContentText type="additional2">版本范围：xxx</FContentText>
+            <Divider type="vertical"/>
+            {/*{console.log(divContainer, 'divContainer')}*/}
+            {ref && <Popover
+              // trigger={'hover'}
+              // onVisibleChange={(visible: boolean) => console.log(visible, 'VVVVV')}
+              // visible={visibleIndex === 0}
+              getPopupContainer={(triggerNode) => {
+                return ref || document.body;
+              }}
+              content={<BasisUpthrows/>}
+            >
+              <div><FContentText type="additional2">3个基础上抛</FContentText></div>
+            </Popover>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>);
+}
