df5a2d14c08765782309b982932cf29bae863a99
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-09-27 18:10:59 +0800

    Create index.tsx

---
+++ packages/console/src/pages/market/$id/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import styles from './index.less';
+import FCenterLayout from "@/layouts/FCenterLayout";
+
+interface ResourceDetailsProps {
+
+}
+
+function ResourceDetails({}: ResourceDetailsProps) {
+  return (<FCenterLayout>
+
+  </FCenterLayout>);
+}
+
+export default ResourceDetails;
