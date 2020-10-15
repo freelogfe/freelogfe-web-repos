e8b7de03aed60a477f174c3288a7e77d1c7e05d1
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-10-15 18:12:20 +0800

    Update index.tsx

--- packages/console/src/pages/resource/$id/index/Sign/Bottom/index.tsx
+++ packages/console/src/pages/resource/$id/index/Sign/Bottom/index.tsx
@@ -12,20 +12,20 @@ interface BottomProps {
 
 function Bottom({dispatch, marketResourcePage}: BottomProps) {
 
-  const resourcePolicyNums = marketResourcePage.signResources.map((sr) => sr.policies.filter((srp) => srp.checked).length);
-  console.log(resourcePolicyNums, 'resourcePolicyNums0329');
-
   return (<div className={styles.signBottom}>
-    <FNormalButton
-      className={styles.signButton}
-      disabled={
-        marketResourcePage.selectedNodeID === -1
-        || marketResourcePage.signedNodeIDs.includes(marketResourcePage.selectedNodeID)
-        || resourcePolicyNums.includes(0)
-      }
-      onClick={() => router.push(`/resource/${marketResourcePage.resourceId}/sign`)}
-    >签约</FNormalButton>
-    {/*<span>该资源已签约，可进入<a>展品管理</a>进行授权管理</span>*/}
+    {
+      !marketResourcePage.signedNodeIDs.includes(marketResourcePage.selectedNodeID)
+        ? (<FNormalButton
+          className={styles.signButton}
+          disabled={
+            marketResourcePage.selectedNodeID === -1
+            || marketResourcePage.signResources.map((sr) => sr.policies.filter((srp) => srp.checked).length).includes(0)
+          }
+          onClick={() => router.push(`/resource/${marketResourcePage.resourceId}/sign`)}
+        >签约</FNormalButton>)
+        : (<span>该资源已签约，可进入<a>展品管理</a>进行授权管理</span>)
+    }
+
   </div>);
 }
 
