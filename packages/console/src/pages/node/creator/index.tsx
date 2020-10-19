a739f1f3f73fd9b63eade14640360c27a9bded9e
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-10-19 15:06:52 +0800

    创建节点UI基本完成

--- packages/console/src/pages/node/creator/index.tsx
+++ packages/console/src/pages/node/creator/index.tsx
@@ -1,12 +1,48 @@
 import * as React from 'react';
 import styles from './index.less';
+import FCenterLayout from "@/layouts/FCenterLayout";
+import {FTitleText, FContentText} from '@/components/FText';
+import {FNormalButton} from '@/components/FButton';
+import {Input} from 'antd';
 
 interface NodeCreatorProps {
 
 }
 
 function NodeCreator({}: NodeCreatorProps) {
-  return (<div>NodeCreator</div>);
+  return (<FCenterLayout>
+    <div className={styles.header}>
+      <FTitleText type="h1" text={'创建节点'}/>
+    </div>
+    <div className={styles.body}>
+      <div className={styles.domain}>
+        <FContentText type="negative" text={'节点地址'}/>
+        <div className={styles.inputWrap}>
+          <Input className={styles.input} placeholder={'输入节点地址'}/>
+        </div>
+        <FContentText type="negative" text={'.freelog.com'}/>
+      </div>
+      <div className={styles.errorTip}>
+        {/*该节点地址已经存在或已经被其它用户使用*/}
+        只能包括小写字母、数字和短横线（-）。<br/>
+        必须以小写字母或者数字开头和结尾。<br/>
+        长度必须在 4-24 字符之间。
+      </div>
+      <div className={styles.name}>
+        <FContentText type="negative" text={'节点名称'}/>
+        <div className={styles.inputWrap}>
+          <Input className={styles.input} placeholder={'输入节点名称'}/>
+        </div>
+      </div>
+      <div className={styles.errorTip}>
+        {/*该节点名称已经存在或已经被其它用户使用*/}
+        长度必须在 1-100 字符之间。<br/>
+        不能以正斜线（/）或者反斜线（\）开头。<br/>
+        开头和结尾的空格会自动删除。
+      </div>
+      <FNormalButton className={styles.button}>创建节点</FNormalButton>
+    </div>
+  </FCenterLayout>);
 }
 
 export default NodeCreator;
