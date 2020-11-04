e59f7c268b4e92303853bc294fc9b8adbed2c68d
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-11-04 10:52:51 +0800

    节点导航

--- packages/console/src/layouts/FLayout/Node/index.tsx
+++ packages/console/src/layouts/FLayout/Node/index.tsx
@@ -8,14 +8,20 @@ import {FNormalButton} from "@/components/FButton";
 import {i18nMessage} from "@/utils/i18n";
 import FDropdown from "@/components/FDropdown";
 import {connect, Dispatch} from 'dva';
-import {ConnectState, NodesModelState} from "@/models/connect";
+import {ConnectState, GlobalModelState, NodesModelState} from "@/models/connect";
 import Nav from "../components/Nav";
 
 interface NodeProps {
+  dispatch: Dispatch;
   nodes: NodesModelState;
+  global: GlobalModelState;
 }
 
-function Node({nodes}: NodeProps) {
+function Node({dispatch, nodes, global}: NodeProps) {
+
+  const cRoute = global.routerHistories[global.routerHistories.length - 1];
+  const isCurrent: boolean = /\/node\/(\d*$)/.test(cRoute.pathname);
+  const nodeId: string | null = (cRoute.pathname.match(/\/node\/(\d*$)/) || [null, null])[1];
 
   function onClickNodes(value: string) {
     return router.push('/node/' + value);
@@ -25,6 +31,7 @@ function Node({nodes}: NodeProps) {
     // visible={true}
     overlay={nodes.list.length > 0 ? (<div>
       <FMenu
+        value={nodeId || ''}
         onClick={onClickNodes}
         options={nodes.list.map((n) => ({
           text: n.nodeName,
@@ -42,10 +49,13 @@ function Node({nodes}: NodeProps) {
       <div style={{height: 30}}/>
       <FNormalButton size="small">创建节点</FNormalButton>
     </div>)}>
-    <Nav onClick={() => null} active={false}>{i18nMessage('node_manage')}</Nav>
+    <Nav
+      onClick={() => null}
+      active={isCurrent}>{i18nMessage('node_manage')}</Nav>
   </FDropdown>);
 }
 
-export default connect(({nodes}:ConnectState) => ({
+export default connect(({nodes, global}: ConnectState) => ({
   nodes,
+  global,
 }))(Node);
