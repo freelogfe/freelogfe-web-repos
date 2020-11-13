defa352996f7b98ed2f0a1a4726c78b19de9973d
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-11-13 18:04:20 +0800

    节点校验

--- packages/console/src/models/nodes.ts
+++ packages/console/src/models/nodes.ts
@@ -13,9 +13,11 @@ export type NodesModelState = WholeReadonly<{
   }[];
 
   nodeName: string;
+  nameVerify: 0 | 1 | 2;
   nameError: string;
 
   nodeDomain: string;
+  domainVerify: 0 | 1 | 2;
   domainError: string;
 }>;
 
@@ -32,12 +34,30 @@ export interface CreateNodeAction extends AnyAction {
   type: 'nodes/createNode';
 }
 
+export interface OnChangeNameAction extends AnyAction {
+  type: 'nodes/onChangeName';
+  payload: string;
+}
+
+export interface OnChangeDomainAction extends AnyAction {
+  type: 'nodes/onChangeDomain';
+  payload: string;
+}
+
+export interface InitModelStateAction extends AnyAction {
+  type: 'nodes/initModelState';
+  // payload: string;
+}
+
 export interface NodesModelType {
   namespace: 'nodes';
   state: WholeReadonly<NodesModelState>;
   effects: {
+    initModelState: (action: InitModelStateAction, effects: EffectsCommandMap) => void;
     fetchNodes: (action: FetchNodesAction, effects: EffectsCommandMap) => void;
     createNode: (action: CreateNodeAction, effects: EffectsCommandMap) => void;
+    onChangeName: (action: OnChangeNameAction, effects: EffectsCommandMap) => void;
+    onChangeDomain: (action: OnChangeDomainAction, effects: EffectsCommandMap) => void;
   };
   reducers: {
     change: DvaReducer<NodesModelState, ChangeAction>;
@@ -47,18 +67,29 @@ export interface NodesModelType {
   };
 }
 
+const initStates: Omit<NodesModelState, 'list'> = {
+  nodeName: '',
+  nameVerify: 0,
+  nameError: '',
+
+  nodeDomain: '',
+  domainVerify: 0,
+  domainError: '',
+};
+
 const Model: NodesModelType = {
   namespace: 'nodes',
   state: {
     list: [],
-
-    nodeName: '',
-    nameError: '',
-
-    nodeDomain: '',
-    domainError: '',
+    ...initStates,
   },
   effects: {
+    * initModelState({}: InitModelStateAction, {put}: EffectsCommandMap) {
+      yield put<ChangeAction>({
+        type: 'change',
+        payload: initStates,
+      });
+    },
     * fetchNodes({}: FetchNodesAction, {call, put}: EffectsCommandMap) {
       // console.log('FetchNodesAction******');
       const params: NodesParamsType = {};
@@ -86,24 +117,34 @@ const Model: NodesModelType = {
         nodes,
       }));
 
+      if (nodes.domainError || nodes.nameError) {
+        return;
+      }
+
+      const params: CreateParamsType = {
+        nodeDomain: nodes.nodeDomain,
+        nodeName: nodes.nodeName,
+      };
+
+      const {data} = yield call(create, params);
+
+      router.push('/node/' + data.nodeId);
+    },
+    * onChangeName({payload}: OnChangeNameAction, {select, call, put}: EffectsCommandMap) {
+
+      yield put<ChangeAction>({
+        type: 'change',
+        payload: {
+          nodeName: payload,
+          nameVerify: 1,
+        },
+      });
+
+      const {nodes}: ConnectState = yield select(({nodes}: ConnectState) => ({
+        nodes,
+      }));
+
       let nameError: string = '';
-      let domainError: string = '';
-
-      if (!/^(?!-)[a-z0-9-]{4,24}(?<!-)$/.test(nodes.nodeDomain)) {
-        domainError = '只能包括小写字母、数字和短横线（-）。\n' +
-          '必须以小写字母或者数字开头和结尾。\n' +
-          '长度必须在 4-24 字符之间。';
-      }
-
-      if (!domainError) {
-        const params1: NodeDetailParamsType2 = {
-          nodeDomain: nodes.nodeDomain,
-        };
-        const {data: data1} = yield call(nodeDetail, params1);
-        if (data1) {
-          domainError = '该节点地址已经存在或已经被其它用户使用';
-        }
-      }
 
       if (!/^[\u4E00-\u9FA5|a-zA-Z0-9]{4,20}$/.test(nodes.nodeName)) {
         nameError = '长度必须在 1-100 字符之间。\n' +
@@ -124,35 +165,50 @@ const Model: NodesModelType = {
       yield put<ChangeAction>({
         type: 'change',
         payload: {
-          domainError,
+          nameVerify: 2,
           nameError,
-        }
+        },
+      });
+    },
+    * onChangeDomain({payload}: OnChangeDomainAction, {call, select, put}: EffectsCommandMap) {
+
+      yield put<ChangeAction>({
+        type: 'change',
+        payload: {
+          nodeDomain: payload,
+          domainVerify: 1,
+        },
       });
 
-      if (domainError || nameError) {
-        return;
+      const {nodes}: ConnectState = yield select(({nodes}: ConnectState) => ({
+        nodes,
+      }));
+
+      let domainError: string = '';
+
+      if (!/^(?!-)[a-z0-9-]{4,24}(?<!-)$/.test(nodes.nodeDomain)) {
+        domainError = '只能包括小写字母、数字和短横线（-）。\n' +
+          '必须以小写字母或者数字开头和结尾。\n' +
+          '长度必须在 4-24 字符之间。';
+      }
+
+      if (!domainError) {
+        const params1: NodeDetailParamsType2 = {
+          nodeDomain: nodes.nodeDomain,
+        };
+        const {data: data1} = yield call(nodeDetail, params1);
+        if (data1) {
+          domainError = '该节点地址已经存在或已经被其它用户使用';
+        }
       }
 
       yield put<ChangeAction>({
         type: 'change',
         payload: {
+          domainVerify: 2,
           domainError,
-          nameError,
-        }
+        },
       });
-
-      if (domainError || nameError) {
-        return;
-      }
-
-      const params: CreateParamsType = {
-        nodeDomain: nodes.nodeDomain,
-        nodeName: nodes.nodeName,
-      };
-
-      const {data} = yield call(create, params);
-      // console.log(data, 'datadata');
-      router.push('/node/' + data.nodeId);
     },
   },
   reducers: {
--- packages/console/src/pages/node/creator/index.tsx
+++ packages/console/src/pages/node/creator/index.tsx
@@ -3,10 +3,18 @@ import styles from './index.less';
 import FCenterLayout from "@/layouts/FCenterLayout";
 import {FTitleText, FContentText} from '@/components/FText';
 import {FNormalButton} from '@/components/FButton';
-import {Input} from 'antd';
+import {Input, Space} from 'antd';
 import {connect, Dispatch} from 'dva';
 import {ConnectState, NodesModelState} from '@/models/connect';
-import {ChangeAction, CreateNodeAction} from "@/models/nodes";
+import {
+  ChangeAction,
+  CreateNodeAction,
+  InitModelStateAction,
+  OnChangeDomainAction,
+  OnChangeNameAction
+} from "@/models/nodes";
+import {FCheck, FLoading} from '@/components/FIcons';
+import FInput from "@/components/FInput";
 
 interface NodeCreatorProps {
   dispatch: Dispatch;
@@ -14,48 +22,66 @@ interface NodeCreatorProps {
 }
 
 function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
+  React.useEffect(() => {
+    dispatch<InitModelStateAction>({
+      type: 'nodes/initModelState',
+    });
+  }, []);
+
   return (<FCenterLayout>
     <div className={styles.header}>
       <FTitleText type="h1" text={'创建节点'}/>
     </div>
     <div className={styles.body}>
-      <div className={styles.domain}>
-        <FContentText type="negative" text={'节点地址'}/>
-        <div className={styles.inputWrap}>
-          <Input
-            value={nodes.nodeDomain}
-            className={styles.input}
-            placeholder={'输入节点地址'}
-            onChange={(e) => dispatch<ChangeAction>({
-              type: 'nodes/change',
-              payload: {
-                nodeDomain: e.target.value,
-              }
-            })}
-          />
+      <Space size={10}>
+        <div className={styles.domain}>
+          <FContentText type="negative" text={'节点地址'}/>
+          <div className={styles.inputWrap}>
+            <FInput
+              value={nodes.nodeDomain}
+              debounce={300}
+              className={styles.input}
+              placeholder={'输入节点地址'}
+              onDebounceChange={(value) => dispatch<OnChangeDomainAction>({
+                type: 'nodes/onChangeDomain',
+                payload: value,
+              })}
+            />
+          </div>
+          <FContentText type="negative" text={'.freelog.com'}/>
         </div>
-        <FContentText type="negative" text={'.freelog.com'}/>
-      </div>
+        <div style={{width: 18}}>
+          {nodes.domainVerify === 1 && <FLoading/>}
+          {nodes.domainVerify === 2 && !nodes.domainError && <FCheck/>}
+        </div>
+      </Space>
       <pre className={styles.errorTip}>{nodes.domainError}</pre>
-      <div className={styles.name}>
-        <FContentText type="negative" text={'节点名称'}/>
-        <div className={styles.inputWrap}>
-          <Input
-            value={nodes.nodeName}
-            onChange={(e) => dispatch<ChangeAction>({
-              type: 'nodes/change',
-              payload: {
-                nodeName: e.target.value,
-              }
-            })}
-            className={styles.input}
-            placeholder={'输入节点名称'}
-          />
+      <Space size={10}>
+        <div className={styles.name}>
+          <FContentText type="negative" text={'节点名称'}/>
+          <div className={styles.inputWrap}>
+            <FInput
+              value={nodes.nodeName}
+              debounce={300}
+              onDebounceChange={(value) => dispatch<OnChangeNameAction>({
+                type: 'nodes/onChangeName',
+                payload: value,
+              })}
+              className={styles.input}
+              placeholder={'输入节点名称'}
+            />
+          </div>
         </div>
-      </div>
+        <div style={{width: 18}}>
+          {nodes.nameVerify === 1 && <FLoading/>}
+          {nodes.nameVerify === 2 && !nodes.nameError && <FCheck/>}
+        </div>
+      </Space>
       <pre className={styles.errorTip}>{nodes.nameError}</pre>
       <FNormalButton
         className={styles.button}
+        disabled={nodes.domainVerify !== 2 || !!nodes.domainError
+        || nodes.nameVerify !== 2 || !!nodes.nameError}
         onClick={() => dispatch<CreateNodeAction>({
           type: 'nodes/createNode',
         })}
