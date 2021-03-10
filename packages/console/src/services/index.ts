695adb6d9f634fb0b60bc5e4eab7b1436604f43b
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-03-10 11:11:59 +0800

    Update index.ts

--- packages/console/src/services/index.ts
+++ packages/console/src/services/index.ts
@@ -1,9 +1,17 @@
 import * as Node from './nodes';
 import * as Exhibit from './presentables';
 import * as Storage from './storages';
+import * as Collection from './collections';
+import * as Resource from './resources';
+import * as User from './user';
+import * as InformalNode from './informalNodes';
 
 export const ApiServer = {
   Node,
   Exhibit,
   Storage,
+  Collection,
+  Resource,
+  User,
+  InformalNode,
 };
