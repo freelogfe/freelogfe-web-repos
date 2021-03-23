93a495bc7c609ae420c0f6c3201b10c994825197
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-03-23 11:26:36 +0800

    update

--- packages/console/src/pages/resource/$id/index/Viewport/index.tsx
+++ packages/console/src/pages/resource/$id/index/Viewport/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {FTitleText} from "@/components/FText";
 import styles from "./index.less";
-import FAntvG6 from "@/components/FAntvG6";
+import {FAntvG6DependencyGraph} from "@/components/FAntvG6";
 
 function Viewport() {
   return (<>
@@ -10,28 +10,78 @@ function Viewport() {
       <FTitleText text={'相关视图'} type={'h4'}/>
       <div style={{height: 20}}/>
       <div className={styles.content}>
-        <FAntvG6 data={{
-          // 点集
-          nodes: [
+        <FAntvG6DependencyGraph
+          nodes={[
             {
-              id: 'node1', // String，该节点存在则必须，节点的唯一标识
-              x: 100, // Number，可选，节点位置的 x 值
-              y: 200, // Number，可选，节点位置的 y 值
+              id: 'node1', // String，该节点存在则必须，节点的唯一标识\
+              name: 'node1',
+              resourceType: 'markdown',
+              version: '1.1.1',
             },
             {
-              id: 'node2', // String，该节点存在则必须，节点的唯一标识
-              x: 300, // Number，可选，节点位置的 x 值
-              y: 200, // Number，可选，节点位置的 y 值
+              id: 'node2',
+              name: 'node2',
+              resourceType: 'json',
+              version: '1.0.0',
             },
-          ],
-          // 边集
-          edges: [
+            {
+              id: 'node3',
+              name: 'node3',
+              resourceType: 'image',
+              version: '0.1.0',
+            },
+            {
+              id: 'node4',
+              name: 'node4',
+              resourceType: 'theme',
+              version: '1.0.1',
+            },
+            {
+              id: 'node5',
+              name: 'node5',
+              resourceType: 'txt',
+              version: '3.0.0',
+            },
+            {
+              id: 'node6',
+              name: 'node6',
+              resourceType: 'txt',
+              version: '3.0.0',
+            },
+            {
+              id: 'node7',
+              name: 'node7',
+              resourceType: 'txt',
+              version: '3.0.0',
+            },
+          ]}
+          edges={[
             {
               source: 'node1', // String，必须，起始点 id
               target: 'node2', // String，必须，目标点 id
             },
-          ],
-        }}/>
+            {
+              source: 'node1', // String，必须，起始点 id
+              target: 'node3', // String，必须，目标点 id
+            },
+            {
+              source: 'node3', // String，必须，起始点 id
+              target: 'node4', // String，必须，目标点 id
+            },
+            {
+              source: 'node3', // String，必须，起始点 id
+              target: 'node5', // String，必须，目标点 id
+            },
+            {
+              source: 'node5', // String，必须，起始点 id
+              target: 'node6', // String，必须，目标点 id
+            },
+            {
+              source: 'node6', // String，必须，起始点 id
+              target: 'node7', // String，必须，目标点 id
+            },
+          ]}
+        />
       </div>
     </div>
     <div style={{height: 20}}/>
