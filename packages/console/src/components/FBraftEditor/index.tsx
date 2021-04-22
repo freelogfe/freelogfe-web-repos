e0eb97e90bfc9e0be568730d0dd8fd6d45ad081d
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-04-22 11:20:55 +0800

    Update index.tsx

--- packages/console/src/components/FBraftEditor/index.tsx
+++ packages/console/src/components/FBraftEditor/index.tsx
@@ -36,11 +36,6 @@ function FBraftEditor({global, value, onChange, ...props}: FBraftEditorProps) {
       ]}
       media={{
         async uploadFn(fileParams) {
-          // console.log(fileParams, 'fileParams1!@#$!@#$@!#$');
-          // if (fileParams.file.type !== 'image/gif' && fileParams.file.type !== 'image/png' && fileParams.file.type !== 'image/jpeg') {
-          //   fMessage(FUtil.I18n.message('limit_resource_image_format'), 'error');
-          //   return false;
-          // }
           const params: Parameters<typeof FApiServer.Storage.uploadImage>[0] = {
             file: fileParams.file,
           };
