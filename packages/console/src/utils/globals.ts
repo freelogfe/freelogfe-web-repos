dada50fd6e0f21ebabeca693f8fb05479d364b91
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-04-30 15:33:08 +0800

    Update globals.ts

--- packages/console/src/utils/globals.ts
+++ packages/console/src/utils/globals.ts
@@ -3,5 +3,3 @@
 export const resourceTypes: string[] = ['json', 'widget', 'image', 'audio', 'markdown', 'theme', 'reveal_slide', 'license', 'video', 'catalog'];
 
 export const pageSize: number = 100;
-
-
