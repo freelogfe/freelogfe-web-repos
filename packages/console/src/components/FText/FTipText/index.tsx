f3bbd88150174ce60ec45a4c547b6478bd447868
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-05-10 16:13:57 +0800

    Update index.tsx

--- packages/console/src/components/FText/FTipText/index.tsx
+++ packages/console/src/components/FText/FTipText/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import styles from './index.less';
+import {CSSProperties} from "react";
+import shared from "@/components/FText/shared.less";
 
-// first second third
 interface FTipTextProps {
   type?: 'first' | 'second' | 'third';
   text: string | number;
+  className?: string;
+  style?: CSSProperties;
+  singleRow?: boolean;
 }
 
-function FTipText({type = 'first', text}: FTipTextProps) {
-  return (<div className={styles[type] + ' ' + styles.text}>{text}</div>);
+function FTipText({type = 'first', text, className = '', style, singleRow}: FTipTextProps) {
+  const singleRowClassName = singleRow ? shared.singleRow : '';
+  return (<div
+    className={[singleRowClassName, styles[type], styles.text, className].join(' ')}
+    style={style}
+  >{text}</div>);
 }
 
 export default FTipText;
