7c5be7603de7cf406d3b90ad5c36ee688ba72baf
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-05-12 15:15:13 +0800

    FRW-158 bug fixed

--- packages/console/src/pages/result/resource/version/create/success/$id/$version/index.tsx
+++ packages/console/src/pages/result/resource/version/create/success/$id/$version/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styles from './index.less';
 import FLayout from '@/layouts/FLayout';
 import {FTipText} from '@/components/FText';
-import {FTextBtn} from '@/components/FButton';
+import {FRectBtn, FTextBtn} from '@/components/FButton';
 import {withRouter, router} from "umi";
 import FCenterLayout from "@/layouts/FCenterLayout";
 import * as AHooks from 'ahooks';
@@ -11,92 +11,101 @@ import {connect, Dispatch} from 'dva';
 import RouterTypes from "umi/routerTypes";
 import FUtil from "@/utils";
 import {RouteComponentProps} from 'react-router';
+import {FApiServer} from "@/services";
 
 interface SuccessProps extends RouteComponentProps<{
   id: string;
   version: string;
 }> {
-  // match: {
-  //   params: {
-  //
-  //
-  //   };
-  //   url: string;
-  // };
   dispatch: Dispatch;
 }
 
-// let clear: any = null;
-// let timeV: number = 3;
-
-function Success({match, route, dispatch}: SuccessProps & RouterTypes) {
+function Success({match, dispatch}: SuccessProps) {
 
   const [count, setCount] = React.useState<number>(3);
+  // 0：初始 1：第一个版本且无策略 2：非第一个版本或有策略
+  const [gotoState, setGotoState] = React.useState<0 | 1 | 2>(0);
+
   AHooks.useInterval(() => {
     const c = count - 1;
     setCount(c);
     if (c === 0) {
-      goto();
+      gotoVersionInfo();
     }
-  }, 1000);
+  }, gotoState === 2 ? 1000 : null);
 
-  React.useEffect(() => {
-    dispatch<ChangeAction>({
-      type: 'global/change',
-      payload: {
-        route: route,
-      },
-    });
-  }, [route]);
+  AHooks.useMount(async () => {
+    const params: Parameters<typeof FApiServer.Resource.info>[0] = {
+      resourceIdOrName: match.params.id,
+    };
 
-  // console.log(match, 'SSSSSSAAAAA');
-  // const [time, setTime] = React.useState<number>(timeV);
+    const {data} = await FApiServer.Resource.info(params);
+    // console.log(data, 'DDDDTTTTAAAAA');
 
-  // React.useEffect(() => {
-  //   clear = setInterval(() => {
-  //     console.log(time, 'EEEEE');
-  //     timeV--;
-  //     setTime(timeV);
-  //     if (timeV === 0) {
-  //       goto();
-  //     }
-  //
-  //   }, 1000);
-  //   return function () {
-  //     clearInterval(clear);
-  //     clear = null;
-  //     timeV = 3;
-  //   }
-  // }, [goto, time]);
+    if (data.resourceVersions.length === 1 && data.policies.length === 0) {
+      setGotoState(1);
+    } else {
+      setGotoState(2);
+    }
+  });
 
-  function goto() {
-
-    // return router.replace(match.url.replace('/success', ''));
+  function gotoVersionInfo() {
     return router.replace(FUtil.LinkTo.resourceVersion({
       resourceID: match.params.id,
       version: match.params.version,
     }));
   }
 
+  function gotoAuth() {
+    return router.replace(FUtil.LinkTo.resourceAuth({
+      resourceID: match.params.id,
+    }));
+  }
+
   return (<FCenterLayout>
     <div style={{height: 100}}/>
     <div className={styles.modal}>
-      <i className={'freelog fl-icon-shenqingchenggong'}/>
-      <div style={{height: 20}}/>
-      {/*<FTipText type={'secondary'} text={`版本 ${match.params.$version} 创建成功`}/>*/}
-      <FTipText
-        type="second"
-        text={FUtil.I18n.message('version_created_successfully', {VersionNumber: match.params.version})}
-      />
+      {
+        gotoState !== 0 && (<>
+          <i className={'freelog fl-icon-shenqingchenggong'}/>
+          <div style={{height: 20}}/>
+          <FTipText
+            type="second"
+            text={FUtil.I18n.message('version_created_successfully', {VersionNumber: match.params.version})}
+          />
+        </>)
+      }
+
       <div style={{height: 40}}/>
-      <div className={styles.goto}>
-        <FTipText type="third" text={FUtil.I18n.message('jump_to_version_edit', {timer: count})}/>
-        <div style={{width: 10}}/>
-        <FTextBtn
-          // theme={'primary'}
-          onClick={goto}
-        >{FUtil.I18n.message('jump_now')}</FTextBtn>
-      </div>
+
+      {
+        gotoState === 1 && (<div className={styles.goto1}>
+          <FTipText type="third" text={'未添加策略的资源不会出现在资源市场中'}/>
+          <div style={{height: 30}}/>
+          <FRectBtn
+            onClick={() => {
+              gotoAuth();
+            }}
+            style={{padding: '0 20px'}}
+          >立即添加授权策略</FRectBtn>
+          <div style={{height: 15}}/>
+          <FTextBtn
+            onClick={() => {
+              gotoVersionInfo();
+            }}
+          >暂不添加</FTextBtn>
+        </div>)
+      }
+      {
+        gotoState === 2 && (<div className={styles.goto2}>
+          <FTipText type="third" text={FUtil.I18n.message('jump_to_version_edit', {timer: count})}/>
+          <div style={{width: 10}}/>
+          <FTextBtn
+            // theme={'primary'}
+            onClick={gotoVersionInfo}
+          >{FUtil.I18n.message('jump_now')}</FTextBtn>
+        </div>)
+      }
     </div>
   </FCenterLayout>)
 }
