d37458b8c2522fb6c248c6d479f3bc6fde638896
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-05-20 17:57:53 +0800

    Update index.tsx

--- packages/console/src/components/FButton/index.tsx
+++ packages/console/src/components/FButton/index.tsx
@@ -1,10 +1,8 @@
-// import FTextButton from './FTextButton';
 import FRectBtn from './FRectBtn';
 import FCircleBtn from './FCircleBtn';
 import FTextBtn from './FTextBtn';
 
 export {
-  // FTextButton,
   FRectBtn,
   FCircleBtn,
   FTextBtn,
