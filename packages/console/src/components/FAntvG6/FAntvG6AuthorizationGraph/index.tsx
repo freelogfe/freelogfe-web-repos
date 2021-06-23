46366a81c19de1c82c76da4917a5ef7a7e2eb9e5
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-06-23 10:38:28 +0800

    update

--- packages/console/src/components/FAntvG6/FAntvG6AuthorizationGraph/index.tsx
+++ packages/console/src/components/FAntvG6/FAntvG6AuthorizationGraph/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from './index.less';
 import G6 from '@antv/g6';
-import {GraphData} from "@antv/g6/lib/types";
+import {GraphData} from "@antv/g6/lib";
 import {FTipText} from "@/components/FText";
 import {FServiceAPI} from '@freelog/tools-lib';
 
