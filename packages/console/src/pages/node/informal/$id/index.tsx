2988a25fea28a9f86165bda8863881d4f7118c65
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-07-15 14:29:43 +0800

    Update index.tsx

--- packages/console/src/pages/node/informal/$id/index.tsx
+++ packages/console/src/pages/node/informal/$id/index.tsx
@@ -15,8 +15,8 @@ import {RouteComponentProps} from "react-router";
 import FLink from "@/components/FLink";
 import {FUtil} from '@freelog/tools-lib';
 import useUrlState from '@ahooksjs/use-url-state';
-import AddInformExhibitDrawer from "@/pages/node/informal/$id/containers/AddInformExhibitDrawer";
-import FReplaceModal from "@/pages/node/informal/$id/containers/FReplaceModal";
+import AddInformExhibitDrawer from "./containers/AddInformExhibitDrawer";
+import FReplaceModal from "./containers/FReplaceModal";
 import * as AHooks from 'ahooks';
 
 interface InformalNodeProps extends RouteComponentProps<{ id: string }> {
@@ -71,93 +71,10 @@ function InformalNode({match, dispatch, informalNodeManagerPage}: InformalNodePr
         </div>
       </div>
     </div>
-
-    {/*<FModal*/}
-    {/*  title={'提示'}*/}
-    {/*  visible={!!informalNodeManagerPage.addOrReplaceCodeExecutionErrorMessages}*/}
-    {/*  onOk={() => {*/}
-    {/*    onChange({addOrReplaceCodeExecutionErrorMessages: null});*/}
-    {/*  }}*/}
-    {/*  cancelButtonProps={{*/}
-    {/*    style: {*/}
-    {/*      display: 'none',*/}
-    {/*    }*/}
-    {/*  }}*/}
-    {/*>*/}
-    {/*  <div className={styles.codeExecutionError}>*/}
-    {/*    <div className={styles.errorTitle}>校验并保存成功，但存在预执行错误。</div>*/}
-    {/*    <div style={{height: 20}}/>*/}
-    {/*    <Space className={styles.errorList} size={5} direction="vertical">*/}
-    {/*      {*/}
-    {/*        informalNodeManagerPage.addOrReplaceCodeExecutionErrorMessages?.map((cme, index) => {*/}
-    {/*          return (<div key={index} className={styles.errorListItem}>*/}
-    {/*            <div>•</div>*/}
-    {/*            <div style={{width: 5}}/>*/}
-    {/*            <div>*/}
-    {/*              <div>*/}
-    {/*                <div>错误提示：</div>*/}
-    {/*                <div>{cme.msg}</div>*/}
-    {/*              </div>*/}
-    {/*            </div>*/}
-    {/*          </div>);*/}
-    {/*        })*/}
-    {/*      }*/}
-    {/*    </Space>*/}
-    {/*  </div>*/}
-    {/*</FModal>*/}
-
+    
     <AddInformExhibitDrawer/>
 
-    {/*{console.log(informalNodeManagerPage.nodeID, 'informalNodeManagerPage.nodeIDinformalNodeManagerPage.nodeID')}*/}
-    <FReplaceModal
-      // nodeID={informalNodeManagerPage.nodeID}
-      // isTheme={informalNodeManagerPage.showPage === 'theme'}
-      // visible={informalNodeManagerPage.replaceModalVisible}
-      // onCancel={() => {
-      //   onChange({
-      //     replaceModalVisible: false
-      //   });
-      //   dispatch<ReplaceInformExhibitInitModelStatesAction>({
-      //     type: 'replaceInformExhibit/initModelStates',
-      //   });
-      // }}
-      // onConfirm={(value) => {
-      //   // console.log(value, '@#ASDFASDfloj98pvaluevaluevalue');
-      //   const {rules}: { rules: any[] } = compile(informalNodeManagerPage.ruleText);
-      //   // console.log(rules, '@#XDFZFSWEAfdjs9flkasjd');
-      //
-      //   for (const v of value) {
-      //     const rule = rules.find((r) => v.exhibitName === r.exhibitName);
-      //     if (rule) {
-      //       let replaces = rule.replaces || [];
-      //       rule.replaces = [
-      //         ...replaces,
-      //         v,
-      //       ];
-      //     } else {
-      //       rules.push({
-      //         operation: 'alter',
-      //         exhibitName: v.exhibitName,
-      //         replaces: [v]
-      //       });
-      //     }
-      //   }
-      //   // console.log(rules, 'nowRules0923jlkfds()UOIJ');
-      //   dispatch<SaveDataRulesAction>({
-      //     type: 'informalNodeManagerPage/saveDataRules',
-      //     payload: {
-      //       type: 'replace',
-      //       data: rules,
-      //     },
-      //   });
-      //   onChange({
-      //     replaceModalVisible: false
-      //   });
-      //   dispatch<ReplaceInformExhibitInitModelStatesAction>({
-      //     type: 'replaceInformExhibit/initModelStates',
-      //   });
-      // }}
-    />
+    <FReplaceModal/>
   </>);
 }
 
