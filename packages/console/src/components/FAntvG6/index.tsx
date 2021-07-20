d21e9115e7c5a1a21e3021a38b1d8e9c91e0651e
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2021-07-20 11:14:11 +0800

    Update index.tsx

--- packages/console/src/components/FAntvG6/index.tsx
+++ packages/console/src/components/FAntvG6/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import styles from './index.less';
-import G6 from '@antv/g6';
 
 import FViewportTabs from './FViewportTabs';
 import FAntvG6DependencyGraph from './FAntvG6DependencyGraph';
