f84525861d7a51ba50b25c91ff103760fff5e950
liu-kai-github <liukaiemailchina@gmail.com>
2021-07-28 14:31:06 +0800

    Update index.tsx

--- packages/console/src/pages/node/informal/$id/Sider/index.tsx
+++ packages/console/src/pages/node/informal/$id/Sider/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import styles from './index.less';
-import {Space} from "antd";
-import FCopyToClipboard from "@/components/FCopyToClipboard";
-import {connect, Dispatch} from 'dva';
-import {ConnectState, InformalNodeManagerPageModelState} from "@/models/connect";
-import {router, withRouter} from "umi";
-import {ChangeAction, FetchNodeInfoAction, OnMountPageSiderAction} from "@/models/informalNodeManagerPage";
-import {RouteComponentProps} from "react-router";
-import {FUtil} from "@freelog/tools-lib";
-import FUtil1 from "@/utils";
-import * as AHooks from "ahooks";
+import { Space } from 'antd';
+import FCopyToClipboard from '@/components/FCopyToClipboard';
+import { connect, Dispatch } from 'dva';
+import { ConnectState, InformalNodeManagerPageModelState } from '@/models/connect';
+import { router, withRouter } from 'umi';
+import { OnMountPageSiderAction } from '@/models/informalNodeManagerPage';
+import { RouteComponentProps } from 'react-router';
+import { FUtil } from '@freelog/tools-lib';
+import FUtil1 from '@/utils';
+import * as AHooks from 'ahooks';
 
 interface SiderProps extends RouteComponentProps<{ id: string }> {
   dispatch: Dispatch;
@@ -17,7 +17,7 @@ interface SiderProps extends RouteComponentProps<{ id: string }> {
   informalNodeManagerPage: InformalNodeManagerPageModelState;
 }
 
-function Sider({match, dispatch, informalNodeManagerPage}: SiderProps) {
+function Sider({ match, dispatch, informalNodeManagerPage }: SiderProps) {
 
   AHooks.useMount(() => {
     dispatch<OnMountPageSiderAction>({
@@ -32,42 +32,25 @@ function Sider({match, dispatch, informalNodeManagerPage}: SiderProps) {
 
   });
 
-  // React.useEffect(() => {
-  //   initData();
-  // }, []);
-  //
-  // async function initData() {
-  //   await dispatch<ChangeAction>({
-  //     type: 'informalNodeManagerPage/change',
-  //     payload: {
-  //       nodeID: Number(match.params.id),
-  //     },
-  //   });
-  //
-  //   await dispatch<FetchNodeInfoAction>({
-  //     type: 'informalNodeManagerPage/fetchNodeInfo',
-  //   });
-  // }
-
   return (<>
-    <div style={{height: 35}}/>
+    <div style={{ height: 35 }} />
     <div className={styles.title}>
       <label>test</label>
       &nbsp;&nbsp;
       <span>{informalNodeManagerPage.nodeName}</span>
     </div>
-    <div style={{height: 15}}/>
+    <div style={{ height: 15 }} />
     <Space size={5} className={styles.url}>
       <a onClick={() => {
         window.open(informalNodeManagerPage.testNodeUrl);
       }}>{informalNodeManagerPage.testNodeUrl.replace(/http(s)?:\/\//, '')}</a>
       <FCopyToClipboard
         text={informalNodeManagerPage.testNodeUrl}
-        // title={'复制测试节点地址'}
+        iconStyle={{fontSize: 14}}
         title={FUtil1.I18n.message('tip_copy_node_domain')}
       />
     </Space>
-    <div style={{height: 35}}/>
+    <div style={{ height: 35 }} />
     <div className={styles.navs}>
       <div
         className={informalNodeManagerPage.showPage === 'exhibit' ? styles.activated : ''}
@@ -92,12 +75,6 @@ function Sider({match, dispatch, informalNodeManagerPage}: SiderProps) {
       <div
         className={informalNodeManagerPage.showPage === 'mappingRule' ? styles.activated : ''}
         onClick={() => {
-          // dispatch<ChangeAction>({
-          //   type: 'informalNodeManagerPage/change',
-          //   payload: {
-          //     showPage: 'mappingRule',
-          //   },
-          // });
           router.push(FUtil.LinkTo.informNodeManagement({
             nodeID: informalNodeManagerPage.nodeID,
             showPage: 'mappingRule',
@@ -109,6 +86,6 @@ function Sider({match, dispatch, informalNodeManagerPage}: SiderProps) {
   </>);
 }
 
-export default withRouter(connect(({informalNodeManagerPage}: ConnectState) => ({
+export default withRouter(connect(({ informalNodeManagerPage }: ConnectState) => ({
   informalNodeManagerPage,
 }))(Sider));
