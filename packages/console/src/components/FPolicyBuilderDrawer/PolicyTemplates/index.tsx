7320550044cb1b7dd684f11e47363e4116227c17
liu-kai-github <liukaiemailchina@gmail.com>
2021-08-11 11:15:02 +0800

    模板基本弄好

--- packages/console/src/components/FPolicyBuilderDrawer/PolicyTemplates/index.tsx
+++ packages/console/src/components/FPolicyBuilderDrawer/PolicyTemplates/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import styles from './index.less';
-import {FContentText} from '@/components/FText';
-import {FDown} from "@/components/FIcons";
-import {Space} from "antd";
-import FUp from "@/components/FIcons/FUp";
+import { FContentText, FTitleText } from '@/components/FText';
+import { FDown } from '@/components/FIcons';
+import { Space } from 'antd';
+import FUp from '@/components/FIcons/FUp';
+import FCodeFormatter from '@/components/FCodeFormatter';
+import { FRectBtn, FTextBtn } from '@/components/FButton';
 
 interface PolicyTemplatesProps {
-  onSelect?({title, text}: { title: string, text: string }): void;
+  onSelect?({ title, text }: { title: string, text: string }): void;
 }
 
 const text1: string = `for public
@@ -19,67 +21,104 @@ finish:
 const text2: string = `for public
 
 initial:
-  ~freelog.TransactionEvent("10","self.account") => auth // 设置价格
+  ~freelog.TransactionEvent("10","self.account") => auth
 auth[active]:
-  ~freelog.RelativeTimeEvent("1","month")  =>  finish // 设置订阅周期
+  ~freelog.RelativeTimeEvent("1","month")  =>  finish
 finish:
   terminate`;
 
 const policiesText: string[] = [];
 
-function PolicyTemplates({onSelect}: PolicyTemplatesProps) {
-  return (<Space
-    direction="vertical"
-    className={styles.styles}
-    size={15}
-  >
+function PolicyTemplates({ onSelect }: PolicyTemplatesProps) {
+  return (<div>
     <PolicyTemplate
       text={text1}
-      title={'免费策略'}
-      onSelect={() => onSelect && onSelect({text: text1, title: '免费策略'})}
+      title={'免费订阅（包月）'}
+      translation={'免费获取授权一个月'}
+      onSelect={() => {
+        onSelect && onSelect({ text: text1, title: '免费策略' });
+      }}
     />
+    <div style={{ height: 20 }} />
     <PolicyTemplate
       text={text2}
-      title={'收费策略'}
-      onSelect={() => onSelect && onSelect({text: text2, title: '收费策略'})}
+      title={'付费订阅（包月）'}
+      translation={'支付 10枚 羽币，可获取一个月的授权'}
+      onSelect={() => {
+        onSelect && onSelect({ text: text2, title: '收费策略' });
+      }}
     />
-  </Space>);
+  </div>);
 }
 
 export default PolicyTemplates;
 
 interface PolicyTemplateProps {
   title: string;
+  translation: string;
   text: string;
 
   onSelect?(): void;
 }
 
-function PolicyTemplate({text, title, onSelect}: PolicyTemplateProps) {
+function PolicyTemplate({ text, title, translation, onSelect }: PolicyTemplateProps) {
 
-  const [visibleText, setVisibleText] = React.useState<boolean>(false);
+  const [visible, setVisible] = React.useState<'none' | 'code' | 'view'>(false);
 
-  return (<div>
-    <div
-      className={styles.header}
-      onClick={() => setVisibleText(!visibleText)}
-    >
-      <Space size={10}>
-        <FContentText text={title}/>
-        <FContentText>{visibleText ? <FUp/> : <FDown/>}</FContentText>
-      </Space>
-      <a onClick={(e) => {
-        e.stopPropagation();
-        onSelect && onSelect();
-      }}>选择</a>
+  return (<div className={styles.PolicyTemplate}>
+    <div className={styles.header}>
+      <FTitleText type='h1' text={title} />
+      <FRectBtn type='primary' size='small'>选择</FRectBtn>
+    </div>
+    <div style={{ height: 15 }} />
+    <div className={styles.translation}>
+      <FContentText text={translation} />
+    </div>
+    <div style={{ height: 15 }} />
+    <div className={styles.navs}>
+      {
+        visible === 'code'
+          ? (<FTextBtn
+            type='primary'
+            onClick={() => {
+              setVisible('none');
+            }}>隐藏代码</FTextBtn>)
+          : (<FTextBtn
+            type='primary'
+            onClick={() => {
+              setVisible('code');
+            }}>显示代码</FTextBtn>)
+      }
+
+      <div style={{ width: 20 }} />
+
+      {
+        visible === 'view'
+          ? (<FTextBtn
+            type='primary'
+            onClick={() => {
+              setVisible('none');
+            }}>隐藏状态机视图</FTextBtn>)
+          : (<FTextBtn
+            type='primary'
+            onClick={() => {
+              setVisible('view');
+            }}>显示状态机视图</FTextBtn>)
+      }
+
     </div>
 
     {
-      visibleText && (<>
-        <div style={{height: 5}}/>
-        <div className={styles.content}>
-          <pre>{text}</pre>
-        </div>
+      visible === 'code' && (<>
+        <div style={{ height: 15 }} />
+        <FCodeFormatter code={text} />
+      </>)
+    }
+
+    {
+      visible === 'view' && (<>
+        <div style={{ height: 15 }} />
+        <div style={{ height: 200 }} />
       </>)
     }
 
