3aa7ae03ed2491b70bb91d7605ae43abb6b2b8e1
liu-kai-github <liukaiemailchina@gmail.com>
2021-08-23 17:33:38 +0800

    update

--- packages/console/src/pages/resource/details/$id/SignPage/ResourcesAndPolicies/index.tsx
+++ packages/console/src/pages/resource/details/$id/SignPage/ResourcesAndPolicies/index.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import styles from './index.less';
-import {FContentText, FTitleText} from '@/components/FText';
-import {Drawer, Space} from 'antd';
-import {connect, Dispatch} from 'dva';
-import {ConnectState, MarketResourcePageModelState} from '@/models/connect';
+import { FContentText, FTitleText } from '@/components/FText';
+import { Drawer, Space } from 'antd';
+import { connect, Dispatch } from 'dva';
+import { ConnectState, MarketResourcePageModelState } from '@/models/connect';
+import FPolicyDisplay from '@/components/FPolicyDisplay';
 
 interface ResourcesAndPoliciesProps {
   dispatch: Dispatch;
   marketResourcePage: MarketResourcePageModelState;
 }
 
-function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPoliciesProps) {
+function ResourcesAndPolicies({ dispatch, marketResourcePage }: ResourcesAndPoliciesProps) {
 
   const showResource = marketResourcePage.signResources;
 
@@ -20,7 +21,7 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
 
   return (<>
     <div className={styles.smallTitle}>当前资源</div>
-    <div style={{height: 10}}/>
+    <div style={{ height: 10 }} />
     {
       showResource
         .filter((r, i: number) => i === 0)
@@ -31,11 +32,11 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
         >
           <div>
             <FContentText
-              type="highlight"
+              type='highlight'
               text={r.name}
             />
-            <div style={{height: 5}}/>
-            <FContentText type="additional2" text={'audio'}/>
+            <div style={{ height: 5 }} />
+            <FContentText type='additional2' text={'audio'} />
           </div>
           <div className={styles.resourcePolicies}>
             {
@@ -48,13 +49,13 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
 
     {
       showResource.length > 1 && <>
-        <div style={{height: 20}}/>
+        <div style={{ height: 20 }} />
         <div className={styles.smallTitle}>基础上抛</div>
-        <div style={{height: 10}}/>
+        <div style={{ height: 10 }} />
         <Space
-          direction="vertical"
+          direction='vertical'
           size={10}
-          style={{width: '100%'}}
+          style={{ width: '100%' }}
         >
           {
             showResource
@@ -66,11 +67,11 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
               >
                 <div>
                   <FContentText
-                    type="highlight"
+                    type='highlight'
                     text={r.name}
                   />
-                  <div style={{height: 5}}/>
-                  <FContentText type="additional2" text={'audio'}/>
+                  <div style={{ height: 5 }} />
+                  <FContentText type='additional2' text={'audio'} />
                 </div>
                 <div className={styles.resourcePolicies}>
                   {
@@ -93,28 +94,28 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
       // title={<span style={{fontWeight: 400}}>stefan/Smell like teen spirit</span>}
       title={null}
       width={720}
-      bodyStyle={{padding: 40}}
+      bodyStyle={{ padding: 40 }}
       onClose={() => setVisibleR('')}
     >
       <FTitleText
         text={showRInfo?.name}
-        type="h3"
+        type='h3'
       />
-      <div style={{height: 10}}/>
+      <div style={{ height: 10 }} />
       <FContentText
-        type="additional1"
+        type='additional1'
         text={showRInfo?.type}
       />
-      <div style={{height: 50}}/>
+      <div style={{ height: 50 }} />
       <FTitleText
-        type="h3"
+        type='h3'
         text={'已选策略'}
       />
-      <div style={{height: 30}}/>
+      <div style={{ height: 30 }} />
       <Space
-        direction="vertical"
+        direction='vertical'
         size={20}
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
       >
         {
           showRInfo?.contracts
@@ -123,12 +124,14 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
               className={styles.policy}
               key={rp.id}
             >
-              <FContentText
-                text={rp.name}
-                type="highlight"
-              />
-              <div style={{height: 15}}/>
-              <pre>{rp.text}</pre>
+              <div style={{ padding: '10px 20px 0' }}>
+                <FContentText
+                  text={rp.name}
+                  type='highlight'
+                />
+              </div>
+              {/*<pre>{rp.text}</pre>*/}
+              <FPolicyDisplay code={rp.text} containerHeight={300} />
             </div>))
         }
         {
@@ -138,12 +141,13 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
               className={styles.policy}
               key={rp.id}
             >
-              <FContentText
-                text={rp.name}
-                type="highlight"
-              />
-              <div style={{height: 15}}/>
-              <pre>{rp.text}</pre>
+              <div style={{ padding: '10px 20px 0' }}>
+                <FContentText
+                  text={rp.name}
+                  type='highlight'
+                />
+              </div>
+              <FPolicyDisplay code={rp.text} containerHeight={300} />
             </div>))
         }
       </Space>
@@ -151,6 +155,6 @@ function ResourcesAndPolicies({dispatch, marketResourcePage}: ResourcesAndPolici
   </>);
 }
 
-export default connect(({marketResourcePage}: ConnectState) => ({
+export default connect(({ marketResourcePage }: ConnectState) => ({
   marketResourcePage,
 }))(ResourcesAndPolicies);
