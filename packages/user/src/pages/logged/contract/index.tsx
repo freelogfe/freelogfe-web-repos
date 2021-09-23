07bd3d487b5f14ee2623864ee6755a870b198e8d
liu-kai-github <liukaiemailchina@gmail.com>
2021-09-23 15:45:53 +0800

    update

--- packages/user/src/pages/logged/contract/index.tsx
+++ packages/user/src/pages/logged/contract/index.tsx
@@ -6,7 +6,32 @@ interface ContractProps {
 }
 
 function Contract({}: ContractProps) {
-  return (<div>Contract</div>);
+  return (<div className={styles.styles}>
+    <div style={{ height: 30 }} />
+    <div className={styles.header}>
+      <a
+        className={styles.active}
+        onClick={() => {
+          // dispatch<OnChange_ShowPage_Action>({
+          //   type: 'settingPage/onChange_ShowPage',
+          //   payload: {
+          //     value: 'profile',
+          //   },
+          // });
+        }}>授权合约</a>
+      <div style={{ width: 30 }} />
+      <a
+        onClick={() => {
+          // dispatch<OnChange_ShowPage_Action>({
+          //   type: 'settingPage/onChange_ShowPage',
+          //   payload: {
+          //     value: 'security',
+          //   },
+          // });
+        }}
+        className={''}>被授权合约</a>
+    </div>
+  </div>);
 }
 
 export default Contract;
