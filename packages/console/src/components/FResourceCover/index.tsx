59664672906d7e0c56aceae8e9189edc8459b72b
liu-kai-github <liukaiemailchina@gmail.com>
2021-12-27 14:42:24 +0800

    Update index.tsx

--- packages/console/src/components/FResourceCover/index.tsx
+++ packages/console/src/components/FResourceCover/index.tsx
@@ -5,20 +5,21 @@ import FResourceStatusBadge from '../FResourceStatusBadge';
 import FCoverImage from '@/components/FCoverImage';
 
 interface FResourceCoverProps {
-  width?: number | string;
-  height?: number | string;
+  // width?: number | string;
+  // height?: number | string;
   src?: string;
   status?: 'online' | 'offline' | 'unreleased';
   children?: React.ReactNode | React.ReactNodeArray;
 }
 
-export default function({ width, height, src, status, children }: FResourceCoverProps) {
+function FResourceCover({ src, status, children }: FResourceCoverProps) {
   return (<div className={styles.Cover}>
     {src && (<FCoverImage src={src} width={200} />)}
     {children}
     <div className={styles.Status}>
       <FResourceStatusBadge status={status} />
     </div>
-
   </div>);
 }
+
+export default FResourceCover;
