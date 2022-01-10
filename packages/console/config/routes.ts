1a50800eef8ff507125d39e499dffcf1759d53ff
liu-kai-github <liukaiemailchina@gmail.com>
2022-01-10 17:55:42 +0800

    update

--- packages/console/config/routes.ts
+++ packages/console/config/routes.ts
@@ -1,23 +1,23 @@
 export default [
-  {exact: true, path: '/ui-example', component: '../pages/ui-example'},
+  { exact: true, path: '/ui-example', component: '../pages/ui-example' },
   {
     path: '/',
     component: '../layouts/FLayout',
     routes: [
-      {exact: true, path: '.', redirect: '/market'},
+      { exact: true, path: '.', redirect: '/market' },
       {
         path: 'market',
         routes: [
-          {exact: true, path: '.', component: '../pages/market/index', title: '市场资源 - Freelog'},
-          {exact: true, path: 'example', component: '../pages/market/index', title: '示例节点 - Freelog'},
-          {component: '../pages/exception/404'},
+          { exact: true, path: '.', component: '../pages/market/index', title: '市场资源 - Freelog' },
+          { exact: true, path: 'example', component: '../pages/market/index', title: '示例节点 - Freelog' },
+          { component: '../pages/exception/404' },
         ],
       },
-      {exact: true, path: 'storage', component: '../pages/storage', title: '存储空间 - Freelog'},
+      { exact: true, path: 'storage', component: '../pages/storage', title: '存储空间 - Freelog' },
       {
         path: 'node',
         routes: [
-          {exact: true, path: 'creator', component: '../pages/node/creator', title: '节点创建 - Freelog'},
+          { exact: true, path: 'creator', component: '../pages/node/creator', title: '节点创建 - Freelog' },
           {
             path: 'formal/:id',
             exact: true,
@@ -33,24 +33,34 @@ export default [
           {
             path: 'exhibit',
             routes: [
-              {path: 'formal/:id', exact: true, component: '../pages/node/exhibit/formal/$id', title: '展品管理 - Freelog'},
-              {path: 'informal/:id', exact: true, component: '../pages/node/exhibit/informal/$id', title: '测试展品管理 - Freelog'},
-              {component: '../pages/exception/404'},
-            ]
+              {
+                path: 'formal/:id',
+                exact: true,
+                component: '../pages/node/exhibit/formal/$id',
+                title: '展品管理 - Freelog',
+              },
+              {
+                path: 'informal/:id',
+                exact: true,
+                component: '../pages/node/exhibit/informal/$id',
+                title: '测试展品管理 - Freelog',
+              },
+              { component: '../pages/exception/404' },
+            ],
           },
-          {component: '../pages/exception/404'},
-        ]
+          { component: '../pages/exception/404' },
+        ],
       },
       {
         path: 'resource',
         routes: [
-          {exact: true, path: '.', redirect: '/resource/list'},
-          {exact: true, path: 'list', component: '../pages/resource/list', title: '我的资源 - Freelog'},
-          {exact: true, path: 'collect', component: '../pages/resource/list', title: '我的收藏 - Freelog'},
-          {exact: true, path: 'creator', component: '../pages/resource/creator', title: '创建资源 - Freelog'},
-          {exact: true, path: 'details/:id', component: '../pages/resource/details/$id', title: '市场资源 - Freelog'},
-          {exact: true, path: 'info/:id', component: '../pages/resource/info/$id', title: '资源信息 - Freelog'},
-          {exact: true, path: 'auth/:id', component: '../pages/resource/auth/$id', title: '授权信息 - Freelog'},
+          { exact: true, path: '.', redirect: '/resource/list' },
+          { exact: true, path: 'list', component: '../pages/resource/list', title: '我的资源 - Freelog' },
+          { exact: true, path: 'collect', component: '../pages/resource/list', title: '我的收藏 - Freelog' },
+          { exact: true, path: 'creator', component: '../pages/resource/creator', title: '创建资源 - Freelog' },
+          { exact: true, path: 'details/:id', component: '../pages/resource/details/$id', title: '市场资源 - Freelog' },
+          { exact: true, path: 'info/:id', component: '../pages/resource/info/$id', title: '资源信息 - Freelog' },
+          { exact: true, path: 'auth/:id', component: '../pages/resource/auth/$id', title: '授权信息 - Freelog' },
           {
             path: 'version',
             routes: [
@@ -66,15 +76,31 @@ export default [
                 component: '../pages/resource/version/info/$id/$version',
                 title: '版本信息 - Freelog',
               },
-              {component: '../pages/exception/404'},
+              { component: '../pages/exception/404' },
             ],
           },
-          {component: '../pages/exception/404'},
-        ]
+          { component: '../pages/exception/404' },
+        ],
       },
       {
         path: 'result',
         routes: [
+          {
+            path: 'node',
+            routes: [
+              {
+                path: 'create',
+                routes: [
+                  {
+                    exact: true,
+                    path: 'success/:id',
+                    component: '../pages/result/node/create/success/$id',
+                    title: '节点创建成功 - Freelog',
+                  },
+                ],
+              },
+            ],
+          },
           {
             path: 'resource',
             routes: [
@@ -85,9 +111,9 @@ export default [
                     exact: true,
                     path: 'success/:id',
                     component: '../pages/result/resource/create/success/$id',
-                    title: '资源创建成功 - Freelog'
+                    title: '资源创建成功 - Freelog',
                   },
-                ]
+                ],
               },
               {
                 path: 'version',
@@ -99,26 +125,26 @@ export default [
                         exact: true,
                         path: 'success/:id/:version',
                         component: '../pages/result/resource/version/create/success/$id/$version',
-                        title: '版本创建成功 - Freelog'
+                        title: '版本创建成功 - Freelog',
                       },
-                    ]
+                    ],
                   },
-                ]
+                ],
               },
             ],
-          }
-        ]
+          },
+        ],
       },
       {
         path: 'exception',
         routes: [
-          {exact: true, path: '403', component: '../pages/exception/403', title: '暂无权限 - Freelog'},
-          {exact: true, path: '404', component: '../pages/exception/404', title: '未找到页面 - Freelog'},
+          { exact: true, path: '403', component: '../pages/exception/403', title: '暂无权限 - Freelog' },
+          { exact: true, path: '404', component: '../pages/exception/404', title: '未找到页面 - Freelog' },
         ],
       },
-      {component: '../pages/exception/404'},
+      { component: '../pages/exception/404' },
     ],
   },
-  {component: '../pages/exception/404'},
+  { component: '../pages/exception/404' },
 
 ];
