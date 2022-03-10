bb88fb55d5cb68136851d4fa5b406182a6fa1123
liu-kai-github <liukaiemailchina@gmail.com>
2022-03-10 16:17:56 +0800

    udpate

--- packages/console/src/pages/result/node/create/success/$id/index.tsx
+++ packages/console/src/pages/result/node/create/success/$id/index.tsx
@@ -12,6 +12,7 @@ import { FUtil } from '@freelog/tools-lib';
 import { RouteComponentProps } from 'react-router';
 import { ChangeAction as MarketChangeAction } from '@/models/marketPage';
 import FNoDataTip from '@/components/FNoDataTip';
+import FResultTip from '@/components/FResultTip';
 
 interface SuccessProps extends RouteComponentProps<{ id: string; }> {
   dispatch: Dispatch;
@@ -41,20 +42,16 @@ function Success({ match, route, dispatch }: RouterTypes & SuccessProps) {
   return (<FCenterLayout>
     <div style={{ height: 100 }} />
     <div className={styles.modal}>
-      {/*<i className={'freelog fl-icon-shenqingchenggong'}/>*/}
-      <div style={{ height: 20 }} />
-      {/*<FTipText type="second" text={FUtil1.I18n.message('msg_nodecreatedsuccessfully')}/>*/}
-      <FTipText type='first' text={'节点创建成功'} />
-      <div style={{ height: 40 }} />
-      {/*<FTipText type="third" text={FUtil1.I18n.message('cta_add_theme')}/>*/}
-      <FTipText type='second' text={'主题决定节点的整体外观和设计，你可以通过激活不同的主题来更改节点的布局、配色方案等。'} />
-      <div style={{ height: 20 }} />
-      {/*<FRectBtn onClick={goto}>{FUtil1.I18n.message('cta_btn_add_theme')}</FRectBtn>*/}
-      <FRectBtn
-        className={styles.btn}
-        size='large'
-        onClick={goto}
-      >{'添加主题'}</FRectBtn>
+      <FResultTip
+        // h1={FUtil1.I18n.message('msg_nodecreatedsuccessfully')}
+        h1={'节点创建成功'}
+        // h2={FUtil1.I18n.message('msg_nodecreatedsuccessfully')}
+        h2={'主题决定节点的整体外观和设计，你可以通过激活不同的主题来更改节点的布局、配色方案等。'}
+        // btnText={FUtil1.I18n.message('cta_btn_add_theme')}
+        btnText={'添加主题'}
+        onClickBtn={goto}
+      />
+
     </div>
   </FCenterLayout>);
 }
