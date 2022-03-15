f3677a83eda15eec5464e39c06eb74454bca0515
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2022-03-15 16:36:54 +0800

    Update index.tsx

--- packages/console/src/components/FPolicyBuilderDrawer/PolicyTemplates/index.tsx
+++ packages/console/src/components/FPolicyBuilderDrawer/PolicyTemplates/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import styles from './index.less';
-import { FContentText, FTitleText } from '../../FText';
+import { FTitleText } from '../../FText';
 import FCodeFormatter from '../../FCodeFormatter';
 import { FRectBtn, FTextBtn } from '../../FButton';
-// import { FUtil } from '@freelog/tools-lib';
 import * as AHooks from 'ahooks';
 import { policyCodeTranslationToText } from '../index';
 
