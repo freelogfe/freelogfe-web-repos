f9ec6c84c7d15025b7a8bc1649d13d329a96e426
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2022-03-18 16:44:39 +0800

    update

--- packages/console/src/components/FAntvG6/registerNode/fAuthorization.tsx
+++ packages/console/src/components/FAntvG6/registerNode/fAuthorization.tsx
@@ -1,7 +1,9 @@
-import { createNodeFromReact, Group, Rect, Text } from '@antv/g6-react-node';
+import { createNodeFromReact, Group, Image, Rect, Text } from '@antv/g6-react-node';
 import { textOverflowEllipsis } from '@/components/FAntvG6/tools';
 import G6 from '@antv/g6';
 import React from 'react';
+import img_Execute from '@/assets/execute.svg';
+import img_Gear from '@/assets/gear.svg';
 
 export interface FNode_Authorization_Resource_Values {
   resourceID: string;
@@ -55,3 +57,86 @@ const FNode_Authorization_Resource = ({ cfg = {} }) => {
 };
 
 G6.registerNode('FNode_Authorization_Resource', createNodeFromReact(FNode_Authorization_Resource));
+
+export interface FNode_Authorization_Contract_Values {
+  contractID: string;
+  contractName: string;
+  isAuth: boolean;
+  isMultiple: boolean;
+}
+
+const FNode_Authorization_Contract = ({ cfg = {} }) => {
+
+  const {
+    contractID,
+    contractName,
+    isAuth,
+    isMultiple,
+  } = (cfg as any).value as FNode_Authorization_Contract_Values;
+  return (<Group>
+    <Rect
+      draggable
+      style={{
+        fill: isAuth ? '#E5F6EF' : '#FBF5EA',
+        stroke: isAuth ? '#8FD6B8' : '#E5C78A',
+        radius: 10,
+        padding: [10, 20],
+        cursor: 'move',
+      }}
+      onClick={() => {
+        console.log('#######98ioklj');
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: 600,
+          fill: '#222',
+          padding: [3, 0],
+        }}
+        onClick={() => {
+          console.log('#####2342394ui3jk');
+        }}
+      >{textOverflowEllipsis(contractName)}</Text>
+      <Rect style={{ height: 10 }} />
+      <Rect style={{ height: 15 }} />
+      <Rect style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
+        {
+          isAuth
+            ? (<>
+              <Image style={{ width: 16, height: 16, img: img_Gear, next: 'inline' }} />
+              <Text style={{
+                fill: '#42C28C',
+                fontSize: 12,
+                fontWeight: 600,
+                lineHeight: 18,
+              }}>已授权</Text>
+            </>)
+            : (<>
+              <Image style={{ width: 16, height: 16, img: img_Execute, next: 'inline' }} />
+              <Text style={{
+                fill: '#E9A923',
+                fontSize: 12,
+                fontWeight: 600,
+                lineHeight: 18,
+              }}>待执行</Text>
+            </>)
+        }
+
+        <Text
+          style={{
+            fill: '#2E88FF',
+            fontSize: 12,
+            fontWeight: 600,
+            cursor: 'pointer',
+          }}
+          onClick={() => {
+            console.log('#####2342394ui3jk');
+          }}
+        >查看</Text>
+      </Rect>
+    </Rect>
+  </Group>);
+};
+
+G6.registerNode('FNode_Authorization_Contract', createNodeFromReact(FNode_Authorization_Contract));
--- packages/console/src/components/FAntvG6/registerNode/fRelationship.tsx
+++ packages/console/src/components/FAntvG6/registerNode/fRelationship.tsx
@@ -26,58 +26,58 @@ const FNode_Relationship_Resource = ({ cfg = {} }) => {
   return (<Group>
     <Rect
       draggable
-  style={{
-    // width: 200,
-    // height: 90,
-    fill: '#fff',
-      stroke: '#EFEFEF',
-      radius: 10,
-      padding: [10, 20],
-      cursor: 'move',
-  }}
-  onClick={() => {
-    console.log('#######98ioklj');
-  }}
+      style={{
+        // width: 200,
+        // height: 90,
+        fill: '#fff',
+        stroke: '#EFEFEF',
+        radius: 10,
+        padding: [10, 20],
+        cursor: 'move',
+      }}
+      onClick={() => {
+        console.log('#######98ioklj');
+      }}
 
->
-  <Text
-    style={{
-    fontSize: 14,
-      fontWeight: 600,
-      fill: '#222',
-      padding: [3, 0],
-  }}
-  onClick={() => {
-    console.log('#####2342394ui3jk');
-  }}
->{textOverflowEllipsis(resourceName)}</Text>
-  <Rect style={{ height: 10 }} />
-  <Text style={{
-    fontSize: 12,
-      fontWeight: 400,
-      fill: '#666',
-      padding: [3, 0],
-  }}>{resourceType} | {version}</Text>
-  {/*<Text style={{fontSize: 14, fill: '#E9A923', marginTop: 24, marginLeft: 10}}>${cfg.pending ? '未授权' : ''}${cfg.pending && cfg.exception ? ' ' : ''}${cfg.exception ? '授权异常' : ''}</Text>*/}
-  <Rect style={{ height: 15 }} />
-  <Rect style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
-  {
-    !isAuth && (<Image style={{ width: 16, height: 16, img: img, next: 'inline' }} />)
-  }
+    >
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: 600,
+          fill: '#222',
+          padding: [3, 0],
+        }}
+        onClick={() => {
+          console.log('#####2342394ui3jk');
+        }}
+      >{textOverflowEllipsis(resourceName)}</Text>
+      <Rect style={{ height: 10 }} />
+      <Text style={{
+        fontSize: 12,
+        fontWeight: 400,
+        fill: '#666',
+        padding: [3, 0],
+      }}>{resourceType} | {version}</Text>
+      {/*<Text style={{fontSize: 14, fill: '#E9A923', marginTop: 24, marginLeft: 10}}>${cfg.pending ? '未授权' : ''}${cfg.pending && cfg.exception ? ' ' : ''}${cfg.exception ? '授权异常' : ''}</Text>*/}
+      <Rect style={{ height: 15 }} />
+      <Rect style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
+        {
+          !isAuth && (<Image style={{ width: 16, height: 16, img: img, next: 'inline' }} />)
+        }
 
-  <Text
-    style={{
-    fill: '#2E88FF',
-      fontSize: 12,
-      fontWeight: 600,
-      cursor: 'pointer',
-  }}
-  onClick={() => {
-    console.log('#####2342394ui3jk');
-  }}
->查看合约</Text>
-  </Rect>
-  </Rect>
+        <Text
+          style={{
+            fill: '#2E88FF',
+            fontSize: 12,
+            fontWeight: 600,
+            cursor: 'pointer',
+          }}
+          onClick={() => {
+            console.log('#####2342394ui3jk');
+          }}
+        >查看合约</Text>
+      </Rect>
+    </Rect>
   </Group>);
 };
 
