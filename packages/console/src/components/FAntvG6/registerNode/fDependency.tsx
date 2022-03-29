ba43f32194633b01bd9cb956c870c8ea41d5b584
liu-kai-github <liukaiemailchina@gmail.com>
2022-03-29 17:26:15 +0800

    Update fDependency.tsx

--- packages/console/src/components/FAntvG6/registerNode/fDependency.tsx
+++ packages/console/src/components/FAntvG6/registerNode/fDependency.tsx
@@ -61,10 +61,85 @@ function FNode_Dependency_Resource({ value }: FNode_Dependency_Resource_Props) {
   </Group>);
 }
 
+export interface FNode_Dependency_Exhibit_Values {
+  exhibitID: string;
+  exhibitName: string;
+  nodeID: number;
+  nodeName: string;
+}
+
+interface FNode_Dependency_Exhibit_Props {
+  value: FNode_Dependency_Exhibit_Values;
+}
+
+function FNode_Dependency_Exhibit({ value }: FNode_Dependency_Exhibit_Props) {
+  const {
+    exhibitID,
+    exhibitName,
+    nodeID,
+    nodeName,
+  } = value;
+  return (<Group>
+    <Rect
+      draggable
+      style={{
+        fill: '#fff',
+        stroke: '#EFEFEF',
+        radius: 10,
+        padding: [10, 20],
+        cursor: 'move',
+      }}
+      onClick={() => {
+
+      }}
+    >
+      <Text style={{
+        fontSize: 12,
+        fontWeight: 600,
+        fill: '#7F8388',
+        padding: [3, 0],
+      }}>节点：</Text>
+      <Rect style={{ height: 10 }} />
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: 600,
+          fill: '#222',
+          padding: [3, 0],
+          cursor: 'pointer',
+        }}
+      >{textOverflowEllipsis(nodeName)}</Text>
+      <Rect style={{ height: 15 }} />
+      <Text style={{
+        fontSize: 12,
+        fontWeight: 600,
+        fill: '#7F8388',
+        padding: [3, 0],
+      }}>展品：</Text>
+      <Rect style={{ height: 10 }} />
+      <Text
+        style={{
+          fontSize: 14,
+          fontWeight: 600,
+          fill: '#222',
+          padding: [3, 0],
+          cursor: 'pointer',
+        }}
+      >{textOverflowEllipsis(exhibitName)}</Text>
+    </Rect>
+  </Group>);
+}
+
 function FNode_Dependency({ cfg = {} }: any) {
   // console.log(cfg, 'cfg@@#09soidjlfsdfjsdlkfjsdlkfjl');
   if (cfg.nodeType === 'resource') {
     return (<FNode_Dependency_Resource
+      value={cfg.value}
+    />);
+  }
+
+  if (cfg.nodeType === 'exhibit') {
+    return (<FNode_Dependency_Exhibit
       value={cfg.value}
     />);
   }
