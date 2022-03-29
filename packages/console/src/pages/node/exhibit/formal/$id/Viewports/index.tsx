0b6088eadb2532657df8db0ebec7a9b716daa3a9
liu-kai-github <liukaiemailchina@gmail.com>
2022-03-29 11:37:15 +0800

    Update index.tsx

--- packages/console/src/pages/node/exhibit/formal/$id/Viewports/index.tsx
+++ packages/console/src/pages/node/exhibit/formal/$id/Viewports/index.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
 import styles from './index.less';
-import {FTitleText} from "@/components/FText";
-import {connect, Dispatch} from 'dva';
-import {ConnectState, ExhibitInfoPageModelState} from "@/models/connect";
-import {ChangeAction} from "@/models/exhibitInfoPage";
+import { FTitleText } from '@/components/FText';
+import { connect, Dispatch } from 'dva';
+import { ConnectState, ExhibitInfoPageModelState } from '@/models/connect';
+import { ChangeAction } from '@/models/exhibitInfoPage';
 import {
-  FAntvG6AuthorizationGraph,
-  FAntvG6DependencyGraph,
   FAntvG6RelationshipGraph,
-  FViewportTabs
-} from "@/components/FAntvG6";
-import FDrawer from "@/components/FDrawer";
-import {FTextBtn} from "@/components/FButton";
-import FUtil1 from "@/utils";
+  FViewportTabs,
+} from '@/components/FAntvG6';
+import FDrawer from '@/components/FDrawer';
+import { FTextBtn } from '@/components/FButton';
+import FUtil1 from '@/utils';
 import FGraph_Tree_Authorization_Exhibit from '@/components/FAntvG6/FGraph_Tree_Authorization_Exhibit';
 
 interface ViewportsProps {
@@ -21,7 +19,7 @@ interface ViewportsProps {
   exhibitInfoPage: ExhibitInfoPageModelState;
 }
 
-function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
+function Viewports({ dispatch, exhibitInfoPage }: ViewportsProps) {
 
   async function onChange(payload: Partial<ExhibitInfoPageModelState>) {
     await dispatch<ChangeAction>({
@@ -29,6 +27,7 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
       payload,
     });
   }
+
 // graph_FullScreen: false,
   //   graph_Viewport_Show: 'relationship',
   //   graph_Viewport_RelationGraph_Nodes: [],
@@ -37,9 +36,9 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
   //   graph_Viewport_AuthorizationGraph_Edges: [],
   return (<div>
     <div className={styles.title}>
-      <FTitleText text={FUtil1.I18n.message('title_exhibit_maps')} type="h3"/>
+      <FTitleText text={FUtil1.I18n.message('title_exhibit_maps')} type='h3' />
       <FTextBtn
-        type="default"
+        type='default'
         onClick={() => {
           onChange({
             graph_FullScreen: true,
@@ -47,20 +46,20 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
         }}
       >全屏查看</FTextBtn>
     </div>
-    <div style={{height: 20}}/>
+    <div style={{ height: 20 }} />
     <FViewportTabs
       options={[
-        {value: 'relationship', label: FUtil1.I18n.message('quick_decision_map')},
-        {value: 'authorization', label: '授权链视图'},
+        { value: 'relationship', label: FUtil1.I18n.message('quick_decision_map') },
+        { value: 'authorization', label: '授权链视图' },
       ]}
       value={exhibitInfoPage.graph_Viewport_Show}
       onChange={(value) => {
-        onChange({graph_Viewport_Show: value as 'relationship'});
+        onChange({ graph_Viewport_Show: value as 'relationship' });
       }}
     >
       {
         exhibitInfoPage.graph_FullScreen
-          ? (<div style={{height: 500}}/>)
+          ? (<div style={{ height: 500 }} />)
           : (<>
             {
               exhibitInfoPage.graph_Viewport_Show === 'relationship' && (<FAntvG6RelationshipGraph
@@ -82,6 +81,7 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
             {
               exhibitInfoPage.graph_Viewport_Show === 'authorization' && (<FGraph_Tree_Authorization_Exhibit
                 exhibitID={exhibitInfoPage.exhibit_ID}
+                version={exhibitInfoPage.side_Version}
                 width={860}
                 height={500}
               />)
@@ -106,8 +106,8 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
 
       <FViewportTabs
         options={[
-          {label: '关系树', value: 'relationship'},
-          {label: '授权链', value: 'authorization'},
+          { label: '关系树', value: 'relationship' },
+          { label: '授权链', value: 'authorization' },
         ]}
         value={exhibitInfoPage.graph_Viewport_Show}
         onChange={(value) => {
@@ -133,10 +133,12 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
         {/*    height={window.innerHeight - 60 - 70 - 50}*/}
         {/*  />)*/}
         {/*}*/}
-
+        {/*{console.log(exhibitInfoPage.side_Version, 'exhibitInfoPage.side_Version2390iojsdklf')}*/}
         {
           exhibitInfoPage.graph_Viewport_Show === 'authorization' && (<FGraph_Tree_Authorization_Exhibit
             exhibitID={exhibitInfoPage.exhibit_ID}
+            // version={exhibitInfoPage.side_Version}
+            version={exhibitInfoPage.side_Version}
             width={window.innerWidth - 60}
             height={window.innerHeight - 60 - 70 - 50}
           />)
@@ -148,6 +150,6 @@ function Viewports({dispatch, exhibitInfoPage}: ViewportsProps) {
   </div>);
 }
 
-export default connect(({exhibitInfoPage}: ConnectState) => ({
+export default connect(({ exhibitInfoPage }: ConnectState) => ({
   exhibitInfoPage,
 }))(Viewports);
