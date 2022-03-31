fff681c86ad4e7c4f491b8ddd876e6ac36d2ccdf
liu-kai-github <liukaiemailchina@gmail.com>
2022-03-31 15:40:41 +0800

    Update index.tsx

--- packages/console/src/pages/node/creator/index.tsx
+++ packages/console/src/pages/node/creator/index.tsx
@@ -1,28 +1,27 @@
 import * as React from 'react';
 import styles from './index.less';
-import FCenterLayout from "@/layouts/FCenterLayout";
-import {FTitleText, FContentText} from '@/components/FText';
-import {FRectBtn} from '@/components/FButton';
-import {Input, Space} from 'antd';
-import {connect, Dispatch} from 'dva';
-import {ConnectState, NodesModelState} from '@/models/connect';
+import FCenterLayout from '@/layouts/FCenterLayout';
+import { FTitleText, FContentText } from '@/components/FText';
+import { FRectBtn } from '@/components/FButton';
+import { Input, Space } from 'antd';
+import { connect, Dispatch } from 'dva';
+import { ConnectState, NodesModelState } from '@/models/connect';
 import {
-  ChangeAction,
   CreateNodeAction,
   InitModelStatesAction,
   OnChangeDomainAction,
-  OnChangeNameAction
-} from "@/models/nodes";
-import {FCheck, FLoading} from '@/components/FIcons';
-import FInput from "@/components/FInput";
-import FContentLayout from "@/layouts/FContentLayout";
+  OnChangeNameAction,
+} from '@/models/nodes';
+import { FCheck, FLoading } from '@/components/FIcons';
+import FInput from '@/components/FInput';
+import FContentLayout from '@/layouts/FContentLayout';
 
 interface NodeCreatorProps {
   dispatch: Dispatch;
   nodes: NodesModelState;
 }
 
-function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
+function NodeCreator({ nodes, dispatch }: NodeCreatorProps) {
   React.useEffect(() => {
     dispatch<InitModelStatesAction>({
       type: 'nodes/initModelStates',
@@ -30,8 +29,8 @@ function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
   }, []);
 
   return (<FContentLayout header={<FTitleText
-    type="h1"
-    text={'创建节点'}/>}
+    type='h1'
+    text={'创建节点'} />}
   >
     {/*<div className={styles.header}>*/}
     {/*  */}
@@ -39,7 +38,7 @@ function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
     <div className={styles.body}>
       <Space size={10}>
         <div className={styles.domain}>
-          <FContentText type="negative" text={'节点地址'}/>
+          <FContentText type='negative' text={'节点地址'} />
           <div className={styles.inputWrap}>
             <FInput
               value={nodes.nodeDomain}
@@ -52,17 +51,17 @@ function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
               })}
             />
           </div>
-          <FContentText type="negative" text={'.freelog.com'}/>
+          <FContentText type='negative' text={'.freelog.com'} />
         </div>
-        <div style={{width: 18}}>
-          {nodes.domainVerify === 1 && <FLoading/>}
-          {nodes.domainVerify === 2 && !nodes.domainError && <FCheck/>}
+        <div style={{ width: 18 }}>
+          {nodes.domainVerify === 1 && <FLoading />}
+          {nodes.domainVerify === 2 && !nodes.domainError && <FCheck />}
         </div>
       </Space>
       <pre className={styles.errorTip}>{nodes.domainError}</pre>
       <Space size={10}>
         <div className={styles.name}>
-          <FContentText type="negative" text={'节点名称'}/>
+          <FContentText type='negative' text={'节点名称'} />
           <div className={styles.inputWrap}>
             <FInput
               value={nodes.nodeName}
@@ -76,9 +75,9 @@ function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
             />
           </div>
         </div>
-        <div style={{width: 18}}>
-          {nodes.nameVerify === 1 && <FLoading/>}
-          {nodes.nameVerify === 2 && !nodes.nameError && <FCheck/>}
+        <div style={{ width: 18 }}>
+          {nodes.nameVerify === 1 && <FLoading />}
+          {nodes.nameVerify === 2 && !nodes.nameError && <FCheck />}
         </div>
       </Space>
       <pre className={styles.errorTip}>{nodes.nameError}</pre>
@@ -89,12 +88,12 @@ function NodeCreator({nodes, dispatch}: NodeCreatorProps) {
         onClick={() => dispatch<CreateNodeAction>({
           type: 'nodes/createNode',
         })}
-        type="primary"
+        type='primary'
       >创建节点</FRectBtn>
     </div>
   </FContentLayout>);
 }
 
-export default connect(({nodes}: ConnectState) => ({
+export default connect(({ nodes }: ConnectState) => ({
   nodes,
 }))(NodeCreator);
