09f02e009ffb456e8dc2e83a0918ac55042e8dcd
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-02 10:32:05 +0800

    Update FRelationDrawer.tsx

--- packages/console/src/components/FAntvG6/FGraph_Tree_Relationship_Resource/FRelationDrawer.tsx
+++ packages/console/src/components/FAntvG6/FGraph_Tree_Relationship_Resource/FRelationDrawer.tsx
@@ -5,7 +5,10 @@ import { FContentText } from '@/components/FText';
 // import FResource from '@/components/FIcons/FResource';
 // import { FNodes, FUser } from '@/components/FIcons';
 import FDrawer from '@/components/FDrawer';
-import { FServiceAPI } from '@freelog/tools-lib';
+import { FServiceAPI, FUtil } from '@freelog/tools-lib';
+import { Space } from 'antd';
+import FIdentityTypeBadge from '@/components/FIdentityTypeBadge';
+import FLoadingTip from '@/components/FLoadingTip';
 
 interface FRelationDrawerProps {
   licensor: {
@@ -38,7 +41,13 @@ interface FRelationDrawerStates {
   } | null;
 }
 
+const initData: FRelationDrawerStates = {
+  dataSource: null,
+};
+
 function FRelationDrawer({ licensor, licensee }: FRelationDrawerProps) {
+
+  const [dataSource, set_DataSource] = React.useState<FRelationDrawerStates['dataSource']>(initData['dataSource']);
 
   React.useEffect(() => {
 
@@ -64,7 +73,7 @@ function FRelationDrawer({ licensor, licensee }: FRelationDrawerProps) {
         resourceId: string;
         resourceName: string;
         userId: number;
-      }
+      }[];
     } = await FServiceAPI.Resource.batchInfo(params0);
 
     console.log(data_ResourceInfos, 'data_ResourceInfos39028iojsdkfs90');
@@ -83,10 +92,39 @@ function FRelationDrawer({ licensor, licensee }: FRelationDrawerProps) {
         contractId: string;
         contractName: string;
         createDate: string;
-      };
+      }[];
     } = await FServiceAPI.Contract.batchContracts(params1);
     console.log(data_Contracts, 'data_Contracts@3098uijoklsdfl');
+    const lor = data_ResourceInfos.find((ri) => {
+      return ri.resourceId === licensor.licensorID;
+    });
+    const lee = data_ResourceInfos.find((ri) => {
+      return ri.resourceId === licensee.licenseeID;
+    });
+    if (!lor || !lee) {
+      return;
+    }
+    const data: FRelationDrawerStates['dataSource'] = {
+      licensor: {
+        licensorID: lor.resourceId,
+        licensorName: lor.resourceName,
+        licensorIdentityType: 'resource',
+      },
+      licensee: {
+        licenseeID: lee.resourceId,
+        licenseeName: lee.resourceName,
+        licensorIdentityType: 'resource',
+      },
+      contracts: data_Contracts.map<NonNullable<FRelationDrawerStates['dataSource']>['contracts'][number]>((dc) => {
+        return {
+          contractID: dc.contractId,
+          contractName: dc.contractName,
+          createDate: FUtil.Format.formatDateTime(dc.createDate),
+        };
+      }),
+    };
 
+    set_DataSource(data);
   }
 
   async function handleData_Resource2Exhibit() {
@@ -95,30 +133,44 @@ function FRelationDrawer({ licensor, licensee }: FRelationDrawerProps) {
 
   return (<FDrawer
     visible={true}
-    title={'合约详情'}
+    title={'授权关系'}
     // onClose={() => onClose && onClose()}
     afterVisibleChange={onChange_DrawerVisible}
   >
-    <FFormLayout>
-      <FFormLayout.FBlock title={'授权方'}>
-        <FContentText
-          type='highlight'
-          text={'1234'}
-        />
-      </FFormLayout.FBlock>
-      <FFormLayout.FBlock title={'被授权方'}>
-        <FContentText
-          type='highlight'
-          text={'5678'}
-        />
-      </FFormLayout.FBlock>
-      <FFormLayout.FBlock title={'合约详情'}>
-        <FContentText
-          type='highlight'
-          text={'5678'}
-        />
-      </FFormLayout.FBlock>
-    </FFormLayout>
+    {
+      !dataSource && (<FLoadingTip height={500} />)
+    }
+
+    {
+      dataSource && (<FFormLayout>
+        <FFormLayout.FBlock title={'授权方'}>
+          <Space size={10}>
+            <FIdentityTypeBadge status={dataSource.licensor.licensorIdentityType} />
+            <FContentText
+              type='highlight'
+              text={dataSource.licensor.licensorName}
+            />
+          </Space>
+
+        </FFormLayout.FBlock>
+        <FFormLayout.FBlock title={'被授权方'}>
+          <Space size={10}>
+            <FIdentityTypeBadge status={dataSource.licensee.licensorIdentityType} />
+            <FContentText
+              type='highlight'
+              text={dataSource.licensee.licenseeName}
+            />
+          </Space>
+        </FFormLayout.FBlock>
+        <FFormLayout.FBlock title={'合约详情'}>
+          <FContentText
+            type='highlight'
+            text={'5678'}
+          />
+        </FFormLayout.FBlock>
+      </FFormLayout>)
+    }
+
   </FDrawer>);
 }
 
