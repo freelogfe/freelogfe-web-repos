8dbdd907650fb0275236985019e07866bbc7e232
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-08 14:51:59 +0800

    update

--- packages/console/src/components/FResourceCard/index.tsx
+++ packages/console/src/components/FResourceCard/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Policy from './Policy';
+// import Policy from './Policy';
 import { FContentText } from '../FText';
 import styles from './index.less';
 import FResourceStatusBadge from '../FResourceStatusBadge';
