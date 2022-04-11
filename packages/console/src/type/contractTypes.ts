d9fe69c452a114ffdc61126af3a0a55a06bc2e13
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-11 16:10:21 +0800

    Update contractTypes.ts

--- packages/console/src/type/contractTypes.ts
+++ packages/console/src/type/contractTypes.ts
@@ -1,7 +1,25 @@
 export type ContractStatus = 'active' | 'testActive' | 'inactive' | 'terminal';
 
 export interface PolicyFullInfo_Type {
-  fsmDescriptionInfo: any;
+  fsmDescriptionInfo: {
+    [k: string]: {
+      isAuth: boolean;
+      isInitial: boolean;
+      isTestAuth: boolean;
+      transitions: Array<({
+        args: { elapsed: number, timeUnit: 'month' };
+        name: 'RelativeTimeEvent';
+      } | {
+        name: 'TimeEvent';
+        args: { dateTime: string };
+      } | {
+        name: 'TransactionEvent';
+        args: { amount: number; account: 'self.account' }
+      }) & {
+        toState: string;
+      }>;
+    }
+  };
   policyId: string;
   policyName: string;
   policyText: string;
