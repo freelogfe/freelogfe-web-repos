19f9c4be458fd2f8682c1aed0494954b0e52387c
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-12 15:18:20 +0800

    update

---
+++ packages/console/src/components/FAntvG6/FGraph_Status_Machine/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './index.less';
+
+interface FGraph_Status_Machine_Props {
+
+}
+
+function FGraph_Status_Machine({}: FGraph_Status_Machine_Props) {
+  return (<div>FGraph_Status_Machine_Props</div>);
+}
+
+export default FGraph_Status_Machine;
