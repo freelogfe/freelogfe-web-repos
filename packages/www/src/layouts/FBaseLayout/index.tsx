93ccd682d08c3b04bfebf957904fd91952f38fb4
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-22 14:32:18 +0800

    update

--- packages/www/src/layouts/FBaseLayout/index.tsx
+++ packages/www/src/layouts/FBaseLayout/index.tsx
@@ -11,6 +11,7 @@ import { connect } from 'dva';
 // import { ConnectState, UserModelState } from '@/models/connect';
 import UserSVG from '@/assets/user.svg';
 import { history } from 'umi';
+import FInput from '@/components/FInput';
 
 // import FUtil1 from '@/utils';
 
@@ -27,39 +28,46 @@ function FBaseLayout({ children }: FBaseLayoutProps) {
         className={['freelog', 'fl-icon-logo-freelog', styles.logo].join(' ')}
       />
     }
-    headerRight={<Space size={5}>
-      <FDropdown overlay={<div className={styles.userPanel}>
-        <div className={styles.userPanelHeader}>
-          <img src={(UserSVG) as string} alt='headImage' />
-          <div style={{ height: 10 }} />
-          <FContentText
-            type='highlight'
-            // text={user.userInfo?.username}
-            text={'LiuKai'}
-          />
-          <div style={{ height: 8 }} />
-          <FContentText
-            // text={user.userInfo?.mobile || user.userInfo?.email}
-            text={'133434333443'}
-          />
-        </div>
-        <div className={styles.userPanelMenu}>
-          {/*<a onClick={() => {*/}
-          {/*  window.location.href = `${FUtil.Format.completeUrlByDomain('www')}/user/Profile`;*/}
-          {/*}}>个人中心</a>*/}
-          <a onClick={async () => {
-            // window.location.href = `${FUtil.Format.completeUrlByDomain('www')}/login`;
-            await FServiceAPI.User.logout({});
-            // return window.location.replace(`${FUtil.Format.completeUrlByDomain('www')}/login`);
-            history.replace(FUtil.LinkTo.login());
-          }}>登出</a>
-        </div>
-      </div>}>
-        <a className={styles.avatar}>
-          <img src={UserSVG} alt={'avatar'} />
-        </a>
-      </FDropdown>
-    </Space>}
+    // headerRight={<Space size={5}>
+    //   <FDropdown overlay={<div className={styles.userPanel}>
+    //     <div className={styles.userPanelHeader}>
+    //       <img src={(UserSVG) as string} alt='headImage' />
+    //       <div style={{ height: 10 }} />
+    //       <FContentText
+    //         type='highlight'
+    //         // text={user.userInfo?.username}
+    //         text={'LiuKai'}
+    //       />
+    //       <div style={{ height: 8 }} />
+    //       <FContentText
+    //         // text={user.userInfo?.mobile || user.userInfo?.email}
+    //         text={'133434333443'}
+    //       />
+    //     </div>
+    //     <div className={styles.userPanelMenu}>
+    //       {/*<a onClick={() => {*/}
+    //       {/*  window.location.href = `${FUtil.Format.completeUrlByDomain('www')}/user/Profile`;*/}
+    //       {/*}}>个人中心</a>*/}
+    //       <a onClick={async () => {
+    //         // window.location.href = `${FUtil.Format.completeUrlByDomain('www')}/login`;
+    //         await FServiceAPI.User.logout({});
+    //         // return window.location.replace(`${FUtil.Format.completeUrlByDomain('www')}/login`);
+    //         history.replace(FUtil.LinkTo.login());
+    //       }}>登出</a>
+    //     </div>
+    //   </div>}>
+    //     <a className={styles.avatar}>
+    //       <img src={UserSVG} alt={'avatar'} />
+    //     </a>
+    //   </FDropdown>
+    // </Space>}
+    headerRight={<div className={styles.notLoggedIn}>
+      <FInput theme='dark' style={{ width: 200 }} />
+      <div style={{ width: 30 }} />
+      <FRectBtn type='default' size='small'>登录</FRectBtn>
+      <div style={{ width: 10 }} />
+      <FRectBtn type='primary' size='small'>注册</FRectBtn>
+    </div>}
   >{children}
   </FLayout>);
 }
