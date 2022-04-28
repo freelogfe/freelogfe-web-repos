e754e54c5248ef428e76bac98de88b1623b01d7c
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-28 11:32:54 +0800

    Update index.tsx

--- packages/console/src/components/FHeaderNavigation/EmptyAndCreate/index.tsx
+++ packages/console/src/components/FHeaderNavigation/EmptyAndCreate/index.tsx
@@ -13,22 +13,15 @@ interface EmptyAndCreateProps {
   target?: '_self' | '_blank';
 }
 
-function EmptyAndCreate({ tipText, btnText, btnHref, target }: EmptyAndCreateProps) {
+function EmptyAndCreate({ tipText, btnText, btnHref, target = '_self' }: EmptyAndCreateProps) {
   return (<div className={styles.emptyDropdown}>
     <FContentText text={tipText} />
     <div style={{ height: 30 }} />
     {
       btnHref.startsWith('http')
-        ? (<a className={styles.Link} href={btnHref}>{btnText}</a>)
-        : (<Link className={styles.Link} target={'_blank'} to={btnHref}>{btnText}</Link>)
+        ? (<a className={styles.Link} href={btnHref} target={target}>{btnText}</a>)
+        : (<Link className={styles.Link} to={btnHref} target={target}>{btnText}</Link>)
     }
-
-    {/*<FRectBtn*/}
-    {/*  onClick={() => {*/}
-
-    {/*  }}*/}
-    {/*  size='small'*/}
-    {/*>{btnText}</FRectBtn>*/}
   </div>);
 }
 
