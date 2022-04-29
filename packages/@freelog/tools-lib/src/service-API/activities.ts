7a11634bd0ca2373f08bbeef879e952069d87429
liu-kai-github <liukaiemailchina@gmail.com>
2022-04-29 14:21:32 +0800

    update

---
+++ packages/@freelog/tools-lib/src/service-API/activities.ts
@@ -0,0 +1,26 @@
+import FUtil from '../utils';
+
+// 列出活动
+interface List4ClientParamsType {
+}
+
+export function list4Client(params: List4ClientParamsType) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/activities/list4Client`,
+    params: params,
+  });
+}
+
+// 查询活动
+interface Find4ClientParamsType {
+  _id: string;
+}
+
+export function find4Client(params: Find4ClientParamsType) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/activities/find4Client`,
+    params: params,
+  });
+}
