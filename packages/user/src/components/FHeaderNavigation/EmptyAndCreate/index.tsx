14c180dbfaaf3405da9e0a5148abdbab3f4e14e2
liu-kai-github <liukaiemailchina@gmail.com>
2022-05-05 16:21:44 +0800

    update

---
+++ packages/user/src/components/FHeaderNavigation/EmptyAndCreate/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styles from './index.less';
+import { FContentText } from '@/components/FText';
+// import { FRectBtn } from '@/components/FButton';
+// import { ChangeAction } from '@/models/storageHomePage';
+import { Link } from 'umi';
+// import { FUtil } from '@freelog/tools-lib';
+
+interface EmptyAndCreateProps {
+  tipText: string;
+  btnText: string;
+  btnHref: string;
+  target?: '_self' | '_blank';
+}
+
+function EmptyAndCreate({ tipText, btnText, btnHref, target = '_self' }: EmptyAndCreateProps) {
+  return (<div className={styles.emptyDropdown}>
+    <FContentText text={tipText} />
+    <div style={{ height: 30 }} />
+    {
+      btnHref.startsWith('http')
+        ? (<a className={styles.Link} href={btnHref} target={target}>{btnText}</a>)
+        : (<Link className={styles.Link} to={btnHref} target={target}>{btnText}</Link>)
+    }
+  </div>);
+}
+
+export default EmptyAndCreate;
