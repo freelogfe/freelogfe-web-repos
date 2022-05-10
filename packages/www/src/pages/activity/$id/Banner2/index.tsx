dec5fdde9bf2909dff816e49dc815e854c17be20
liu-kai-github <liukaiemailchina@gmail.com>
2022-05-10 15:46:40 +0800

    update

--- packages/www/src/pages/activity/$id/Banner2/index.tsx
+++ packages/www/src/pages/activity/$id/Banner2/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styles from './index.less';
+import { FRectBtn, FTextBtn } from '@/components/FButton';
 
 interface Banner2Props {
 
@@ -7,7 +8,19 @@ interface Banner2Props {
 
 function Banner2({}: Banner2Props) {
   return (<div className={styles.banner2}>
-
+    <div className={styles.content}>
+      <FRectBtn style={{
+        position: 'absolute',
+        top: 218,
+        left: 206,
+        // top: 218px;
+        // left: 206px;
+        height: 50,
+        padding: '0 50px',
+      }}>立即参赛</FRectBtn>
+      <FTextBtn type="primary" style={{bottom: 0, left: 0, position: 'absolute'}}>如何参赛？</FTextBtn>
+      <FTextBtn type="primary" style={{bottom: 0, left: 80, position: 'absolute'}}>规则详情</FTextBtn>
+    </div>
   </div>);
 }
 
