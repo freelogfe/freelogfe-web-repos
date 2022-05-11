d19eac30251de8b1d5e1c3441d4ffb632444a81c
liu-kai-github <liukaiemailchina@gmail.com>
2022-05-11 10:45:10 +0800

    update

--- packages/www/src/pages/activity/$id/ResourceCompetition/Banner2/index.tsx
+++ packages/www/src/pages/activity/$id/ResourceCompetition/Banner2/index.tsx
@@ -1,27 +1,48 @@
 import * as React from 'react';
 import styles from './index.less';
 import { FRectBtn, FTextBtn } from '@/components/FButton';
+import FModal from '@/components/FModal';
 
 interface Banner2Props {
 
 }
 
 function Banner2({}: Banner2Props) {
-  return (<div className={styles.banner2}>
-    <div className={styles.content}>
-      <FRectBtn style={{
-        position: 'absolute',
-        top: 218,
-        left: 206,
-        // top: 218px;
-        // left: 206px;
-        height: 50,
-        padding: '0 50px',
-      }}>立即参赛</FRectBtn>
-      <FTextBtn type="primary" style={{bottom: 0, left: 0, position: 'absolute'}}>如何参赛？</FTextBtn>
-      <FTextBtn type="primary" style={{bottom: 0, left: 80, position: 'absolute'}}>规则详情</FTextBtn>
+  return (<>
+    <div className={styles.banner2}>
+      <div className={styles.content}>
+        <FRectBtn style={{
+          position: 'absolute',
+          top: 218,
+          left: 206,
+          // top: 218px;
+          // left: 206px;
+          height: 50,
+          padding: '0 50px',
+        }}>立即参赛</FRectBtn>
+        <FTextBtn
+          type='primary'
+          style={{ bottom: 0, left: 0, position: 'absolute' }}
+        >如何参赛？</FTextBtn>
+        <FTextBtn
+          type='primary'
+          style={{ bottom: 0, left: 80, position: 'absolute' }}
+          onClick={() => {
+
+          }}
+        >规则详情</FTextBtn>
+      </div>
     </div>
-  </div>);
+    <FModal
+      width={1440}
+      visible={false}
+      footer={null}
+    >
+      <div>
+        1234
+      </div>
+    </FModal>
+  </>);
 }
 
 export default Banner2;
