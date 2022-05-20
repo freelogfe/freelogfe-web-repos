d687ea94cc8b0760f4654f49940be61d1cf32140
liu-kai-github <liukaiemailchina@gmail.com>
2022-05-20 16:56:11 +0800

    update

--- packages/console/src/pages/resource/details/$id/index.tsx
+++ packages/console/src/pages/resource/details/$id/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import styles from './index.less';
-import {Dispatch, connect} from 'dva';
+import { Dispatch, connect } from 'dva';
 import Sign from './Sign';
-import {FTitleText, FContentText} from '@/components/FText';
-import {FFavorite, FSwap} from '@/components/FIcons';
+import { FTitleText, FContentText } from '@/components/FText';
+import { FFavorite, FSwap } from '@/components/FIcons';
 import Description from './Description';
 import Property from './Property';
 import Option from './Option';
 import Viewport from './Viewport';
-import {ConnectState, MarketResourcePageModelState} from '@/models/connect';
+import { ConnectState, MarketResourcePageModelState } from '@/models/connect';
 import {
   OnClickCollectionAction,
   OnMountPageAction,
@@ -16,22 +16,25 @@ import {
   OnUnmountPageAction,
 } from '@/models/marketResourcePage';
 import FDropdownMenu from '@/components/FDropdownMenu';
-import {Space} from 'antd';
+import { Space } from 'antd';
 import SignPage from './SignPage';
-import {RouteComponentProps} from "react-router";
+import { RouteComponentProps } from 'react-router';
 import * as AHooks from 'ahooks';
 import useUrlState from '@ahooksjs/use-url-state';
-import {router} from "umi";
-import {FUtil} from '@freelog/tools-lib';
-import {FTextBtn} from "@/components/FButton";
+import { router } from 'umi';
+import { FUtil } from '@freelog/tools-lib';
+import { FTextBtn } from '@/components/FButton';
 import { Helmet } from 'react-helmet';
+import FNoDataTip from '@/components/FNoDataTip';
+import FResultTip from '@/components/FResultTip';
+import FUtil1 from '@/utils';
 
 interface ResourceDetailsProps extends RouteComponentProps<{ id: string }> {
   dispatch: Dispatch;
   marketResourcePage: MarketResourcePageModelState,
 }
 
-function ResourceDetails({match, dispatch, marketResourcePage}: ResourceDetailsProps) {
+function ResourceDetails({ match, dispatch, marketResourcePage }: ResourceDetailsProps) {
 
   const [state] = useUrlState<{ version: string }>();
 
@@ -60,7 +63,28 @@ function ResourceDetails({match, dispatch, marketResourcePage}: ResourceDetailsP
   }, [state]);
 
   if (marketResourcePage.isSignPage) {
-    return (<SignPage/>);
+    return (<SignPage />);
+  }
+
+  if (marketResourcePage.allVersions.length === 0) {
+    return <>
+      <div style={{ height: 100 }} />
+      <div className={styles.modal}>
+        <FResultTip
+          h1={FUtil1.I18n.message('msg_resource_status_prepareforrelease')}
+          // h1={FUtil1.I18n.message('msg_resource_status_prepareforrelease')}
+          // h2={FUtil1.I18n.message('msg_nodecreatedsuccessfully')}
+          // h2={'主题决定节点的整体外观和设计，你可以通过激活不同的主题来更改节点的布局、配色方案等。'}
+          // btnText={FUtil1.I18n.message('cta_btn_add_theme')}
+          btnText={'返回首页'}
+          onClickBtn={() => {
+            router.push(FUtil.LinkTo.dashboard());
+          }}
+        />
+
+
+      </div>
+    </>;
   }
 
   return (<div className={styles.style}>
@@ -69,19 +93,19 @@ function ResourceDetails({match, dispatch, marketResourcePage}: ResourceDetailsP
     </Helmet>
     <div className={styles.wrap}>
 
-      <div style={{height: 35}}/>
+      <div style={{ height: 35 }} />
 
       <div className={styles.header}>
         <Space size={10}>
           <label className={styles.resourceType}>{marketResourcePage.resourceInfo?.type || ''}</label>
           <FTitleText
-            style={{width: 700}}
+            style={{ width: 700 }}
             singleRow
             text={marketResourcePage.resourceInfo?.name || ''}
           />
         </Space>
         <FTextBtn
-          type="default"
+          type='default'
           className={styles.favoriteBtn}
           onClick={() => dispatch<OnClickCollectionAction>({
             type: 'marketResourcePage/onClickCollection',
@@ -91,28 +115,28 @@ function ResourceDetails({match, dispatch, marketResourcePage}: ResourceDetailsP
           <FFavorite
             filled={marketResourcePage.hasCollect}
           />
-          <div style={{width: 2}}/>
+          <div style={{ width: 2 }} />
           <span>{marketResourcePage.hasCollect ? '已收藏' : '收藏'}</span>
-          <div style={{width: 5}}/>
+          <div style={{ width: 5 }} />
           <span>({marketResourcePage.popularity}人气)</span>
         </FTextBtn>
       </div>
 
-      <div style={{height: 35}}/>
+      <div style={{ height: 35 }} />
 
-      <Sign/>
+      <Sign />
 
-      <div style={{height: 50}}/>
-      <div style={{borderTop: '1px solid #E5E7EB'}}/>
-      <div style={{height: 10}}/>
+      <div style={{ height: 50 }} />
+      <div style={{ borderTop: '1px solid #E5E7EB' }} />
+      <div style={{ height: 10 }} />
 
       {
         marketResourcePage.version && (<div className={styles.versionWrap}>
           <div className={styles.versionTitle}>
             <Space size={10}>
-              <FTitleText text={'当前版本 ' + marketResourcePage.version}/>
+              <FTitleText text={'当前版本 ' + marketResourcePage.version} />
               <FDropdownMenu
-                options={[...marketResourcePage.allVersions].reverse().map((v) => ({value: v}))}
+                options={[...marketResourcePage.allVersions].reverse().map((v) => ({ value: v }))}
                 onChange={(value) => {
                   router.push(FUtil.LinkTo.resourceDetails({
                     resourceID: marketResourcePage.resourceId,
@@ -120,31 +144,31 @@ function ResourceDetails({match, dispatch, marketResourcePage}: ResourceDetailsP
                   }));
                 }}
               >
-                <FSwap style={{cursor: 'pointer'}}/>
+                <FSwap style={{ cursor: 'pointer' }} />
               </FDropdownMenu>
             </Space>
 
             <FContentText
               text={'发布时间 ' + marketResourcePage.releaseTime}
-              type="negative"
+              type='negative'
             />
 
           </div>
 
-          <Description/>
+          <Description />
 
-          <Property/>
+          <Property />
 
-          <Option/>
+          <Option />
 
-          <Viewport/>
+          <Viewport />
         </div>)
       }
-      <div style={{height: 80}}/>
+      <div style={{ height: 80 }} />
     </div>
   </div>);
 }
 
-export default connect(({marketResourcePage}: ConnectState) => ({
+export default connect(({ marketResourcePage }: ConnectState) => ({
   marketResourcePage,
 }))(ResourceDetails);
