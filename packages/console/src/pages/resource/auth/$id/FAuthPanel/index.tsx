5569a324c397c93ba22d6aa4cdae8bc9486f84bc
liu-kai-github <liukaiemailchina@gmail.com>
2022-05-24 10:14:04 +0800

    Update index.tsx

--- packages/console/src/pages/resource/auth/$id/FAuthPanel/index.tsx
+++ packages/console/src/pages/resource/auth/$id/FAuthPanel/index.tsx
@@ -3,12 +3,12 @@ import styles from './index.less';
 import Resources from './Resources';
 import Contracts from './Contracts';
 import Policies from './Policies';
-import { Space } from 'antd';
+// import { Space } from 'antd';
 import { connect, Dispatch } from 'dva';
 import { ConnectState, ResourceAuthPageModelState } from '@/models/connect';
 import { FContentText } from '@/components/FText';
 import { FTextBtn } from '@/components/FButton';
-import { FUtil } from '@freelog/tools-lib';
+// import { FUtil } from '@freelog/tools-lib';
 import FTerminatedContractListDrawer from '@/components/FTerminatedContractListDrawer';
 
 export interface FAuthPanelProps {
