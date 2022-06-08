a0f87aa9deac79ea75187c29b814e394b365c998
liu-kai-github <liukaiemailchina@gmail.com>
2022-06-08 15:36:53 +0800

    update

--- packages/@freelog/tools-lib/src/service-API/activities.ts
+++ packages/@freelog/tools-lib/src/service-API/activities.ts
@@ -26,3 +26,35 @@ export function find4Client(params: Find4ClientParamsType) {
     params: params,
   });
 }
+
+// 列出广告
+interface AdsListParamsType {
+  skip?: number;
+  limit?: number;
+  place: 1 | 2 | 3 | 4; //  投放位置 1：顶部公告栏 2：右侧浮窗 3：概览页 4：发现页
+
+}
+
+export function adsList(params: AdsListParamsType) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/activities/ads/list4Client`,
+    params: params,
+  });
+}
+
+// 查询广告
+interface AdsDetailsParamsType {
+  skip?: number;
+  limit?: number;
+  place: 1 | 2 | 3 | 4; //  投放位置 1：顶部公告栏 2：右侧浮窗 3：概览页 4：发现页
+
+}
+
+export function adsDetails(params: AdsDetailsParamsType) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/activities/ads/find4Client`,
+    params: params,
+  });
+}
