c502ea0c6aebb4d07fc45e22e11a5e1f8f7bd54d
liu-kai-github <liukaiemailchina@gmail.com>
2022-06-08 16:15:07 +0800

    update

---
+++ packages/@freelog/components-lib/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import styles from "./index.less";
+
+// Delete me
+export const Thing = () => {
+  return <div className={styles.header}>the snozzberries taste like snozzberries</div>;
+};
