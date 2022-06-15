c0fca90d64a6042f8ff7dc099f06f4672fcbec2e
liu-kai-github <liukaiemailchina@gmail.com>
2022-06-15 16:15:13 +0800

    Update testQualifications.ts

--- packages/@freelog/tools-lib/src/service-API/testQualifications.ts
+++ packages/@freelog/tools-lib/src/service-API/testQualifications.ts
@@ -8,7 +8,7 @@ interface BetaCodesActivateParamsType {
 export function betaCodesActivate({...params}: BetaCodesActivateParamsType) {
   return FUtil.Request({
     method: 'POST',
-    url: '/v2/testQualifications/beta/codes/activate',
+    url: `/v2/testQualifications/beta/codes/activate`,
     data: params,
   });
 }
@@ -25,7 +25,32 @@ interface BetaApplyParamsType {
 export function betaApply({...params}: BetaApplyParamsType) {
   return FUtil.Request({
     method: 'POST',
-    url: '/v2/testQualifications/beta/apply',
+    url: `/v2/testQualifications/beta/apply`,
+    data: params,
+  });
+}
+
+// 查看内测资格申请详情
+interface GetBetaApply1ParamsType {
+
+}
+
+export function getBetaApply1({...params}: GetBetaApply1ParamsType = {}) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/testQualifications/beta/apply/current`,
+    params: params,
+  });
+}
+
+interface GetBetaApply2ParamsType {
+  recordId: string;
+}
+
+export function getBetaApply2({recordId, ...params}: GetBetaApply2ParamsType) {
+  return FUtil.Request({
+    method: 'GET',
+    url: `/v2/testQualifications/beta/apply/${recordId}`,
     data: params,
   });
 }
