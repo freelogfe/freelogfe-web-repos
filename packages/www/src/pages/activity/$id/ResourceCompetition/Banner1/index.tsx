5eea32c1c5fdcd2ad8c4eaed317fee1a5f32b8d9
liu-kai-github <liukaiemailchina@gmail.com>
2022-08-08 11:04:22 +0800

    udpate

--- packages/www/src/pages/activity/$id/ResourceCompetition/Banner1/index.tsx
+++ packages/www/src/pages/activity/$id/ResourceCompetition/Banner1/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import styles from './index.less';
 import { connect } from 'dva';
 import { ActivityDetailsPageModelState, ConnectState } from '@/models/connect';
+import * as img_wired from '@/assets/activity/wired.png';
+import moment from 'moment';
 
 interface Banner1Props {
   activityDetailsPage: ActivityDetailsPageModelState;
@@ -23,16 +25,23 @@ function Banner1({ activityDetailsPage }: Banner1Props) {
           <div style={{ height: 4 }} />
           <div className={styles.time}>{activityDetailsPage.startTime || 'YYYY·MM·DD'}</div>
         </div>
+        <img src={img_wired} style={{ width: 38, height: 15 }} />
         <div className={styles.banner1Content_Time2}>
           <div className={styles.title}>活动结束</div>
           <div style={{ height: 4 }} />
           {/*<div className={styles.time}>2022·02·10</div>*/}
           <div className={styles.time}>{activityDetailsPage.endTime || 'YYYY·MM·DD'}</div>
         </div>
+        <img src={img_wired} style={{ width: 38, height: 15 }} />
         <div className={styles.banner1Content_Time3}>
           <div className={styles.title}>获奖公示</div>
           <div style={{ height: 4 }} />
-          <div className={styles.time}>{'YYYY·MM·DD'}</div>
+          {
+            !!activityDetailsPage.endTime
+              ? (<div className={styles.time}>{moment(activityDetailsPage.endTime, 'YYYY·MM·DD').add(7, 'days').format('YYYY·MM·DD')}</div>)
+              : (<div className={styles.time}>{'YYYY·MM·DD'}</div>)
+          }
+
         </div>
       </div>
     </div>
