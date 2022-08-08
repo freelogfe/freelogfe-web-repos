66d8dfe67fc833ec053ddfc83625747dbac540ad
liu-kai-github <liukaiemailchina@gmail.com>
2022-08-08 11:10:29 +0800

    Update index.tsx

--- packages/console/src/pages/dashboard/Sider/index.tsx
+++ packages/console/src/pages/dashboard/Sider/index.tsx
@@ -51,102 +51,105 @@ function Sider({}: SiderProps) {
 
   });
 
-  return (<Space size={10} direction={'vertical'} className={styles.statisticsRight}>
+  return (<div>
+    <div style={{ height: 50 }} />
+    <Space size={10} direction={'vertical'} className={styles.statisticsRight}>
 
 
-    {
-      ads.map((ad) => {
-        return (<a key={ad.id} href={ad.href} target='_blank' className={styles.imgCard}>
-          <img src={ad.cover} alt={''} />
-        </a>);
-      })
-    }
+      {
+        ads.map((ad) => {
+          return (<a key={ad.id} href={ad.href} target='_blank' className={styles.imgCard}>
+            <img src={ad.cover} alt={''} />
+          </a>);
+        })
+      }
 
 
-    {/*<a href={'#'} className={styles.imgCard}>*/}
-    {/*  <img src={img_Questionnaire} alt={''} />*/}
-    {/*</a>*/}
+      {/*<a href={'#'} className={styles.imgCard}>*/}
+      {/*  <img src={img_Questionnaire} alt={''} />*/}
+      {/*</a>*/}
 
-    <div className={styles.panelCard}>
-      <FFormLayout>
-        <FFormLayout.FBlock
-          title={'常见问题'}
-          extra={<FComponentsLib.FTextBtn
-            type='default'
-            onClick={() => {
-              window.open('https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62d0d04f456ff0002e329537')
-            }}
-          >更多 &gt;</FComponentsLib.FTextBtn>}
-        >
-          <div className={styles.linkList}>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a
-                href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                target={'_blank'}
-              >如何创建资源？</a>
+      <div className={styles.panelCard}>
+        <FFormLayout>
+          <FFormLayout.FBlock
+            title={'常见问题'}
+            extra={<FComponentsLib.FTextBtn
+              type='default'
+              onClick={() => {
+                window.open('https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62d0d04f456ff0002e329537');
+              }}
+            >更多 &gt;</FComponentsLib.FTextBtn>}
+          >
+            <div className={styles.linkList}>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a
+                  href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                  target={'_blank'}
+                >如何创建资源？</a>
+              </div>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a
+                  href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                  target={'_blank'}
+                >如何创建节点？</a>
+              </div>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a
+                  href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                  target={'_blank'}
+                >Freelog有哪些使用场景？</a>
+              </div>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a
+                  href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                  target={'_blank'}
+                >资源作者和节点商有什么区别？</a>
+              </div>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a
+                  href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                  target={'_blank'}
+                >如何添加授权策略？</a>
+              </div>
             </div>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a
-                href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                target={'_blank'}
-              >如何创建节点？</a>
-            </div>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a
-                href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                target={'_blank'}
-              >Freelog有哪些使用场景？</a>
-            </div>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a
-                href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                target={'_blank'}
-              >资源作者和节点商有什么区别？</a>
-            </div>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a
-                href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                target={'_blank'}
-              >如何添加授权策略？</a>
-            </div>
-          </div>
-        </FFormLayout.FBlock>
-      </FFormLayout>
-    </div>
+          </FFormLayout.FBlock>
+        </FFormLayout>
+      </div>
 
-    <div className={styles.panelCard}>
-      <FFormLayout>
-        <FFormLayout.FBlock
-          title={'热门讨论'}
-          extra={<FComponentsLib.FTextBtn type='default'>更多 &gt;</FComponentsLib.FTextBtn>}
-        >
-          <div className={styles.linkList}>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a href={'#'} target='_blank'>Freelog内测签到盖楼，完成签到和其他基础任务领6元现金奖励！</a>
+      <div className={styles.panelCard}>
+        <FFormLayout>
+          <FFormLayout.FBlock
+            title={'热门讨论'}
+            extra={<FComponentsLib.FTextBtn type='default'>更多 &gt;</FComponentsLib.FTextBtn>}
+          >
+            <div className={styles.linkList}>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a href={'#'} target='_blank'>Freelog内测签到盖楼，完成签到和其他基础任务领6元现金奖励！</a>
+              </div>
+              <div className={styles.linkListItem}>
+                <i />
+                <span />
+                <a href={'#'} target='_blank'>Freelog内测等你来“找茬”，参与有机会赢取400元京东购物卡！</a>
+              </div>
             </div>
-            <div className={styles.linkListItem}>
-              <i />
-              <span />
-              <a href={'#'} target='_blank'>Freelog内测等你来“找茬”，参与有机会赢取400元京东购物卡！</a>
-            </div>
-          </div>
-        </FFormLayout.FBlock>
-      </FFormLayout>
-    </div>
+          </FFormLayout.FBlock>
+        </FFormLayout>
+      </div>
 
-  </Space>);
+    </Space>
+  </div>);
 }
 
 export default Sider;
