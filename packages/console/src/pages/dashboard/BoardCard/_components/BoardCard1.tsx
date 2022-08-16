48423d9a3de486e2bf811c03a2145ae7913c05c7
liu-kai-github <liukaiemailchina@gmail.com>
2022-08-16 17:37:33 +0800

    update

--- packages/console/src/pages/dashboard/BoardCard/_components/BoardCard1.tsx
+++ packages/console/src/pages/dashboard/BoardCard/_components/BoardCard1.tsx
@@ -3,15 +3,33 @@ import styles from './index.less';
 import { Space } from 'antd';
 import FPopover from '@/components/FPopover';
 import FComponentsLib from '@freelog/components-lib';
+import { FUtil } from '@freelog/tools-lib';
+import * as AHooks from 'ahooks';
+import { FServiceAPI } from '@freelog/tools-lib';
 
 interface BoardCard1Props {
   unfold: boolean;
-  data: any;
 
   onMouseEnter?(): void;
 }
 
-function BoardCard1({ unfold, onMouseEnter, data }: BoardCard1Props) {
+const url = [
+  FUtil.Format.completeUrlByDomain('user') + FUtil.LinkTo.setting(),
+  'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f',
+  'https://forum.freelog.com/topic/2/freelog%E5%86%85%E6%B5%8B%E7%AD%BE%E5%88%B0%E6%A5%BC-%E5%AE%8C%E6%88%90%E7%AD%BE%E5%88%B0%E5%8D%B3%E6%9C%89%E6%9C%BA%E4%BC%9A%E9%A2%866%E5%85%83%E7%8E%B0%E9%87%91%E5%A5%96%E5%8A%B1',
+  '',
+  '',
+] as const;
+
+function BoardCard1({ unfold, onMouseEnter }: BoardCard1Props) {
+
+  const [dataSource, set_dataSource] = React.useState<any[]>([]);
+
+  AHooks.useMount(async () => {
+    const { data } = await FServiceAPI.Activity.getBaseTaskInfo();
+    set_dataSource(data);
+  });
+
   return (
     <div
       className={styles.board1}
@@ -27,103 +45,105 @@ function BoardCard1({ unfold, onMouseEnter, data }: BoardCard1Props) {
           完成下列基础任务可以了解Freelog的基本功能，以便更顺畅的使用Freelog完成资源、节点创建和推广
         </div>
         <div className={styles.title3}>
-          还差{data.filter((item: any) => item.status === 1).length}步领取 <span>6元</span> 奖励
+          还差{dataSource.filter((item: any) => item.status === 1).length}步领取 <span>6元</span> 奖励
         </div>
         <div />
       </div>
       <div className={styles.tasks}>
-        {data.map((item: any, index: number) => {
-          return (
-            <div className={styles.task} key={item.taskConfigCode}>
-              {item.taskConfigCode === 'TS000011' ? (
-                <FPopover
-                  placement='right'
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText
-                        text={'Freelog平台存在资源作者和节点商两个角色：'}
-                        type='highlight'
-                      />
-                      <div style={{ height: 15 }} />
-                      <div style={{ display: 'flex' }}>
-                        <i
-                          style={{
-                            width: 3,
-                            height: 3,
-                            borderRadius: '50%',
-                            marginTop: 8,
-                            marginRight: 5,
-                            backgroundColor: '#666',
-                          }}
-                        />
+        {
+          dataSource.map((item: any, index: number) => {
+            return (
+              <div className={styles.task} key={item.taskConfigCode}>
+                {item.taskConfigCode === 'TS000011' ? (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
                         <FComponentsLib.FContentText
-                          text={'「资源作者」可通过创建并发行资源获取收益；'}
+                          text={'Freelog平台存在资源作者和节点商两个角色：'}
                           type='highlight'
                         />
+                        <div style={{ height: 15 }} />
+                        <div style={{ display: 'flex' }}>
+                          <i
+                            style={{
+                              width: 3,
+                              height: 3,
+                              borderRadius: '50%',
+                              marginTop: 8,
+                              marginRight: 5,
+                              backgroundColor: '#666',
+                            }}
+                          />
+                          <FComponentsLib.FContentText
+                            text={'「资源作者」可通过创建并发行资源获取收益；'}
+                            type='highlight'
+                          />
+                        </div>
+                        <div style={{ height: 15 }} />
+                        <div style={{ display: 'flex' }}>
+                          <i
+                            style={{
+                              width: 3,
+                              height: 3,
+                              borderRadius: '50%',
+                              marginTop: 8,
+                              marginRight: 5,
+                              backgroundColor: '#666',
+                            }}
+                          />
+                          <FComponentsLib.FContentText
+                            text={'「节点商」是资源的整合者，通过在节点展示推广资源获取中间人收益。'}
+                            type='highlight'
+                          />
+                        </div>
+                        <div style={{ height: 15 }} />
+                        <Space size={5}>
+                          <FComponentsLib.FContentText text={'查看'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >资源作者使用教程</a>
+                          <FComponentsLib.FContentText text={'或'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >节点商使用教</a>
+                        </Space>
                       </div>
-                      <div style={{ height: 15 }} />
-                      <div style={{ display: 'flex' }}>
-                        <i
-                          style={{
-                            width: 3,
-                            height: 3,
-                            borderRadius: '50%',
-                            marginTop: 8,
-                            marginRight: 5,
-                            backgroundColor: '#666',
-                          }}
-                        />
-                        <FComponentsLib.FContentText
-                          text={'「节点商」是资源的整合者，通过在节点展示推广资源获取中间人收益。'}
-                          type='highlight'
-                        />
+                    }
+                  >
+                    <a className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</a>
+                  </FPopover>
+                ) : (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
+                        <FComponentsLib.FContentText text={item.taskConfigDescription} type='highlight' />
                       </div>
-                      <div style={{ height: 15 }} />
-                      <Space size={5}>
-                        <FComponentsLib.FContentText text={'查看'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type='primary'
-                        >资源作者使用教程</a>
-                        <FComponentsLib.FContentText text={'或'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type='primary'
-                        >节点商使用教</a>
-                      </Space>
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              ) : (
-                <FPopover
-                  placement='right'
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText text={item.taskConfigDescription} type='highlight' />
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              )}
+                    }
+                  >
+                    <a className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</a>
+                  </FPopover>
+                )}
 
-              {item.status === 2 ? (
-                <div className={styles.taskState}>
-                  <i
-                    className={['freelog', 'fl-icon-shenqingchenggong1', styles.taskIcon].join(' ')}
-                  />
-                  <span className={styles.taskFinished}>已完成</span>
-                </div>
-              ) : (
-                <div className={styles.taskState}>未完成</div>
-              )}
-            </div>
-          );
-        })}
+                {item.status === 2 ? (
+                  <div className={styles.taskState}>
+                    <i
+                      className={['freelog', 'fl-icon-shenqingchenggong1', styles.taskIcon].join(' ')}
+                    />
+                    <span className={styles.taskFinished}>已完成</span>
+                  </div>
+                ) : (
+                  <div className={styles.taskState}>未完成</div>
+                )}
+              </div>
+            );
+          })
+        }
       </div>
     </div>
   );
--- packages/console/src/pages/dashboard/BoardCard/_components/BoardCard3.tsx
+++ packages/console/src/pages/dashboard/BoardCard/_components/BoardCard3.tsx
@@ -3,14 +3,24 @@ import styles from './index.less';
 import { Space } from 'antd';
 import FPopover from '@/components/FPopover';
 import FComponentsLib from '@freelog/components-lib';
+import * as AHooks from 'ahooks';
+import { FServiceAPI } from '@freelog/tools-lib';
 
 interface BoardCard3Props {
   unfold: boolean;
-  data: any;
+
   onMouseEnter?(): void;
 }
 
-function BoardCard3({ unfold, onMouseEnter, data }: BoardCard3Props) {
+function BoardCard3({ unfold, onMouseEnter }: BoardCard3Props) {
+
+  const [dataSource, set_dataSource] = React.useState<any[]>([]);
+
+  AHooks.useMount(async () => {
+    const { data } = await FServiceAPI.Activity.getBaseTaskInfo();
+    set_dataSource(data);
+  });
+
   return (
     <div
       className={styles.board3}
@@ -26,145 +36,147 @@ function BoardCard3({ unfold, onMouseEnter, data }: BoardCard3Props) {
           完成“节点任务”即可成为Freelog节点商，节点商是平台资源的整合方，通过在节点上展示资源和制定授权策略获取资运营收益
         </div>
         <div className={styles.title3}>
-          还差{data.filter((item: any) => item.status === 1).length}步领取 <span>7元</span> 奖励
+          还差{dataSource.filter((item: any) => item.status === 1).length}步领取 <span>7元</span> 奖励
         </div>
         <div />
       </div>
       <div className={styles.tasks}>
-        {data.map((item: any, index: number) => {
-          return (
-            <div className={styles.task} key={item.taskConfigCode}>
-              {item.taskConfigCode === 'TS000031' ? (
-                <FPopover
-                  placement="right"
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText text={'成功创建1个节点即可。'} type="highlight" />
-                      <div style={{ height: 15 }} />
-                      <Space size={5}>
-                        <FComponentsLib.FContentText text={'可查阅'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >节点商使用教程</a>
-                        <FComponentsLib.FContentText text={'（视频）或'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >节点创建教程</a>
-                        <FComponentsLib.FContentText text={'（图文）创建节点。'} />
-                      </Space>
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              ) : item.taskConfigCode === 'TS000032' ? (
-                <FPopover
-                  placement="right"
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText text={'资源被签约到节点即变为展品。'} type="highlight" />
-                      <div style={{ height: 15 }} />
-                      <Space size={5}>
-                        <FComponentsLib.FContentText text={'可查阅'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >展品添加上线教程</a>
-                        <FComponentsLib.FContentText text={'（图文）或'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >节点商使用教程</a>
-                        <FComponentsLib.FContentText text={'（视频）在资源市场添加并上线展品。'} />
-                      </Space>
-                      <div style={{ height: 15 }} />
-                      <div style={{ color: '#EE4040' }}>
-                        *只有处于上线状态的展品才能被用户签约消费。
+        {
+          dataSource.map((item: any, index: number) => {
+            return (
+              <div className={styles.task} key={item.taskConfigCode}>
+                {item.taskConfigCode === 'TS000031' ? (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
+                        <FComponentsLib.FContentText text={'成功创建1个节点即可。'} type='highlight' />
+                        <div style={{ height: 15 }} />
+                        <Space size={5}>
+                          <FComponentsLib.FContentText text={'可查阅'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >节点商使用教程</a>
+                          <FComponentsLib.FContentText text={'（视频）或'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >节点创建教程</a>
+                          <FComponentsLib.FContentText text={'（图文）创建节点。'} />
+                        </Space>
                       </div>
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              ) : item.taskConfigCode === 'TS000033' ? (
-                <FPopover
-                  placement="right"
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText text={'主题决定节点的展示外观。'} type="highlight" />
-                      <div style={{ height: 15 }} />
-                      <Space size={5}>
-                        <FComponentsLib.FContentText text={'可查阅'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >主题激活教程</a>
-                        <FComponentsLib.FContentText text={'（图文）或'} />
-                        <a
-                          href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
-                          target={'_blank'}
-                          type="primary"
-                        >节点商使用教程</a>
-                        <FComponentsLib.FContentText text={'（视频）激活主题。'} />
-                      </Space>
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              ) : (
-                <FPopover
-                  placement="right"
-                  content={
-                    <div className={styles.tooltipDisplay}>
-                      <FComponentsLib.FContentText text={item.taskConfigDescription} type="highlight" />
-                    </div>
-                  }
-                >
-                  <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
-                </FPopover>
-              )}
+                    }
+                  >
+                    <a className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</a>
+                  </FPopover>
+                ) : item.taskConfigCode === 'TS000032' ? (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
+                        <FComponentsLib.FContentText text={'资源被签约到节点即变为展品。'} type='highlight' />
+                        <div style={{ height: 15 }} />
+                        <Space size={5}>
+                          <FComponentsLib.FContentText text={'可查阅'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >展品添加上线教程</a>
+                          <FComponentsLib.FContentText text={'（图文）或'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >节点商使用教程</a>
+                          <FComponentsLib.FContentText text={'（视频）在资源市场添加并上线展品。'} />
+                        </Space>
+                        <div style={{ height: 15 }} />
+                        <div style={{ color: '#EE4040' }}>
+                          *只有处于上线状态的展品才能被用户签约消费。
+                        </div>
+                      </div>
+                    }
+                  >
+                    <a className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</a>
+                  </FPopover>
+                ) : item.taskConfigCode === 'TS000033' ? (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
+                        <FComponentsLib.FContentText text={'主题决定节点的展示外观。'} type='highlight' />
+                        <div style={{ height: 15 }} />
+                        <Space size={5}>
+                          <FComponentsLib.FContentText text={'可查阅'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >主题激活教程</a>
+                          <FComponentsLib.FContentText text={'（图文）或'} />
+                          <a
+                            href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
+                            target={'_blank'}
+                            type='primary'
+                          >节点商使用教程</a>
+                          <FComponentsLib.FContentText text={'（视频）激活主题。'} />
+                        </Space>
+                      </div>
+                    }
+                  >
+                    <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
+                  </FPopover>
+                ) : (
+                  <FPopover
+                    placement='right'
+                    content={
+                      <div className={styles.tooltipDisplay}>
+                        <FComponentsLib.FContentText text={item.taskConfigDescription} type='highlight' />
+                      </div>
+                    }
+                  >
+                    <div className={styles.taskTitle}>{index + 1 + '.' + item.taskConfigTitle}</div>
+                  </FPopover>
+                )}
 
-              {item.status === 2 ? (
-                <div className={styles.taskState}>
-                  <i
-                    className={['freelog', 'fl-icon-shenqingchenggong1', styles.taskIcon].join(' ')}
-                  />
-                  <span className={styles.taskFinished}>已完成</span>
-                </div>
-              ) : (
-                <div className={styles.taskState}>未完成</div>
-              )}
-            </div>
-          );
-        })}
+                {item.status === 2 ? (
+                  <div className={styles.taskState}>
+                    <i
+                      className={['freelog', 'fl-icon-shenqingchenggong1', styles.taskIcon].join(' ')}
+                    />
+                    <span className={styles.taskFinished}>已完成</span>
+                  </div>
+                ) : (
+                  <div className={styles.taskState}>未完成</div>
+                )}
+              </div>
+            );
+          })
+        }
 
         <div className={styles.task}>
           <FPopover
-            placement="right"
+            placement='right'
             content={
               <div className={styles.tooltipDisplay}>
-                <FComponentsLib.FContentText text={'主题决定节点的展示外观。'} type="highlight" />
+                <FComponentsLib.FContentText text={'主题决定节点的展示外观。'} type='highlight' />
                 <div style={{ height: 15 }} />
                 <Space size={5}>
                   <FComponentsLib.FContentText text={'可查阅'} />
                   <a
                     href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
                     target={'_blank'}
-                    type="primary"
+                    type='primary'
                   >主题激活教程</a>
                   <FComponentsLib.FContentText text={'（图文）或'} />
                   <a
                     href={'https://freelog3.freelog.com/$freelog-61f252ef6fe5c1002e2c7b4b=/home_id=62ce6f8a456ff0002e32915f'}
                     target={'_blank'}
-                    type="primary"
+                    type='primary'
                   >节点商使用教程</a>
                   <FComponentsLib.FContentText text={'（视频）激活主题。'} />
                 </Space>
@@ -177,12 +189,12 @@ function BoardCard3({ unfold, onMouseEnter, data }: BoardCard3Props) {
         </div>
         <div className={styles.task}>
           <FPopover
-            placement="right"
+            placement='right'
             content={
               <div className={styles.tooltipDisplay}>
                 <FComponentsLib.FContentText
                   text={'分享节点可以获得更多签约消费，快去复制链接和好友分享你的节点吧。'}
-                  type="highlight"
+                  type='highlight'
                 />
               </div>
             }
@@ -195,4 +207,5 @@ function BoardCard3({ unfold, onMouseEnter, data }: BoardCard3Props) {
     </div>
   );
 }
+
 export default BoardCard3;
