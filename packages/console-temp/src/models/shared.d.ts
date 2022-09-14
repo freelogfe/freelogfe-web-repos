a39df842e99333aa0aaafe7427778fedd16b9801
liu-kai-github <liukaiemailchina@gmail.com>
2022-09-14 15:12:41 +0800

    update

--- packages/console-temp/src/models/shared.d.ts
+++
@@ -1,9 +0,0 @@
-export type DvaReducer<S, A> = (state: S, action: A) => S;
-
-export type WholeReadonly<T> = {
-  readonly [P in keyof T]: WholeReadonly<T[P]>;
-}
-
-export type WholeMutable<T> = {
-  -readonly [P in keyof T]: WholeMutable<T[P]>;
-}
