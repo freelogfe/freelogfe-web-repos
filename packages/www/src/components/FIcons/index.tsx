6848d485d448a0795761bec011484ba44e603ed4
liu-kai-github <liukaiemailchina@gmail.com>
2022-09-21 16:58:52 +0800

    Update index.tsx

--- packages/www/src/components/FIcons/index.tsx
+++ packages/www/src/components/FIcons/index.tsx
@@ -1,23 +1,19 @@
 import * as React from 'react';
 
-import FAdd from './FAdd';
 import FCheck from './FCheck';
 import FClose from './FClose';
 import FCloudUpload from './FCloudUpload';
-// import FCopy from './FCopy';
 import FDelete from './FDelete';
 import FDoubleDown from './FDoubleDown';
 import FDoubleUp from './FDoubleUp';
 import FDown from './FDown';
 import FDownload from './FDownload';
-// import FEdit from './FEdit';
 import FExclamation from './FExclamation';
 import FFavorite from './FFavorite';
 import FFileSearch from './FFileSearch';
 import FFileText from './FFileText';
 import FInfo from './FInfo';
 import FLeft from './FLeft';
-// import FLine from './FLine';
 import FLoading from './FLoading';
 import FMappingRuleAdd from './FMappingRuleAdd';
 import FMappingRuleAttr from './FMappingRuleAttr';
@@ -32,17 +28,14 @@ import FObject from './FObject';
 import FPlus from './FPlus';
 import FRedo from './FRedo';
 import FSwap from './FSwap';
-// import FWarning from './FWarning';
 import FExit from './FExit';
 import FExport from './FExport';
 import FImport from './FImport';
 import FCode from './FCode';
 import FNodes from './FNodes';
 import FUp from './FUp';
-// import FUser from './FUser';
 
 export {
-  FAdd,
   FDownload,
   FLeft,
   FDelete,
@@ -53,14 +46,11 @@ export {
   FInfo,
   FFileText,
   FFileSearch,
-  // FEdit,
   FExclamation,
   FClose,
   FDoubleUp,
   FDoubleDown,
   FPlus,
-  // FWarning,
-  // FCopy,
   FCloudUpload,
   FCheck,
   FLoading,
@@ -81,5 +71,4 @@ export {
   FCode,
   FNodes,
   FUp,
-  // FUser,
 };
