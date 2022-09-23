ae58fe53e77a85a0a3e255929722ef1dd957276b
liu-kai-github <liukaiemailchina@gmail.com>
2022-09-23 16:40:56 +0800

    Update index.tsx

--- packages/@freelog/components-lib/src/FIcons/FPlus/index.tsx
+++ packages/@freelog/components-lib/src/FIcons/FPlus/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 // import styles from './index.less';
-// import {PlusOutlined} from '@ant-design/icons';
-// import {CSSProperties} from "react";
 
 interface FPlusProps {
   className?: string;
