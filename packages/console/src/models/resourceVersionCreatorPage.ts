c9eb8bfcc08badb228673f04e4a54031735d021c
liu-kai-github <liukaiemailchina@gmail.com>
2022-10-10 14:47:07 +0800

    udpate

--- packages/console/src/models/resourceVersionCreatorPage.ts
+++ packages/console/src/models/resourceVersionCreatorPage.ts
@@ -10,31 +10,7 @@ import * as semver from 'semver';
 import moment from 'moment';
 import { FUtil, FServiceAPI } from '@freelog/tools-lib';
 import { PolicyFullInfo_Type } from '@/type/contractTypes';
-
-const fileAttrUnits: any = {
-  fileSize(value: number): string {
-    return FUtil.Format.humanizeSize(value);
-  },
-  imageWidth(value: number): string {
-    return value + 'px';
-  },
-  imageHeight(value: number): string {
-    return value + 'px';
-  },
-  duration(value: number): string {
-    const time = value / 1000;
-    const h = parseInt(String(time / 3600));
-    const minute = parseInt(String(time / 60 % 60));
-    const second = Math.ceil(time % 60);
-
-    const hours = h < 10 ? '0' + h : h;
-    const formatSecond = second > 59 ? 59 : second;
-    return `${hours > 0 ? `${hours}:` : ''}${minute < 10 ? '0' + minute : minute}:${formatSecond < 10 ? '0' + formatSecond : formatSecond}`;
-  },
-  frameRate(value: number): string {
-    return value + 'fps';
-  },
-};
+import { fileAttrUnits } from '@/utils/format';
 
 export type DepResources = {
   id: string;
---
+++ packages/console/src/utils/format.ts
@@ -0,0 +1,26 @@
+import { FUtil } from '@freelog/tools-lib';
+
+export const fileAttrUnits: any = {
+  fileSize(value: number): string {
+    return FUtil.Format.humanizeSize(value);
+  },
+  imageWidth(value: number): string {
+    return value + 'px';
+  },
+  imageHeight(value: number): string {
+    return value + 'px';
+  },
+  duration(value: number): string {
+    const time = value / 1000;
+    const h = parseInt(String(time / 3600));
+    const minute = parseInt(String(time / 60 % 60));
+    const second = Math.ceil(time % 60);
+
+    const hours = h < 10 ? '0' + h : h;
+    const formatSecond = second > 59 ? 59 : second;
+    return `${hours > 0 ? `${hours}:` : ''}${minute < 10 ? '0' + minute : minute}:${formatSecond < 10 ? '0' + formatSecond : formatSecond}`;
+  },
+  frameRate(value: number): string {
+    return value + 'fps';
+  },
+};
