76eacac55de50153907a52fe92cba95a3d2d3d4a
liu-kai-github <liukaiemailchina@gmail.com>
2022-10-12 17:00:49 +0800

    udpate

--- packages/console/src/components/FIcons/index.tsx
+++ packages/console/src/components/FIcons/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
-import FObject from './FObject';
-import FRedo from './FRedo';
-import FSwap from './FSwap';
-import FNodes from './FNodes';
-import FUp from './FUp';
+// import FObject from './FObject';
+// import FRedo from './FRedo';
+// import FSwap from './FSwap';
+// import FNodes from './FNodes';
+// import FUp from './FUp';
 
-export {
-  FSwap,
-  FObject,
-  FRedo,
-  FNodes,
-  FUp,
-};
+// export {
+//   FSwap,
+//   // FObject,
+//   FRedo,
+//   FNodes,
+//   FUp,
+// };
