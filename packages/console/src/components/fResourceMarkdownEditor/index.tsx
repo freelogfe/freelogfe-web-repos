4739f8603f2822f81990c5ba6954095f64b07708
liu-kai-github <liukaiemailchina@gmail.com>
2022-11-24 11:06:53 +0800

    Update index.tsx

--- packages/console/src/components/fResourceMarkdownEditor/index.tsx
+++ packages/console/src/components/fResourceMarkdownEditor/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './index.less';
 import * as ReactDOM from 'react-dom/client';
 import { MarkdownEditor } from './FResourceMarkdownEditorModal';
+import * as AHooks from 'ahooks';
 
 interface fResourceMarkdownEditorProps {
   resourceID: string;
@@ -12,20 +13,52 @@ interface fResourceMarkdownEditorProps {
 function fResourceMarkdownEditor({ resourceID, onChange_Saved }: fResourceMarkdownEditorProps): Promise<null> {
   return new Promise<null>((resolve) => {
     const root = ReactDOM.createRoot(document.getElementById('drawer-root') as HTMLDivElement);
-    return root.render(<MarkdownEditor
-      resourceId={resourceID}
-      show={true}
-      setSaved={(saved) => {
+    return root.render(<BufferModal
+      resourceID={resourceID}
+      onChange_Saved={(saved) => {
         onChange_Saved && onChange_Saved(saved);
       }}
-      close={() => {
+      onClose={() => {
         resolve(null);
         setTimeout(() => {
           root.unmount();
-        }, 1000);
+        }, 300);
       }}
     />);
   });
 }
 
 export default fResourceMarkdownEditor;
+
+interface BufferModalProps {
+  resourceID: string;
+
+  onChange_Saved?(saved: boolean): void;
+
+  onClose?(): void;
+}
+
+function BufferModal({ resourceID, onChange_Saved, onClose }: BufferModalProps) {
+
+  const [visible, set_visible] = React.useState<boolean>(false);
+
+  AHooks.useMount(() => {
+    setTimeout(() => {
+      set_visible(true);
+    }, 30);
+  });
+
+  return (<MarkdownEditor
+    resourceId={resourceID}
+    show={visible}
+    setSaved={(saved) => {
+      onChange_Saved && onChange_Saved(saved);
+    }}
+    close={() => {
+      set_visible(false);
+      setTimeout(() => {
+        onClose && onClose();
+      }, 300);
+    }}
+  />);
+}
