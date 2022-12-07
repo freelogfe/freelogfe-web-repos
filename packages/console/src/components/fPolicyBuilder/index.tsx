23b6268384c7c1dd6e02a397b5e0e84299e86103
liu-kai-github <liukaiemailchina@gmail.com>
2022-12-07 14:27:43 +0800

    update

---
+++ packages/console/src/components/fPolicyBuilder/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './index.less';
+
+interface fPolicyBuilderProps {
+
+}
+
+function fPolicyBuilder({}: fPolicyBuilderProps) {
+  return (<div>__Template</div>);
+}
+
+export default fPolicyBuilder;
