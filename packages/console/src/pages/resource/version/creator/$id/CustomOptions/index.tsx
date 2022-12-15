b70b2fa9269510814783905da5509ed14225f6b5
liu-kai-github <liukaiemailchina@gmail.com>
2022-12-15 16:56:49 +0800

    Update index.tsx

--- packages/console/src/pages/resource/version/creator/$id/CustomOptions/index.tsx
+++ packages/console/src/pages/resource/version/creator/$id/CustomOptions/index.tsx
@@ -251,8 +251,11 @@ function CustomOptions({ dispatch, resourceVersionCreatorPage }: CustomOptionsPr
             }}
           >
             <span>自定义选项（高级）</span>
-            {customOptionsDataVisible ? (<FComponentsLib.FIcons.FUp />) : (
-              <FComponentsLib.FIcons.FDown />)}
+            {
+              customOptionsDataVisible
+                ? (<FComponentsLib.FIcons.FUp style={{ fontSize: 12 }} />)
+                : (<FComponentsLib.FIcons.FDown style={{ fontSize: 12 }} />)
+            }
           </FComponentsLib.FTextBtn>
           <FTooltip title={FI18n.i18nNext.t('info_versionoptions')}>
             <div><FComponentsLib.FIcons.FInfo style={{ fontSize: 14 }} /></div>
