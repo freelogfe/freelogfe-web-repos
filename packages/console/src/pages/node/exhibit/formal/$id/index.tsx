f10827d090265c681fb430f819c9fa6bea7724f0
liu-kai-github <liukaiemailchina@gmail.com>
2023-01-10 16:30:29 +0800

    Update index.tsx

--- packages/console/src/pages/node/exhibit/formal/$id/index.tsx
+++ packages/console/src/pages/node/exhibit/formal/$id/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from './index.less';
 import FSwitch from '@/components/FSwitch';
-import { Checkbox, Space } from 'antd';
+import { Checkbox, message, Space } from 'antd';
 import Policies from './Policies';
 import Contracts from './Contracts';
 import Viewports from './Viewports';
@@ -43,11 +43,11 @@ function Presentable({ dispatch, exhibitInfoPage, match }: PresentableProps) {
 
   const [urlState] = useUrlState<{ openCreatePolicyDrawer: 'true', openOperatePolicyDrawer: 'true' }>();
 
-  const [activeDialogShow, setActiveDialogShow] = React.useState(false);
-  const [inactiveDialogShow, setInactiveDialogShow] = React.useState(false);
+  // const [activeDialogShow, setActiveDialogShow] = React.useState(false);
+  // const [inactiveDialogShow, setInactiveDialogShow] = React.useState(false);
   const [resultPopupType, setResultPopupType] = React.useState<null | 0 | 1>(null);
   const [loading, setLoading] = React.useState(false);
-  const [noLonger, setNoLonger] = React.useState(false);
+  // const [noLonger, setNoLonger] = React.useState(false);
 
   AHooks.useMount(() => {
     dispatch<OnMountPageAction>({
@@ -99,148 +99,147 @@ function Presentable({ dispatch, exhibitInfoPage, match }: PresentableProps) {
 
     await onlineExhibit(exhibitInfoPage.exhibit_ID);
 
-
     dispatch<FetchInfoAction>({
       type: 'exhibitInfoPage/fetchInfo',
     });
   }
 
   /** 展品上下架 */
-  async function changeExhibitStatus(value: boolean) {
-    if (exhibitInfoPage.side_ResourceType.includes('主题')) {
-
-    }
-    if (value) {
-      if (exhibitInfoPage.policy_List.length === 0) {
-        setActiveDialogShow(true);
-      } else if (!exhibitInfoPage.policy_List.some((item: { status: number }) => item.status === 1)) {
-        // exhibitInfoPage.policy_List.forEach((item: any) => {
-        //   item.checked = false;
-        // });
-        dispatch<ChangeAction>({
-          type: 'exhibitInfoPage/change',
-          payload: {
-            policyOperaterVisible: true,
-          },
-        });
-      } else {
-        const data = { onlineStatus: 1 };
-        upOrDownExhibit(data);
-      }
-    } else {
-      // 下架
-      const resourceNoTip = localStorage.getItem('exhibitNoTip') || false;
-      if (resourceNoTip) {
-        inactiveResource();
-      } else {
-        setNoLonger(false);
-        setInactiveDialogShow(true);
-      }
-    }
-  }
+  // async function changeExhibitStatus(value: boolean) {
+  //   if (exhibitInfoPage.side_ResourceType.includes('主题')) {
+  //
+  //   }
+  //   if (value) {
+  //     if (exhibitInfoPage.policy_List.length === 0) {
+  //       setActiveDialogShow(true);
+  //     } else if (!exhibitInfoPage.policy_List.some((item: { status: number }) => item.status === 1)) {
+  //       // exhibitInfoPage.policy_List.forEach((item: any) => {
+  //       //   item.checked = false;
+  //       // });
+  //       dispatch<ChangeAction>({
+  //         type: 'exhibitInfoPage/change',
+  //         payload: {
+  //           policyOperaterVisible: true,
+  //         },
+  //       });
+  //     } else {
+  //       const data = { onlineStatus: 1 };
+  //       upOrDownExhibit(data);
+  //     }
+  //   } else {
+  //     // 下架
+  //     const resourceNoTip = localStorage.getItem('exhibitNoTip') || false;
+  //     if (resourceNoTip) {
+  //       inactiveResource();
+  //     } else {
+  //       setNoLonger(false);
+  //       setInactiveDialogShow(true);
+  //     }
+  //   }
+  // }
 
   // function activateTheme() {
   //
   // }
 
   /** 打开添加策略弹窗 */
-  function openPolicyBuilder() {
-    dispatch<ChangeAction>({
-      type: 'exhibitInfoPage/change',
-      payload: {
-        policyEditorVisible: true,
-      },
-    });
-    setActiveDialogShow(false);
-  }
+  // function openPolicyBuilder() {
+  //   dispatch<ChangeAction>({
+  //     type: 'exhibitInfoPage/change',
+  //     payload: {
+  //       policyEditorVisible: true,
+  //     },
+  //   });
+  //   setActiveDialogShow(false);
+  // }
 
   /** 上架 */
-  function activeResource() {
-    const updatePolicies = exhibitInfoPage.policy_List
-      .filter((item: any) => item.checked)
-      .map((item: { policyId: string }) => {
-        return { policyId: item.policyId, status: 1 };
-      });
-    const data = { onlineStatus: 1, updatePolicies };
-    upOrDownExhibit(data);
-  }
+  // function activeResource() {
+  //   const updatePolicies = exhibitInfoPage.policy_List
+  //     .filter((item: any) => item.checked)
+  //     .map((item: { policyId: string }) => {
+  //       return { policyId: item.policyId, status: 1 };
+  //     });
+  //   const data = { onlineStatus: 1, updatePolicies };
+  //   upOrDownExhibit(data);
+  // }
 
   /** 下架 */
-  function inactiveResource() {
-    if (inactiveDialogShow && noLonger) {
-      localStorage.setItem('exhibitNoTip', 'true');
-    }
-
-    const data = { onlineStatus: 0 };
-    upOrDownExhibit(data);
-  }
+  // function inactiveResource() {
+  //   if (inactiveDialogShow && noLonger) {
+  //     localStorage.setItem('exhibitNoTip', 'true');
+  //   }
+  //
+  //   const data = { onlineStatus: 0 };
+  //   upOrDownExhibit(data);
+  // }
 
   /** 资源上下架 */
-  async function upOrDownExhibit(data: any) {
-    setActiveDialogShow(false);
-    setInactiveDialogShow(false);
-    setLoading(true);
-    setResultPopupType(data.onlineStatus);
+  // async function upOrDownExhibit(data: any) {
+  //   setActiveDialogShow(false);
+  //   setInactiveDialogShow(false);
+  //   setLoading(true);
+  //   setResultPopupType(data.onlineStatus);
+  //
+  //   const result = await FUtil.Request({
+  //     method: 'PUT',
+  //     url: `/v2/presentables/${match.params.id}/onlineStatus`,
+  //     data,
+  //   });
+  //   if (result.errCode === 0) {
+  //     setTimeout(() => {
+  //       setLoading(false);
+  //       setTimeout(() => {
+  //         setResultPopupType(null);
+  //       }, 1000);
+  //     }, 1000);
+  //
+  //     exhibitInfoPage.exhibit_Online = data.onlineStatus === 1;
+  //     if (data.updatePolicies) {
+  //       dispatch<ChangeAction>({
+  //         type: 'exhibitInfoPage/change',
+  //         payload: {
+  //           policyOperaterVisible: false,
+  //         },
+  //       });
+  //       data.updatePolicies.forEach((item: any) => {
+  //         const i = exhibitInfoPage.policy_List.findIndex(
+  //           (policy) => policy.policyId === item.policyId,
+  //         );
+  //         exhibitInfoPage.policy_List[i].status = 1;
+  //       });
+  //     }
+  //
+  //     dispatch<ChangeAction>({
+  //       type: 'exhibitInfoPage/change',
+  //       payload: {
+  //         policy_List: exhibitInfoPage.policy_List,
+  //       },
+  //     });
+  //   } else {
+  //     fMessage(result.msg, 'error');
+  //     setLoading(false);
+  //     setResultPopupType(null);
+  //   }
+  // }
 
-    const result = await FUtil.Request({
-      method: 'PUT',
-      url: `/v2/presentables/${match.params.id}/onlineStatus`,
-      data,
-    });
-    if (result.errCode === 0) {
-      setTimeout(() => {
-        setLoading(false);
-        setTimeout(() => {
-          setResultPopupType(null);
-        }, 1000);
-      }, 1000);
-
-      exhibitInfoPage.exhibit_Online = data.onlineStatus === 1;
-      if (data.updatePolicies) {
-        dispatch<ChangeAction>({
-          type: 'exhibitInfoPage/change',
-          payload: {
-            policyOperaterVisible: false,
-          },
-        });
-        data.updatePolicies.forEach((item: any) => {
-          const i = exhibitInfoPage.policy_List.findIndex(
-            (policy) => policy.policyId === item.policyId,
-          );
-          exhibitInfoPage.policy_List[i].status = 1;
-        });
-      }
-
-      dispatch<ChangeAction>({
-        type: 'exhibitInfoPage/change',
-        payload: {
-          policy_List: exhibitInfoPage.policy_List,
-        },
-      });
-    } else {
-      fMessage(result.msg, 'error');
-      setLoading(false);
-      setResultPopupType(null);
-    }
-  }
-
-  /** 添加授权策略 */
-  async function addPolicy(title: string, text: string) {
-    dispatch<AddAPolicyAction>({
-      type: 'exhibitInfoPage/addAPolicy',
-      payload: {
-        title,
-        text,
-      },
-    });
-
-    dispatch<ChangeAction>({
-      type: 'exhibitInfoPage/change',
-      payload: {
-        policyEditorVisible: false,
-      },
-    });
-  }
+  // /** 添加授权策略 */
+  // async function addPolicy(title: string, text: string) {
+  //   dispatch<AddAPolicyAction>({
+  //     type: 'exhibitInfoPage/addAPolicy',
+  //     payload: {
+  //       title,
+  //       text,
+  //     },
+  //   });
+  //
+  //   dispatch<ChangeAction>({
+  //     type: 'exhibitInfoPage/change',
+  //     payload: {
+  //       policyEditorVisible: false,
+  //     },
+  //   });
+  // }
 
   if (exhibitInfoPage.pageLoading) {
     return <FLoadingTip height={'calc(100vh - 140px)'} />;
@@ -369,104 +368,104 @@ function Presentable({ dispatch, exhibitInfoPage, match }: PresentableProps) {
       </div>
       <div style={{ height: 100 }} />
 
-      <FDialog
-        show={activeDialogShow}
-        title={FI18n.i18nNext.t('set_resource_available_for_auth_activate_auth_plan_title')}
-        desc={FI18n.i18nNext.t('msg_set_resource_avaliable_for_auth01')}
-        sureText={FI18n.i18nNext.t('set_resource_available_for_auth_btn_create_auth_plan')}
-        cancelText={FI18n.i18nNext.t('btn_cancel')}
-        cancel={() => {
-          setActiveDialogShow(false);
-        }}
-        sure={openPolicyBuilder}
-        loading={loading}
-      />
+      {/*<FDialog*/}
+      {/*  show={activeDialogShow}*/}
+      {/*  title={FI18n.i18nNext.t('set_resource_available_for_auth_activate_auth_plan_title')}*/}
+      {/*  desc={FI18n.i18nNext.t('msg_set_resource_avaliable_for_auth01')}*/}
+      {/*  sureText={FI18n.i18nNext.t('set_resource_available_for_auth_btn_create_auth_plan')}*/}
+      {/*  cancelText={FI18n.i18nNext.t('btn_cancel')}*/}
+      {/*  cancel={() => {*/}
+      {/*    setActiveDialogShow(false);*/}
+      {/*  }}*/}
+      {/*  sure={openPolicyBuilder}*/}
+      {/*  loading={loading}*/}
+      {/*/>*/}
 
-      <FDialog
-        show={inactiveDialogShow}
-        title={FI18n.i18nNext.t('remove_exhibit_from_auth_confirmation_title')}
-        desc={FI18n.i18nNext.t('confirm_msg_remove_exhibits_from_auth')}
-        sureText={FI18n.i18nNext.t('btn_remove_exhibits_from_auth')}
-        cancel={() => {
-          setInactiveDialogShow(false);
-        }}
-        sure={inactiveResource}
-        loading={loading}
-        footer={
-          <Checkbox
-            className={styles['no-longer']}
-            checked={noLonger}
-            onChange={(e) => setNoLonger(e.target.checked)}
-          >
-            {FI18n.i18nNext.t('checkbox_dontaskmeagain')}
-          </Checkbox>
-        }
-      />
+      {/*<FDialog*/}
+      {/*  show={inactiveDialogShow}*/}
+      {/*  title={FI18n.i18nNext.t('remove_exhibit_from_auth_confirmation_title')}*/}
+      {/*  desc={FI18n.i18nNext.t('confirm_msg_remove_exhibits_from_auth')}*/}
+      {/*  sureText={FI18n.i18nNext.t('btn_remove_exhibits_from_auth')}*/}
+      {/*  cancel={() => {*/}
+      {/*    setInactiveDialogShow(false);*/}
+      {/*  }}*/}
+      {/*  sure={inactiveResource}*/}
+      {/*  loading={loading}*/}
+      {/*  footer={*/}
+      {/*    <Checkbox*/}
+      {/*      className={styles['no-longer']}*/}
+      {/*      checked={noLonger}*/}
+      {/*      onChange={(e) => setNoLonger(e.target.checked)}*/}
+      {/*    >*/}
+      {/*      {FI18n.i18nNext.t('checkbox_dontaskmeagain')}*/}
+      {/*    </Checkbox>*/}
+      {/*  }*/}
+      {/*/>*/}
 
-      <FPolicyBuilderDrawer
-        visible={exhibitInfoPage.policyEditorVisible}
-        alreadyUsedTexts={exhibitInfoPage?.policy_List.map((ip: any) => {
-          return ip.policyText;
-        })}
-        alreadyUsedTitles={exhibitInfoPage?.policy_List.map((ip: any) => {
-          return ip.policyName;
-        })}
-        targetType='resource'
-        onCancel={() => {
-          dispatch<ChangeAction>({
-            type: 'exhibitInfoPage/change',
-            payload: {
-              policyEditorVisible: false,
-            },
-          });
-        }}
-        onConfirm={({ title, text }) => {
-          addPolicy(title, text);
-          console.log('***8wofisjd;lfkjsdlfjsdlfjsldfjlsdjflkjflkjsdlkfjlkj');
-          FComponentsLib.fSetHotspotTooltipVisible('exhibitDetailPage.onlineSwitch', {
-            value: true,
-            effectiveImmediately: true,
-            onlyNullish: true,
-          });
+      {/*<FPolicyBuilderDrawer*/}
+      {/*  visible={exhibitInfoPage.policyEditorVisible}*/}
+      {/*  alreadyUsedTexts={exhibitInfoPage?.policy_List.map((ip: any) => {*/}
+      {/*    return ip.policyText;*/}
+      {/*  })}*/}
+      {/*  alreadyUsedTitles={exhibitInfoPage?.policy_List.map((ip: any) => {*/}
+      {/*    return ip.policyName;*/}
+      {/*  })}*/}
+      {/*  targetType='resource'*/}
+      {/*  onCancel={() => {*/}
+      {/*    dispatch<ChangeAction>({*/}
+      {/*      type: 'exhibitInfoPage/change',*/}
+      {/*      payload: {*/}
+      {/*        policyEditorVisible: false,*/}
+      {/*      },*/}
+      {/*    });*/}
+      {/*  }}*/}
+      {/*  onConfirm={({ title, text }) => {*/}
+      {/*    addPolicy(title, text);*/}
+      {/*    console.log('***8wofisjd;lfkjsdlfjsdlfjsldfjlsdjflkjflkjsdlkfjlkj');*/}
+      {/*    FComponentsLib.fSetHotspotTooltipVisible('exhibitDetailPage.onlineSwitch', {*/}
+      {/*      value: true,*/}
+      {/*      effectiveImmediately: true,*/}
+      {/*      onlyNullish: true,*/}
+      {/*    });*/}
 
-          setTimeout(() => {
-            FComponentsLib.fSetHotspotTooltipVisible('exhibitDetailPage.onlineSwitch', {
-              value: false,
-              effectiveImmediately: false,
-              onlyNullish: false,
-            });
-          });
-        }}
-      />
+      {/*    setTimeout(() => {*/}
+      {/*      FComponentsLib.fSetHotspotTooltipVisible('exhibitDetailPage.onlineSwitch', {*/}
+      {/*        value: false,*/}
+      {/*        effectiveImmediately: false,*/}
+      {/*        onlyNullish: false,*/}
+      {/*      });*/}
+      {/*    });*/}
+      {/*  }}*/}
+      {/*/>*/}
 
-      <FPolicyOperatorDrawer
-        visible={exhibitInfoPage.policyOperaterVisible}
-        // visible={true}
-        // type='resource'
-        titleText={exhibitInfoPage.side_ResourceType.includes('主题')
-          ? FI18n.i18nNext.t('activatetheme_activate_authplan_title')
-          : FI18n.i18nNext.t('showexhibit_activate_authplan_title')}
-        confirmText={exhibitInfoPage.side_ResourceType.includes('主题')
-          ? FI18n.i18nNext.t('activatetheme_activate_authplan_btn')
-          : FI18n.i18nNext.t('showexhibit_activate_authplan_btn')}
-        tipText={exhibitInfoPage.side_ResourceType.includes('主题')
-          ? FI18n.i18nNext.t('msg_activate_theme_for_auth')
-          : FI18n.i18nNext.t('msg_set_exhibits_avaliable_for_auth')}
-        policiesList={exhibitInfoPage?.policy_List || []}
-        onCancel={() => {
-          dispatch<FetchInfoAction>({
-            type: 'exhibitInfoPage/fetchInfo',
-          });
-          dispatch<ChangeAction>({
-            type: 'exhibitInfoPage/change',
-            payload: {
-              policyOperaterVisible: false,
-            },
-          });
-        }}
-        onConfirm={activeResource}
-        onNewPolicy={openPolicyBuilder}
-      />
+      {/*<FPolicyOperatorDrawer*/}
+      {/*  visible={exhibitInfoPage.policyOperaterVisible}*/}
+      {/*  // visible={true}*/}
+      {/*  // type='resource'*/}
+      {/*  titleText={exhibitInfoPage.side_ResourceType.includes('主题')*/}
+      {/*    ? FI18n.i18nNext.t('activatetheme_activate_authplan_title')*/}
+      {/*    : FI18n.i18nNext.t('showexhibit_activate_authplan_title')}*/}
+      {/*  confirmText={exhibitInfoPage.side_ResourceType.includes('主题')*/}
+      {/*    ? FI18n.i18nNext.t('activatetheme_activate_authplan_btn')*/}
+      {/*    : FI18n.i18nNext.t('showexhibit_activate_authplan_btn')}*/}
+      {/*  tipText={exhibitInfoPage.side_ResourceType.includes('主题')*/}
+      {/*    ? FI18n.i18nNext.t('msg_activate_theme_for_auth')*/}
+      {/*    : FI18n.i18nNext.t('msg_set_exhibits_avaliable_for_auth')}*/}
+      {/*  policiesList={exhibitInfoPage?.policy_List || []}*/}
+      {/*  onCancel={() => {*/}
+      {/*    dispatch<FetchInfoAction>({*/}
+      {/*      type: 'exhibitInfoPage/fetchInfo',*/}
+      {/*    });*/}
+      {/*    dispatch<ChangeAction>({*/}
+      {/*      type: 'exhibitInfoPage/change',*/}
+      {/*      payload: {*/}
+      {/*        policyOperaterVisible: false,*/}
+      {/*      },*/}
+      {/*    });*/}
+      {/*  }}*/}
+      {/*  onConfirm={activeResource}*/}
+      {/*  onNewPolicy={openPolicyBuilder}*/}
+      {/*/>*/}
 
       {
         resultPopupType !== null && (
@@ -614,11 +613,27 @@ async function onlineExhibit(exhibit_ID: string): Promise<boolean> {
     await FServiceAPI.Exhibit.updatePresentable(params1);
   }
 
+  const messageKey: number = 888999;
+  message.loading({
+    content: isTheme
+      ? FI18n.i18nNext.t('set_resource_available_for_auth_msg_processing')
+      : FI18n.i18nNext.t('remove_resource_from_auth_msg_processing'),
+    key: messageKey,
+  });
+
+
   const params2: Parameters<typeof FServiceAPI.Exhibit.presentablesOnlineStatus>[0] = {
     presentableId: exhibit_ID,
     onlineStatus: 1,
   };
   await FServiceAPI.Exhibit.presentablesOnlineStatus(params2);
 
+  message.success({
+    content: isTheme
+      ? FI18n.i18nNext.t('msg_done ')
+      : FI18n.i18nNext.t('set_resource_available_for_auth_msg_done'),
+    key: messageKey,
+    duration: 2,
+  });
   return true;
 }
