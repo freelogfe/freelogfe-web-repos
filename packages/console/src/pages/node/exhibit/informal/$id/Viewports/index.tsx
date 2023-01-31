abb6743ff02729fffae90ac4935b287bacb776e6
liu-kai-github <liukaiemailchina@gmail.com>
2023-01-31 17:47:53 +0800

    update

--- packages/console/src/pages/node/exhibit/informal/$id/Viewports/index.tsx
+++ packages/console/src/pages/node/exhibit/informal/$id/Viewports/index.tsx
@@ -16,6 +16,7 @@ import { ConnectState } from '@/models/connect';
 import FResultTip from '@/components/FResultTip';
 import { FI18n } from '@freelog/tools-lib';
 import FComponentsLib from '@freelog/components-lib';
+import FViewportCards_Exhibit from '@/components/FAntvG6/FViewportCards_Exhibit';
 
 interface ViewportsProps {
   dispatch: Dispatch;
@@ -41,140 +42,85 @@ function Viewports({ dispatch, informExhibitInfoPage }: ViewportsProps) {
   return (<div>
     <div className={styles.title}>
       <FComponentsLib.FTitleText text={FI18n.i18nNext.t('title_exhibit_maps')} type='h3' />
-      <FComponentsLib.FTextBtn
-        type='default'
-        onClick={() => {
-          dispatch<OnClick_Graph_FullScreenBtn_Action>({
-            type: 'informExhibitInfoPage/onClick_Graph_FullScreenBtn',
-          });
-        }}
-      >全屏查看</FComponentsLib.FTextBtn>
+      {/*<FComponentsLib.FTextBtn*/}
+      {/*  type='default'*/}
+      {/*  onClick={() => {*/}
+      {/*    dispatch<OnClick_Graph_FullScreenBtn_Action>({*/}
+      {/*      type: 'informExhibitInfoPage/onClick_Graph_FullScreenBtn',*/}
+      {/*    });*/}
+      {/*  }}*/}
+      {/*>全屏查看</FComponentsLib.FTextBtn>*/}
     </div>
     <div style={{ height: 20 }} />
-    <FViewportTabs
-      // options={[
-      //   { value: 'authorization', label: '授权链视图' },
-      //   { value: 'dependency', label: '授权链视图' },
-      // ]}
-      options={tabOptions}
-      value={informExhibitInfoPage.graph_Viewport_Show}
-      onChange={(value: 'relationship') => {
-        // onChange({ graph_Viewport_Show: value as 'relationship' });
-        dispatch<OnChange_Graph_Tab_Action>({
-          type: 'informExhibitInfoPage/onChange_Graph_Tab',
-          payload: {
-            value: value,
-          },
-        });
-      }}
-    >
-      {
-        informExhibitInfoPage.graph_FullScreen
-          ? (<div style={{ height: 500 }} />)
-          : (<>
+    {/*<FViewportCards_Exhibit />*/}
+    {/*<FViewportTabs*/}
+    {/*  // options={[*/}
+    {/*  //   { value: 'authorization', label: '授权链视图' },*/}
+    {/*  //   { value: 'dependency', label: '授权链视图' },*/}
+    {/*  // ]}*/}
+    {/*  options={tabOptions}*/}
+    {/*  value={informExhibitInfoPage.graph_Viewport_Show}*/}
+    {/*  onChange={(value: 'relationship') => {*/}
+    {/*    // onChange({ graph_Viewport_Show: value as 'relationship' });*/}
+    {/*    dispatch<OnChange_Graph_Tab_Action>({*/}
+    {/*      type: 'informExhibitInfoPage/onChange_Graph_Tab',*/}
+    {/*      payload: {*/}
+    {/*        value: value,*/}
+    {/*      },*/}
+    {/*    });*/}
+    {/*  }}*/}
+    {/*>*/}
+    {/*  {*/}
+    {/*    informExhibitInfoPage.graph_FullScreen*/}
+    {/*      ? (<div style={{ height: 500 }} />)*/}
+    {/*      : (<>*/}
 
             <div style={{
-              height: 500,
+              height: 220,
               display: 'flex',
               alignItems: 'center',
               justifyContent: 'center',
             }}>
               <FResultTip h1={'即将推出，敬请期待...'} />
             </div>
+    {/*      </>)*/}
+    {/*  }*/}
+    {/*</FViewportTabs>*/}
 
-            {/*{*/}
-            {/*  informExhibitInfoPage.graph_Viewport_Show === 'relationship' && (<FAntvG6RelationshipGraph*/}
-            {/*    nodes={informExhibitInfoPage.graph_Viewport_RelationGraph_Nodes}*/}
-            {/*    edges={informExhibitInfoPage.graph_Viewport_RelationGraph_Edges}*/}
-            {/*    width={860}*/}
-            {/*  />)*/}
-            {/*}*/}
+    {/*<FDrawer*/}
+    {/*  open={informExhibitInfoPage.graph_FullScreen}*/}
+    {/*  title={'相关视图'}*/}
+    {/*  destroyOnClose*/}
+    {/*  width={'100%'}*/}
+    {/*  onClose={() => {*/}
+    {/*    dispatch<OnCancel_Graph_FullScreenDrawer_Action>({*/}
+    {/*      type: 'informExhibitInfoPage/onCancel_Graph_FullScreenDrawer',*/}
+    {/*    });*/}
+    {/*  }}*/}
+    {/*>*/}
 
-
-            {/*{*/}
-            {/*  informExhibitInfoPage.graph_Viewport_Show === 'authorization' && (<FAntvG6AuthorizationGraph*/}
-            {/*    nodes={informExhibitInfoPage.graph_Viewport_AuthorizationGraph_Nodes}*/}
-            {/*    edges={informExhibitInfoPage.graph_Viewport_AuthorizationGraph_Edges}*/}
-            {/*    width={860}*/}
-            {/*  />)*/}
-            {/*}*/}
-
-
-            {/*{*/}
-            {/*  informExhibitInfoPage.graph_Viewport_Show === 'dependency' && (<FAntvG6DependencyGraph*/}
-            {/*    nodes={informExhibitInfoPage.graph_Viewport_DependencyGraph_Nodes}*/}
-            {/*    edges={informExhibitInfoPage.graph_Viewport_DependencyGraph_Edges}*/}
-            {/*    width={860}*/}
-            {/*  />)*/}
-            {/*}*/}
-
-
-          </>)
-      }
-    </FViewportTabs>
-
-    <FDrawer
-      open={informExhibitInfoPage.graph_FullScreen}
-      title={'相关视图'}
-      destroyOnClose
-      width={'100%'}
-      onClose={() => {
-        dispatch<OnCancel_Graph_FullScreenDrawer_Action>({
-          type: 'informExhibitInfoPage/onCancel_Graph_FullScreenDrawer',
-        });
-      }}
-    >
-
-      <FViewportTabs
-        options={tabOptions}
-        value={informExhibitInfoPage.graph_Viewport_Show}
-        onChange={(value: 'relationship') => {
-          dispatch<OnChange_Graph_Tab_Action>({
-            type: 'informExhibitInfoPage/onChange_Graph_Tab',
-            payload: {
-              value: value,
-            },
-          });
-        }}
-      >
-        <div style={{
-          height: window.innerHeight - 60 - 70 - 50,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-          <FResultTip h1={'即将推出，敬请期待...'} />
-        </div>
-        )
-        {/*{*/}
-        {/*  informExhibitInfoPage.graph_Viewport_Show === 'relationship' && (<FAntvG6RelationshipGraph*/}
-        {/*    nodes={informExhibitInfoPage.graph_Viewport_RelationGraph_Nodes}*/}
-        {/*    edges={informExhibitInfoPage.graph_Viewport_RelationGraph_Edges}*/}
-        {/*    width={window.innerWidth - 60}*/}
-        {/*    height={window.innerHeight - 60 - 70 - 50}*/}
-        {/*  />)*/}
-        {/*}*/}
-
-        {/*{*/}
-        {/*  informExhibitInfoPage.graph_Viewport_Show === 'authorization' && (<FAntvG6AuthorizationGraph*/}
-        {/*    nodes={informExhibitInfoPage.graph_Viewport_AuthorizationGraph_Nodes}*/}
-        {/*    edges={informExhibitInfoPage.graph_Viewport_AuthorizationGraph_Edges}*/}
-        {/*    width={window.innerWidth - 60}*/}
-        {/*    height={window.innerHeight - 60 - 70 - 50}*/}
-        {/*  />)*/}
-        {/*}*/}
-
-        {/*{*/}
-        {/*  informExhibitInfoPage.graph_Viewport_Show === 'dependency' && (<FAntvG6DependencyGraph*/}
-        {/*    nodes={informExhibitInfoPage.graph_Viewport_DependencyGraph_Nodes}*/}
-        {/*    edges={informExhibitInfoPage.graph_Viewport_DependencyGraph_Edges}*/}
-        {/*    width={window.innerWidth - 60}*/}
-        {/*    height={window.innerHeight - 60 - 70 - 50}*/}
-        {/*  />)*/}
-        {/*}*/}
-
-      </FViewportTabs>
-    </FDrawer>
+    {/*  <FViewportTabs*/}
+    {/*    options={tabOptions}*/}
+    {/*    value={informExhibitInfoPage.graph_Viewport_Show}*/}
+    {/*    onChange={(value: 'relationship') => {*/}
+    {/*      dispatch<OnChange_Graph_Tab_Action>({*/}
+    {/*        type: 'informExhibitInfoPage/onChange_Graph_Tab',*/}
+    {/*        payload: {*/}
+    {/*          value: value,*/}
+    {/*        },*/}
+    {/*      });*/}
+    {/*    }}*/}
+    {/*  >*/}
+    {/*    <div style={{*/}
+    {/*      height: window.innerHeight - 60 - 70 - 50,*/}
+    {/*      display: 'flex',*/}
+    {/*      alignItems: 'center',*/}
+    {/*      justifyContent: 'center',*/}
+    {/*    }}>*/}
+    {/*      <FResultTip h1={'即将推出，敬请期待...'} />*/}
+    {/*    </div>*/}
+    {/*  </FViewportTabs>*/}
+    {/*</FDrawer>*/}
 
   </div>);
 }
