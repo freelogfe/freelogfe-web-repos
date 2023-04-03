50b45f43a0dde2e9c046bf9a83b42a460a2eb5d7
liu-kai-github <liukaiemailchina@gmail.com>
2023-04-03 17:59:01 +0800

    udpate

---
+++ packages/console/src/components/FResourceTypeFilter/index.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import styles from './index.less';
+import { Cascader } from 'antd';
+import * as AHooks from 'ahooks';
+import { FServiceAPI } from '@freelog/tools-lib';
+
+interface FResourceTypeFilterProps {
+  value: Array<string | number> | undefined;
+
+  onChange?(value: FResourceTypeFilterProps['value']): void;
+}
+
+interface Option {
+  value: string | number;
+  label: string;
+  children?: Option[];
+}
+
+interface ServerData {
+  code: string;
+  name: string;
+  children: ServerData[];
+}
+
+function FResourceTypeFilter({ value, onChange }: FResourceTypeFilterProps) {
+
+  const [options, set_options] = React.useState<Option[]>([]);
+
+  AHooks.useMount(async () => {
+    const { data: data_resourceTypes }: {
+      data: ServerData[];
+    } = await FServiceAPI.Resource.resourceTypes();
+    // console.log(data_resourceTypes, 'data_resourceTypessiodjdflkjsdlkjflksdjlk');
+    const options: Option[] = handledData(data_resourceTypes);
+    set_options(options);
+  });
+
+  AHooks.useUnmount(() => {
+
+  });
+
+  return (<Cascader
+    allowClear={true}
+    value={value}
+    options={options}
+    onChange={(value: Array<string | number> | undefined, selectedOptions) => {
+      // console.log(value, selectedOptions, 'value, selectedOptions sdi8ofjsdlkfjsldkfjlkj');
+      onChange && onChange(value);
+    }}
+    placeholder='Please select'
+  />);
+}
+
+export default FResourceTypeFilter;
+
+
+function handledData(data: ServerData[]): Option[] {
+  return data.map((d) => {
+    return {
+      value: d.code,
+      label: d.name,
+      children: handledData(d.children),
+    };
+  });
+}
