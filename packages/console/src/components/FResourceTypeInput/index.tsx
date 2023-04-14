1e69e6935f96ed3e0884d9cf5176da7b1c47db14
liu-kai-github <liukaiemailchina@gmail.com>
2023-04-14 11:17:13 +0800

    udpate

--- packages/console/src/components/FResourceTypeInput/index.tsx
+++ packages/console/src/components/FResourceTypeInput/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styles from './index.less';
 import { Cascader, Dropdown } from 'antd';
 import * as AHooks from 'ahooks';
-import { FServiceAPI } from '@freelog/tools-lib';
+import { FI18n, FServiceAPI } from '@freelog/tools-lib';
 import FComponentsLib from '@freelog/components-lib';
 
 interface Option {
@@ -24,9 +24,20 @@ interface FResourceTypeInputProps {
   onChange?(value: FResourceTypeInputProps['value'], selectedOptions: Option[]): void;
 }
 
+interface FResourceTypeInputStates {
+  options: Option[];
+  isOpen: boolean;
+}
+
+const initStates: FResourceTypeInputStates = {
+  options: [],
+  isOpen: false,
+};
+
 function FResourceTypeInput({ value, useKey = 'code', onChange }: FResourceTypeInputProps) {
 
-  const [options, set_options] = React.useState<Option[]>([]);
+  const [options, set_options] = React.useState<FResourceTypeInputStates['options']>(initStates['options']);
+  const [isOpen, set_isOpen] = React.useState<FResourceTypeInputStates['isOpen']>(initStates['isOpen']);
 
   AHooks.useMount(async () => {
     const { data: data_resourceTypes }: {
@@ -41,10 +52,43 @@ function FResourceTypeInput({ value, useKey = 'code', onChange }: FResourceTypeI
 
   });
 
-  return (<div className={styles.square}>
-    <span>选择类型</span>
-    <FComponentsLib.FIcons.FDown style={{ fontSize: 12 }} />
-  </div>);
+  return (<Dropdown
+    onOpenChange={(o) => {
+      set_isOpen(o);
+    }}
+    overlayClassName={styles.overlayClassName}
+    overlay={(<div className={styles.overlay}
+    >
+      <div className={styles.recommend}>
+        <FComponentsLib.FContentText
+          text={FI18n.i18nNext.t('createresource_selectresourcetype_input_hint2')}
+          type={'additional2'}
+        />
+        <div style={{ height: 20 }} />
+        <div className={styles.recommendResourceTypes}>
+          <label>阅读/文本</label>
+          <label>阅读/演示文稿</label>
+          <label>音频/有声书</label>
+          <label>音频/播客</label>
+          <label>视频/长视频</label>
+          <label>视频/短视频</label>
+        </div>
+      </div>
+    </div>)}>
+    <div className={styles.square} style={{ borderColor: isOpen ? '#2784FF' : '#D4D4D4' }}>
+      <span>选择类型</span>
+      {
+        isOpen
+          ? (<FComponentsLib.FIcons.FUp style={{ fontSize: 12 }} />)
+          : (<FComponentsLib.FIcons.FDown style={{ fontSize: 12 }} />)
+      }
+    </div>
+    <FComponentsLib.FContentText
+      text={FI18n.i18nNext.t('createresource_selectresourcetype_input_hint3')}
+      type={'additional2'}
+    />
+    <div style={{ height: 20 }} />
+  </Dropdown>);
 
   // return (<Cascader
   //   allowClear={true}
