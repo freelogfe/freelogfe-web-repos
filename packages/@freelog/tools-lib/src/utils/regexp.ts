73dc00ec905d94b6dd95accc83a2da3981c8c300
liu-kai-github <liukaiemailchina@gmail.com>
2023-05-23 17:36:27 +0800

    Update regexp.ts

--- packages/@freelog/tools-lib/src/utils/regexp.ts
+++ packages/@freelog/tools-lib/src/utils/regexp.ts
@@ -43,7 +43,8 @@ export const NATURAL_NUMBER: RegExp = new RegExp(/^[0-9]*$/);
 export const POSITIVE_INTEGER = new RegExp(/^[1-9]\d*$/);
 
 // 最多两位小数的正数
-export const MAX_2_DECIMAL_POSITIVE_NUMBER = new RegExp(/^\d+(.\d{1,2})?$/);
+// export const MAX_2_DECIMAL_POSITIVE_NUMBER = new RegExp(/^\d+(.\d{1,2})?$/);
+export const MAX_2_DECIMAL_POSITIVE_NUMBER = new RegExp(/^\d+(\.\d{1,2})?$/);
 
 // 对象的Bucket名称
 export const BUCKET_NAME: RegExp = new RegExp(/^([a-z0-9][a-z0-9-]{0,61})?[a-z0-9]$/);
