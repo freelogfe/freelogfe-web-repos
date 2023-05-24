deac61288eb6709ee3014683ad969dd6735d2409
liu-kai-github <liukaiemailchina@gmail.com>
2023-05-24 15:38:26 +0800

    Update index.tsx

--- packages/console/src/components/FBraftEditor/index.tsx
+++ packages/console/src/components/FBraftEditor/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import styles from './index.less';
-import BraftEditor, {BraftEditorProps, EditorState} from 'braft-editor';
+import BraftEditor, { BraftEditorProps, EditorState } from 'braft-editor';
 import 'braft-editor/dist/index.css';
-import {connect} from "dva";
-import {ConnectState, GlobalModelState} from "@/models/connect";
-import {FServiceAPI} from "@freelog/tools-lib";
-import {CSSProperties} from "react";
+import { connect } from 'dva';
+import { ConnectState, GlobalModelState } from '@/models/connect';
+import { FServiceAPI } from '@freelog/tools-lib';
+import { CSSProperties } from 'react';
+import fMessage from '@/components/fMessage';
 
 interface FBraftEditorProps extends BraftEditorProps {
   global: GlobalModelState;
@@ -16,7 +17,7 @@ interface FBraftEditorProps extends BraftEditorProps {
   onChange?(value: EditorState): void;
 }
 
-function FBraftEditor({global, value, onChange, ...props}: FBraftEditorProps) {
+function FBraftEditor({ global, value, onChange, ...props }: FBraftEditorProps) {
 
   return (
     <BraftEditor
@@ -39,8 +40,15 @@ function FBraftEditor({global, value, onChange, ...props}: FBraftEditorProps) {
           const params: Parameters<typeof FServiceAPI.Storage.uploadImage>[0] = {
             file: fileParams.file,
           };
-          const {data} = await FServiceAPI.Storage.uploadImage(params);
+          const { ret, errCode, msg, data } = await FServiceAPI.Storage.uploadImage(params);
           // fileParams.progress();
+          if (ret !== 0 || errCode !== 0) {
+            fMessage(msg, 'error');
+            fileParams.error({
+              msg,
+            });
+            return;
+          }
           fileParams.success({
             url: data.url,
             meta: {
@@ -51,10 +59,11 @@ function FBraftEditor({global, value, onChange, ...props}: FBraftEditorProps) {
               autoPlay: false,
               controls: false,
               poster: '',
-            }
+            },
           });
         },
         validateFn(file) {
+          // console.log(file, 'validateFn98898987897897897897');
           return true;
         },
         accepts: {
@@ -76,6 +85,6 @@ function FBraftEditor({global, value, onChange, ...props}: FBraftEditorProps) {
 }
 
 
-export default connect(({global}: ConnectState) => ({
+export default connect(({ global }: ConnectState) => ({
   global: global,
 }))(FBraftEditor);
