2b71da5d4776b1604ec871e1bb509f2ae49ea4de
liu-kai-github <liukaiemailchina@gmail.com>
2023-06-07 16:17:37 +0800

    update

--- packages/console/src/components/FExhibitLabelEditor/index.tsx
+++ packages/console/src/components/FExhibitLabelEditor/index.tsx
@@ -38,75 +38,80 @@ function FExhibitLabelEditor($prop: FExhibitLabelEditorProps) {
   }, [$prop.value]);
 
   return (<div>
-    <FComponentsLib.FInput.FSingleLine
-      ref={inputElementRef}
-      size={'small'}
-      lengthLimit={-1}
-      value={$state.input}
-      placeholder={FI18n.i18nNext.t('hint_add_resource_tag')}
-      className={[styles.Input].join(' ')}
-      onChange={(e) => {
-        const value: string = e.target.value.replaceAll('#', '');
-        // set_input(value);
-        $setState({
-          input: value,
-        });
-
-        let errorText: string = '';
-        if (value.length > 20) {
-          errorText = '不超过20个字符';
-        } else if ($prop.value.includes(value)) {
-          errorText = '不能有重复';
-        }
-        // set_errorText(errorText);
-        $setState({
-          inputError: errorText,
-        });
-      }}
-      onPressEnter={() => {
-        if ($state.inputError !== '') {
-          return;
-        }
-        if ($state.input === '') {
-          // onChangeInput('');
-          // onChangeErrorText('');
-          inputElementRef.current?.blur();
-          return;
-        }
-        // if ($state.input === '') {
-        //   $setState({
-        //     inputError: '不能为空',
-        //   });
-        //   return;
-        // }
-        $setState({
-          input: '',
-        });
-        $prop.onChange && $prop.onChange([
-          ...$prop.value,
-          $state.input.replace(new RegExp(/#/, 'g'), ''),
-        ]);
-      }}
-      onKeyUp={(event) => {
-        if (event.key === 'Escape') {
-          // set_input('');
-          // set_errorText('');
-          $setState({
-            input: '',
-            inputError: '',
-          });
-          inputElementRef.current?.blur();
-        }
-      }}
-    />
     {
-      $state.inputError !== '' && (<>
-        <div style={{ height: 5 }} />
-        <div className={styles.errorTip}>{$state.inputError}</div>
+      $prop.value.length < 20 && (<>
+        <FComponentsLib.FInput.FSingleLine
+          ref={inputElementRef}
+          size={'small'}
+          lengthLimit={-1}
+          value={$state.input}
+          placeholder={FI18n.i18nNext.t('hint_add_resource_tag')}
+          className={[styles.Input].join(' ')}
+          onChange={(e) => {
+            const value: string = e.target.value.replaceAll('#', '');
+            // set_input(value);
+            $setState({
+              input: value,
+            });
+
+            let errorText: string = '';
+            if (value.length > 20) {
+              errorText = '不超过20个字符';
+            } else if ($prop.value.includes(value)) {
+              errorText = '不能有重复';
+            }
+            // set_errorText(errorText);
+            $setState({
+              inputError: errorText,
+            });
+          }}
+          onPressEnter={() => {
+            if ($state.inputError !== '') {
+              return;
+            }
+            // if ($state.input === '') {
+            //   // onChangeInput('');
+            //   // onChangeErrorText('');
+            //   inputElementRef.current?.blur();
+            //   return;
+            // }
+            if ($state.input === '') {
+              $setState({
+                inputError: '不能为空',
+              });
+              return;
+            }
+            $setState({
+              input: '',
+            });
+            $prop.onChange && $prop.onChange([
+              ...$prop.value,
+              $state.input.replace(new RegExp(/#/, 'g'), ''),
+            ]);
+          }}
+          onKeyUp={(event) => {
+            if (event.key === 'Escape') {
+              // set_input('');
+              // set_errorText('');
+              $setState({
+                input: '',
+                inputError: '',
+              });
+              inputElementRef.current?.blur();
+            }
+          }}
+        />
+        {
+          $state.inputError !== '' && (<>
+            <div style={{ height: 5 }} />
+            <div className={styles.errorTip}>{$state.inputError}</div>
+          </>)
+        }
+
+        <div style={{ height: 20 }} />
       </>)
     }
 
-    <div style={{ height: 20 }} />
     <div className={styles.selectedLabels}>
       {
         $prop.value.map((v, w) => {
