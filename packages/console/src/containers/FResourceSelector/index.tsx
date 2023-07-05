9e1508fdea4e9ce27c44c990da8de5d94a9ab128
liu-kai-github <liukaiemailchina@gmail.com>
2023-07-05 10:59:41 +0800

    Update index.tsx

--- packages/console/src/containers/FResourceSelector/index.tsx
+++ packages/console/src/containers/FResourceSelector/index.tsx
@@ -24,9 +24,6 @@ interface FResourceSelectorProps {
   onSelect?({ id, name }: { id: string; name: string; }): void;
 
   onDelete?({ id, name }: { id: string; name: string; }): void;
-
-  // dispatch: Dispatch;
-  // storageObjectDepSelector: StorageObjectDepSelectorModelState;
 }
 
 interface FResourceSelectorStates {
@@ -40,58 +37,27 @@ interface FResourceSelectorStates {
   }[];
   rPageSize: number;
   rTotal: number;
-  rSelect: '1' | '2' | '3';
+  rSelect: 'market' | 'my' | 'collection';
   rInput: string;
-
-  // visibleOResourceType: string;
-  // objectList: {
-  //   objectId: string;
-  //   bucketName: string;
-  //   objectName: string;
-  //   resourceType: string[];
-  //   updateDate: string;
-  // }[];
-  // oPageSize: number;
-  // oTotal: number;
-  // oSelect: '_all' | string;
-  // oInput: string;
 }
 
 const initState: FResourceSelectorStates = {
   resourceList: [],
   rTotal: -1,
   rPageSize: 20,
-  rSelect: '1',
+  rSelect: 'market',
   rInput: '',
-  // visibleOResourceType: '',
-  // objectList: [],
-  // oTotal: -1,
-  // oPageSize: 20,
-  // oSelect: '_all',
-  // oInput: '',
 };
 
-const selectOptions: { text?: string, value: string }[] = [
-  { text: '资源市场', value: '1' },
-  { text: '我的资源', value: '2' },
-  { text: '我的收藏', value: '3' },
+const selectOptions: { text: string, value: FResourceSelectorStates['rSelect'] }[] = [
+  { text: '资源市场', value: 'market' },
+  { text: '我的资源', value: 'my' },
+  { text: '我的收藏', value: 'collection' },
 ];
 
-function FResourceSelector({
-                             disabledIDsOrNames, showRemoveIDsOrNames, onSelect, onDelete,
-                             // dispatch, storageObjectDepSelector,
-                           }: FResourceSelectorProps) {
+function FResourceSelector($prop: FResourceSelectorProps) {
 
   const [$state, $setState] = AHooks.useSetState<FResourceSelectorStates>(initState);
-
-  // React.useEffect(() => {
-  // if (selector.rTotal === -1) {
-  // dispatch<FetchResourcesAction>({
-  //   type: 'storageObjectDepSelector/fetchResources',
-  //   payload: true,
-  // });
-  // }
-  // }, []);
 
   AHooks.useMount(() => {
     fetchResources(true);
@@ -110,7 +76,7 @@ function FResourceSelector({
     }
     // let dataSource: any;
     let totalItem: number = -1;
-    if ($state.rSelect === '3') {
+    if ($state.rSelect === 'collection') {
       const params: Parameters<typeof FServiceAPI.Collection.collectionResources>[0] = {
         skip: resourceList.length,
         // limit: storageObjectDepSelector.rPageSize,
@@ -139,8 +105,8 @@ function FResourceSelector({
         // limit: storageObjectDepSelector.rPageSize,
         limit: FUtil.Predefined.pageSize,
         keywords: $state.rInput,
-        status: $state.rSelect === '2' ? undefined : 1,
-        isSelf: $state.rSelect === '2' ? 1 : undefined,
+        status: $state.rSelect === 'my' ? undefined : 1,
+        isSelf: $state.rSelect === 'my' ? 1 : undefined,
       };
       const { data } = await FServiceAPI.Resource.list(params);
       // dataSource = data;
@@ -165,95 +131,19 @@ function FResourceSelector({
     });
   }
 
-  // async function fetchObjects(restart: boolean = false) {
-  //   let objectListData: StorageObjectDepSelectorModelState['objectList'] = [];
-  //
-  //   if (!restart) {
-  //     objectListData = $state.objectList;
-  //   }
-  //
-  //   const params: Parameters<typeof FServiceAPI.Storage.objectList>[0] = {
-  //     bucketName: $state.oSelect,
-  //     resourceType: $state.visibleOResourceType || undefined,
-  //     isLoadingTypeless: 1,
-  //     keywords: $state.oInput,
-  //     skip: objectListData.length,
-  //     // limit: selector.oPageSize,
-  //     limit: FUtil.Predefined.pageSize,
-  //   };
-  //   const { data } = await FServiceAPI.Storage.objectList(params);
-  //   // console.log(data, 'datadata322');
-  //
-  //   $setState({
-  //     oTotal: data.totalItem,
-  //     objectList: [
-  //       ...objectListData,
-  //       ...data.dataList.map((o: any) => ({
-  //         objectId: o.objectId,
-  //         bucketName: o.bucketName,
-  //         objectName: o.objectName,
-  //         resourceType: o.resourceType,
-  //         updateDate: FUtil.Format.formatDateTime(o.updateDate, true),
-  //       })),
-  //     ],
-  //   });
-  //   // yield put<ChangeAction>({
-  //   //   type: 'change',
-  //   //   payload: {
-  //   //
-  //   //   },
-  //   // });
-  // }
-
-  // function onChangeRConditions(payload: Partial<Pick<FResourceSelectorStates, 'rSelect' | 'rInput'>>) {
-  //   // yield put<ChangeAction>({
-  //   //   type: 'change',
-  //   //   payload: {
-  //   //     ...payload,
-  //   //   },
-  //   // });
-  //   $setState({
-  //     ...payload,
-  //   });
-  //
-  //   yield put<FetchResourcesAction>({
-  //     type: 'fetchResources',
-  //     payload: true,
-  //   });
-  // }
-
   return (<>
     <div className={styles.filter}>
       <FDropdownMenu
         options={selectOptions}
         onChange={(value) => {
-          // dispatch<OnChangeRConditionsAction>({
-          //   type: 'storageObjectDepSelector/onChangeRConditions',
-          //   payload: {
-          //     rSelect: value as ('1' | '2' | '3'),
-          //   },
-          // });
           $setState({
-            rSelect: value as '1',
+            rSelect: value as 'market',
           });
         }}
       >
         <a>{(selectOptions.find((op) => op.value === $state.rSelect) as any).text}
           <FComponentsLib.FIcons.FDown style={{ marginLeft: 8, fontSize: 12 }} /></a>
       </FDropdownMenu>
-      {/*<FInput*/}
-      {/*  theme='dark'*/}
-      {/*  debounce={300}*/}
-      {/*  value={storageObjectDepSelector.rInput}*/}
-      {/*  onDebounceChange={(value) => {*/}
-      {/*    dispatch<OnChangeRConditionsAction>({*/}
-      {/*      type: 'storageObjectDepSelector/onChangeRConditions',*/}
-      {/*      payload: {*/}
-      {/*        rInput: value,*/}
-      {/*      },*/}
-      {/*    });*/}
-      {/*  }}*/}
-      {/*/>*/}
       <FComponentsLib.FInput.FSearch
         value={$state.rInput}
         onChange={(e) => {
@@ -263,10 +153,9 @@ function FResourceSelector({
         }}
       />
     </div>
-    {/*{console.log(storageObjectDepSelector.resourceList, 'selector.resourceList@!@#$@!#$@#$!234234')}*/}
     <FResourceList
-      showRemoveIDsOrNames={showRemoveIDsOrNames}
-      disabledIDsOrNames={disabledIDsOrNames}
+      showRemoveIDsOrNames={$prop.showRemoveIDsOrNames}
+      disabledIDsOrNames={$prop.disabledIDsOrNames}
       resourceObjects={$state.resourceList.map((r) => ({
         id: r.resourceId,
         title: r.resourceName,
@@ -278,22 +167,16 @@ function FResourceSelector({
       loading={$state.rTotal === -1}
       stillMore={$state.resourceList.length < $state.rTotal}
       onSelect={(value) => {
-        onSelect && onSelect({ id: value.id, name: value.title });
+        $prop.onSelect && $prop.onSelect({ id: value.id, name: value.title });
       }}
       onDelete={(value) => {
-        onDelete && onDelete({ id: value.id, name: value.title });
+        $prop.onDelete && $prop.onDelete({ id: value.id, name: value.title });
       }}
       onLoadMord={() => {
-        // dispatch<FetchResourcesAction>({
-        //   type: 'storageObjectDepSelector/fetchResources',
-        //   payload: false,
-        // });
         fetchResources(false);
       }}
     />
   </>);
 }
 
-export default connect(({ storageObjectDepSelector }: ConnectState) => ({
-  storageObjectDepSelector: storageObjectDepSelector,
-}))(FResourceSelector);
+export default FResourceSelector;
