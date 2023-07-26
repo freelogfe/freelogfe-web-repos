5116f979bbd95f2fc66da21b7b64f9f91fc1e797
liu-kai-github <liukaiemailchina@gmail.com>
2023-07-26 17:25:08 +0800

    update

--- packages/console/src/components/fResourcePropertyEditor/FResourcePropertyEditorDrawer/index.tsx
+++ packages/console/src/components/fResourcePropertyEditor/FResourcePropertyEditorDrawer/index.tsx
@@ -289,10 +289,12 @@ function FResourcePropertyEditorDrawer({
   }
 
   function onVerify_dataTime() {
-    const timeRegex: RegExp = new RegExp(/^((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))\\s+([0-1]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/);
+    // const timeRegex: RegExp = new RegExp(/^((([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29))\\s+([0-1]?[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/);
+    const timeRegex: RegExp = new RegExp(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/);
     const valueInput = get$valueInput();
     const formatString = 'YYYY-MM-DD hh:mm:ss';
     const valueFormat = get$valueFormat();
+    // console.log(valueInput, timeRegex, 'valueInputsdifjsdlkfjl ');
     if (valueInput === '' || !timeRegex.test(valueInput)) {
       set$valueInput('');
       $setState({
