aa8f45d4e4bbdbff1569a5083a1c2f4e6e16e10a
liu-kai-github <liukaiemailchina@gmail.com>
2023-08-16 16:22:20 +0800

    Update index.tsx

--- packages/console/src/pages/resource/creator/Step3/index.tsx
+++ packages/console/src/pages/resource/creator/Step3/index.tsx
@@ -6,7 +6,10 @@ import { ConnectState, ResourceCreatorPageModelState } from '@/models/connect';
 import { Dispatch } from 'redux';
 // import { OnClick_step3_skipBtn_Action } from '@/models/resourceCreatorPage';
 import { history } from '@@/core/history';
-import { FUtil } from '@freelog/tools-lib';
+import { FI18n, FUtil } from '@freelog/tools-lib';
+import FPolicyList from '@/components/FPolicyList';
+import fResourceOptionEditor from '@/components/fResourceOptionEditor';
+import { OnChange_step2_customConfigurations_Action } from '@/models/resourceCreatorPage';
 
 // import { FI18n } from '@freelog/tools-lib';
 
@@ -19,23 +22,46 @@ function Step3({ dispatch, resourceCreatorPage }: Step3Props) {
   return (<>
     <div style={{ height: 40 }} />
     <div className={styles.block}>
-      <FComponentsLib.FContentText text={'授权策略'} type={'highlight'} />
-      {/*<FComponentsLib.FContentText text={FI18n.i18nNext.t('authplanmgnt_title')} type={'highlight'}/>*/}
-      <div style={{ height: 10 }} />
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <FComponentsLib.FContentText text={'资源需添加授权策略才能上架到资源市场，开放签约。'} type={'additional2'} />
-        <FComponentsLib.FContentText text={'授权策略，即资源对外授权范围和条件的申明，如支付10元获得一个月使用授权。'} type={'additional2'} />
-        <div style={{ height: 20 }} />
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <FComponentsLib.FContentText text={'授权策略'} type={'highlight'} />
+        {/*<FComponentsLib.FContentText text={FI18n.i18nNext.t('authplanmgnt_title')} type={'highlight'}/>*/}
+        <FComponentsLib.FTextBtn
+          style={{ fontSize: 12, fontWeight: 600, display: 'flex', alignItems: 'center', gap: 5 }}
+          type='primary'
+          onClick={async () => {
 
-        {/*{FI18n.i18nNext.t('authplanmgnt_list_empty_btn')}*/}
-        <FComponentsLib.FRectBtn
-          disabled={true}
-          type={'primary'}
-          onClick={() => {
-
-          }}
-        >添加授权策略</FComponentsLib.FRectBtn>
+          }}>
+          <FComponentsLib.FIcons.FConfiguration style={{ fontSize: 14 }} />
+          <span>添加策略</span>
+        </FComponentsLib.FTextBtn>
       </div>
+      <>
+        <div style={{ height: 10 }} />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <FComponentsLib.FContentText text={'资源需添加授权策略才能上架到资源市场，开放签约。'} type={'additional2'} />
+          <FComponentsLib.FContentText text={'授权策略，即资源对外授权范围和条件的申明，如支付10元获得一个月使用授权。'} type={'additional2'} />
+          <div style={{ height: 20 }} />
+
+          {/*{FI18n.i18nNext.t('authplanmgnt_list_empty_btn')}*/}
+          <FComponentsLib.FRectBtn
+            type={'primary'}
+            onClick={() => {
+
+            }}
+          >添加授权策略</FComponentsLib.FRectBtn>
+        </div>
+      </>
+
+      {/*<FPolicyList*/}
+      {/*  atLeastOneUsing={resourceAuthPage.status === 1}*/}
+      {/*  dataSource={resourceAuthPage.policies}*/}
+      {/*  onCheckChange={(data) => {*/}
+      {/*    if (data.using) {*/}
+      {/*      self._czc?.push(['_trackEvent', '授权信息页', '上线', '', 1]);*/}
+      {/*    }*/}
+      {/*    onPolicyStatusChange(data.id, data.using);*/}
+      {/*  }}*/}
+      {/*/>*/}
     </div>
 
     <div style={{ height: 5 }} />
@@ -75,7 +101,7 @@ function Step3({ dispatch, resourceCreatorPage }: Step3Props) {
 
       {/*{FI18n.i18nNext.t('rqr_step3_btn_next')}*/}
       <FComponentsLib.FRectBtn
-        disabled={true}
+        disabled={false}
         type={'primary'}
         onClick={() => {
 
