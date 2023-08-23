f1a797a0c788ae2f8dc36dadbb6ad11760f3504b
liu-kai-github <liukaiemailchina@gmail.com>
2023-08-23 16:58:47 +0800

    update

--- packages/console/src/pages/resource/sidebar/policy/$id/index.tsx
+++ packages/console/src/pages/resource/sidebar/policy/$id/index.tsx
@@ -8,13 +8,19 @@ import * as AHooks from 'ahooks';
 import { withRouter } from 'umi';
 import { connect } from 'dva';
 import { ConnectState } from '@/models/connect';
+import FComponentsLib from '../../../../../../../@freelog/components-lib';
+import { FI18n, FUtil } from '../../../../../../../@freelog/tools-lib';
+import { OnClick_step3_addPolicyBtn_Action, OnClick_step3_submitBtn_Action } from '@/models/resourceCreatorPage';
+import FPolicyList from '@/components/FPolicyList';
+import PolicyTemplates from '@/pages/resource/creator/Step3/PolicyTemplates';
+import { history } from '@@/core/history';
 
 interface PolicyProps extends RouteComponentProps<{ id: string }> {
   dispatch: Dispatch;
   resourceAuthPage: ResourceAuthPageModelState;
 }
 
-function Policy({dispatch, resourceAuthPage, match}: PolicyProps) {
+function Policy({ dispatch, resourceAuthPage, match }: PolicyProps) {
   AHooks.useMount(async () => {
     dispatch<OnMount_Sidebar_Action>({
       type: 'resourceSider/onMount_Page',
@@ -24,7 +30,100 @@ function Policy({dispatch, resourceAuthPage, match}: PolicyProps) {
     });
   });
 
-  return (<div>__Template</div>);
+  return (<>
+    <div>
+      <div style={{ height: 40 }} />
+      <div className={styles.block}>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          {/*<FComponentsLib.FContentText text={'授权策略'} type={'highlight'} />*/}
+          <FComponentsLib.FContentText text={FI18n.i18nNext.t('authplanmgnt_title')} type={'highlight'} />
+          {
+            resourceAuthPage.policies.length > 0 && (<FComponentsLib.FTextBtn
+              style={{ fontSize: 12, fontWeight: 600, display: 'flex', alignItems: 'center', gap: 5 }}
+              type='primary'
+              onClick={async () => {
+                // dispatch<OnClick_step3_addPolicyBtn_Action>({
+                //   type: 'resourceCreatorPage/onClick_step3_addPolicyBtn',
+                //   payload: {},
+                // });
+              }}>
+              <FComponentsLib.FIcons.FConfiguration style={{ fontSize: 14 }} />
+              <span>添加策略</span>
+            </FComponentsLib.FTextBtn>)
+          }
+
+        </div>
+        {
+          resourceAuthPage.policies.length === 0 && (<>
+            <div style={{ height: 10 }} />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <FComponentsLib.FContentText text={'资源需添加授权策略才能上架到资源市场，开放签约。'} type={'additional2'} />
+              <FComponentsLib.FContentText text={'授权策略，即资源对外授权范围和条件的申明，如支付10元获得一个月使用授权。'} type={'additional2'} />
+              <div style={{ height: 20 }} />
+
+              {/*{FI18n.i18nNext.t('authplanmgnt_list_empty_btn')}*/}
+              <FComponentsLib.FRectBtn
+                type={'primary'}
+                onClick={() => {
+                  // dispatch<OnClick_step3_addPolicyBtn_Action>({
+                  //   type: 'resourceCreatorPage/onClick_step3_addPolicyBtn',
+                  //   payload: {},
+                  // });
+                }}
+              >{FI18n.i18nNext.t('authplanmgnt_list_empty_btn')}</FComponentsLib.FRectBtn>
+            </div>
+          </>)
+        }
+
+        {
+          resourceAuthPage.policies.length > 0 && (<>
+            <div style={{ height: 20 }} />
+            <FPolicyList
+              allDisabledSwitch={true}
+              dataSource={resourceAuthPage.policies}
+              // onCheckChange={(data) => {
+              //   if (data.using) {
+              //     self._czc?.push(['_trackEvent', '授权信息页', '上线', '', 1]);
+              //   }
+              //   onPolicyStatusChange(data.id, data.using);
+              // }}
+            />
+          </>)
+        }
+
+      </div>
+
+      <div style={{ height: 5 }} />
+
+      <div className={styles.block}>
+        <FComponentsLib.FContentText
+          text={'策略模板'}
+          type={'highlight'}
+        />
+        {/*<FComponentsLib.FContentText text={FI18n.i18nNext.t('authplanmgnt_title_templates')} type={'highlight'}/>*/}
+        <div style={{ height: 5 }} />
+        {/*<FComponentsLib.FContentText*/}
+        {/*  text={'点击下方推荐的策略模板，可以快速添加策略'}*/}
+        {/*  type={'additional2'} />*/}
+        <FComponentsLib.FContentText text={FI18n.i18nNext.t('authplanmgnt_title_templates_help')} type={'additional2'} />
+        <div style={{ height: 20 }} />
+        <PolicyTemplates
+          onSelect={(value) => {
+            // fPolicyBuilder({ targetType: 'resource', defaultValue: value });
+            // dispatch<OnClick_step3_addPolicyBtn_Action>({
+            //   type: 'resourceCreatorPage/onClick_step3_addPolicyBtn',
+            //   payload: {
+            //     defaultValue: value,
+            //   },
+            // });
+          }}
+        />
+      </div>
+
+      <div style={{ height: 100 }} />
+
+    </div>
+  </>);
 }
 
 export default withRouter(connect(({ resourceAuthPage }: ConnectState) => ({
