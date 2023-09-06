f72857a41716b7c9bcb496e2a3c328dd825d3c69
liu-kai-github <liukaiemailchina@gmail.com>
2023-09-06 17:16:39 +0800

    update

--- packages/www/src/pages/activity/$id/ExperienceOfficer/index.tsx
+++ packages/www/src/pages/activity/$id/ExperienceOfficer/index.tsx
@@ -4,6 +4,10 @@ import { ActivityDetailsPageModelState } from '@/models/activityDetailsPage';
 import * as AHooks from 'ahooks';
 import { connect } from 'dva';
 import { ConnectState } from '@/models/connect';
+import img_banner from '@/assets/activity/ExperienceOfficer/banner@2x.png';
+import { Affix, Popover } from 'antd';
+import FComponentsLib from '@freelog/components-lib';
+import FPropaganda from '@/components/FPropaganda';
 
 interface ExperienceOfficerProps {
   activityDetailsPage: ActivityDetailsPageModelState;
@@ -14,7 +18,50 @@ function ExperienceOfficer({}: ExperienceOfficerProps) {
     self._czc?.push(['_trackPageview', self.location.pathname]);
   });
 
-  return (<div>ExperienceOfficer</div>);
+  return (<div>
+    <img className={styles.banner} src={img_banner} />
+    <Affix offsetTop={70}>
+      <div className={styles.AffixContent}>
+
+      </div>
+    </Affix>
+    <div className={styles.content}>
+      <div style={{ height: 40 }} />
+      <div className={styles.activityTime}>
+        <div className={styles.activityTimeCard}></div>
+        <div className={styles.activityTimeCard}></div>
+        <div className={styles.activityTimeCard}></div>
+      </div>
+      <div style={{ height: 50 }} />
+      <div className={styles.award}>
+
+      </div>
+      <div style={{ height: 50 }} />
+      <div className={styles.questionnaire}>
+
+      </div>
+
+      <div style={{ height: 50 }} />
+      <div className={styles.point}>
+
+      </div>
+
+      <div style={{ height: 50 }} />
+      <div className={styles.regulation}>
+
+      </div>
+      <div style={{ height: 100 }} />
+
+    </div>
+    <FPropaganda style={{ backgroundColor: '#fff' }} />
+    <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+      <div style={{ border: '1px solid #979797', width: 680, opacity: .15 }} />
+    </div>
+    <FComponentsLib.FPageFooter
+      PopoverPatch={Popover}
+      style={{}}
+    />
+  </div>);
 }
 
 export default connect(({ activityDetailsPage }: ConnectState) => ({
