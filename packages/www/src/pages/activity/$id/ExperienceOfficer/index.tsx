5059352ac5b5a0c57c1d716412ec2eaa47753881
liu-kai-github <liukaiemailchina@gmail.com>
2023-09-07 14:21:00 +0800

    update

--- packages/www/src/pages/activity/$id/ExperienceOfficer/index.tsx
+++ packages/www/src/pages/activity/$id/ExperienceOfficer/index.tsx
@@ -7,6 +7,9 @@ import { ConnectState } from '@/models/connect';
 import img_banner from '@/assets/activity/ExperienceOfficer/banner@2x.png';
 import img_award1 from '@/assets/activity/ExperienceOfficer/award1@2x.png';
 import img_award2 from '@/assets/activity/ExperienceOfficer/award2@2x.png';
+import img_questionnaire1 from '@/assets/activity/ExperienceOfficer/questionnaire1@2x.png';
+import img_questionnaire2 from '@/assets/activity/ExperienceOfficer/questionnaire2@2x.png';
+import img_questionnaire3 from '@/assets/activity/ExperienceOfficer/questionnaire3@2x.png';
 import { Affix, Popover, Space } from 'antd';
 import FComponentsLib from '@freelog/components-lib';
 import FPropaganda from '@/components/FPropaganda';
@@ -103,7 +106,42 @@ function ExperienceOfficer({}: ExperienceOfficerProps) {
       </div>
       <div style={{ height: 50 }} />
       <div className={styles.questionnaire}>
-
+        <div style={{ height: 60 }} />
+        <div className={styles.h1}>填写体验官招募问卷，获参与资格</div>
+        <div style={{ height: 10 }} />
+        <div className={styles.h2}>首批体验官名额仅限200名，先到先得！</div>
+        <div style={{ height: 40 }} />
+        <div style={{ width: 640, display: 'flex', justifyContent: 'space-between' }}>
+          <img style={{ width: 184 }} src={img_questionnaire1} alt={''} />
+          <img style={{ width: 184 }} src={img_questionnaire2} alt={''} />
+        </div>
+        <div style={{ height: 20 }} />
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: 730 }}>
+          <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+            <FComponentsLib.FContentText text={'招募漫画、小说/文章、图片/插画、'} type={'highlight'} />
+            <FComponentsLib.FContentText text={'音乐/音效/播客、视频等内容创作者及游戏、'} type={'highlight'} />
+            <FComponentsLib.FContentText text={'主题、插件开发者'} type={'highlight'} />
+          </div>
+          <div>
+            <FComponentsLib.FContentText text={'参与方式：填写招募问卷即可报名参与内测'} type={'highlight'} />
+          </div>
+        </div>
+        <div style={{ height: 40 }} />
+        <a className={styles.button}>立即成为首批体验官</a>
+        <div style={{ height: 60 }} />
+        <div className={styles.longDashed} />
+        <div style={{ height: 60 }} />
+        <div className={styles.h1}>3步玩转体验官积分，赢¥2000现金大奖</div>
+        <div style={{ height: 10 }} />
+        <div className={styles.h2}>参与体验官积分活动，获得体验官专属福利，更有机会赢取现金大奖</div>
+        <div style={{ height: 50 }} />
+        <img style={{ width: 748 }} src={img_questionnaire3} alt={''} />
+        <div style={{ height: 50 }} />
+        <Space size={30}>
+          <a className={styles.button}>查看教程快速上手</a>
+          <a className={styles.button}>了解体验官积分规则</a>
+        </Space>
+        <div style={{ height: 60 }} />
 
       </div>
 
