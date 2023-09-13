ff1dba70d7e04722707271049c9c16feda7012db
liu-kai-github <liukaiemailchina@gmail.com>
2023-09-13 16:37:58 +0800

    update

--- packages/console/src/pages/result/resource/create/success/$id/index.tsx
+++ packages/console/src/pages/result/resource/create/success/$id/index.tsx
@@ -4,9 +4,11 @@ import FCenterLayout from '@/layouts/FCenterLayout';
 import { withRouter, history } from 'umi';
 import { connect } from 'dva';
 import { Dispatch } from 'redux';
-import { FUtil, FI18n } from '@freelog/tools-lib';
+import { FUtil, FI18n, FServiceAPI } from '@freelog/tools-lib';
 import { RouteComponentProps } from 'react-router';
 import FComponentsLib from '@freelog/components-lib';
+import FCoverImage from '@/components/FCoverImage';
+import * as AHooks from 'ahooks';
 
 interface SuccessProps extends RouteComponentProps<{ id: string; }> {
   dispatch: Dispatch;
@@ -14,34 +16,82 @@ interface SuccessProps extends RouteComponentProps<{ id: string; }> {
 
 function Success({ match, dispatch }: SuccessProps) {
 
-  // React.useEffect(() => {
-  //   dispatch<ChangeAction>({
-  //     type: 'global/change',
-  //     payload: {
-  //       route: route,
-  //     },
-  //   });
-  // }, [route]);
+  const [$resourceInfo, set$resourceInfo] = FUtil.Hook.useGetState<null | {
+    cover: string;
+    name: string;
+    type: string[];
+  }>(null);
+
+  AHooks.useMount(async () => {
+    const { ret, errCode, msg, data } = await FServiceAPI.Resource.info({ resourceIdOrName: match.params.id });
+    // console.log(data, 'DDD9wieojflksdjflksdjlfkjlkj');
+    set$resourceInfo({
+      cover: data.coverImages[0] || '',
+      name: data.resourceName,
+      type: data.resourceType,
+    });
+  });
+
 
   function goto() {
-    self._czc?.push(['_trackEvent', '资源创建成功页', '创建新版本', '', 1]);
+    // self._czc?.push(['_trackEvent', '资源创建成功页', '创建新版本', '', 1]);
     history.replace(FUtil.LinkTo.resourceVersionInfo({
       resourceID: match.params.id,
     }));
   }
 
-  return (<FCenterLayout>
-    <div style={{ height: 100 }} />
-    <div className={styles.modal}>
-      <i className={'freelog fl-icon-shenqingchenggong'} />
-      <div style={{ height: 20 }} />
-      <FComponentsLib.FTipText type='second' text={FI18n.i18nNext.t('resource_created_successfully')} />
-      <div style={{ height: 40 }} />
-      <FComponentsLib.FTipText type='third' text={FI18n.i18nNext.t('hint_create_1st_version')} />
-      <div style={{ height: 20 }} />
-      <FComponentsLib.FRectBtn onClick={goto}>{FI18n.i18nNext.t('create_first_version')}</FComponentsLib.FRectBtn>
+  return (<div className={styles.Success}>
+    <div style={{ flexGrow: 2 }} />
+    <FComponentsLib.FIcons.FCheck style={{ fontSize: 76 }} />
+    <div style={{ height: 30 }} />
+    <div style={{
+      color: '#666',
+      lineHeight: '42px',
+      fontSize: 30,
+    }}>{FI18n.i18nNext.t('resource_created_successfully')}</div>
+    <div style={{ height: 60 }} />
+    <div className={styles.resourceInfo}>
+      <FCoverImage src={''} width={68} />
+      <div>
+        <FComponentsLib.FContentText
+          text={$resourceInfo?.name || ''}
+          type={'highlight'}
+          style={{ fontSize: 16, maxWidth: 265 }}
+          singleRow={true}
+        />
+        <div style={{ height: 10 }} />
+        <FComponentsLib.FContentText text={$resourceInfo?.type.join('/')} type={'additional2'} />
+      </div>
     </div>
-  </FCenterLayout>);
+    <div style={{ height: 60 }} />
+    <div className={styles.buttons}>
+      <FComponentsLib.FTextBtn
+        type={'primary'}
+        onClick={() => {
+          history.replace(FUtil.LinkTo.resourceDetails({
+            resourceID: match.params.id,
+          }));
+        }}
+      >查看资源详情</FComponentsLib.FTextBtn>
+      <FComponentsLib.FTextBtn
+        type={'primary'}
+        onClick={() => {
+          history.replace(FUtil.LinkTo.resourceVersionInfo({
+            resourceID: match.params.id,
+          }));
+        }}
+      >管理我的资源</FComponentsLib.FTextBtn>
+      <FComponentsLib.FTextBtn
+        type={'primary'}
+        onClick={() => {
+          history.replace(FUtil.LinkTo.resourceCreator());
+        }}
+      >继续创建资源</FComponentsLib.FTextBtn>
+    </div>
+    {/*<FComponentsLib.FRectBtn onClick={goto}>{FI18n.i18nNext.t('create_first_version')}</FComponentsLib.FRectBtn>*/}
+    <div style={{ flexGrow: 3 }} />
+
+  </div>);
 }
 
 export default withRouter(connect()(Success));
