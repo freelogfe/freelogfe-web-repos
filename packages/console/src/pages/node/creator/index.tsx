000f80e60843b3457358ddfbe1c5ef1604c4f4c8
liu-kai-github <liukaiemailchina@gmail.com>
2023-10-24 10:56:08 +0800

    Update index.tsx

--- packages/console/src/pages/node/creator/index.tsx
+++ packages/console/src/pages/node/creator/index.tsx
@@ -24,7 +24,7 @@ interface NodeCreatorProps {
 }
 
 function NodeCreator({ nodeCreatorPage, dispatch }: NodeCreatorProps) {
-  console.log(moment(FI18n.i18nNext.t('event_contest_eventperiod_end')).format('YYYY-MM-DD'), 'sdiofjsdlkfjlskdjflkjlkjl');
+  // console.log(moment(FI18n.i18nNext.t('event_contest_eventperiod_end')).format('YYYY-MM-DD'), 'sdiofjsdlkfjlskdjflkjlkjl');
   AHooks.useMount(() => {
     self._czc?.push(['_trackPageview', self.location.pathname]);
     dispatch<OnMount_Page_Action>({
