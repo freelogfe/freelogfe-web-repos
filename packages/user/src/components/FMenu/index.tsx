6de5c7742a32e5b2495d7e683a7e836597a011fd
liu-kai-github <liukaiemailchina@gmail.com>
2023-11-01 10:42:48 +0800

    update

--- packages/user/src/components/FMenu/index.tsx
+++ packages/user/src/components/FMenu/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import styles from './index.less';
-import {Menu} from 'antd';
+import { Menu } from 'antd';
 
 interface FMenuItem {
-  text?: React.ReactNode;
+  text?: string;
   value: string;
 }
 
@@ -13,23 +13,32 @@ export interface FMenuProps {
   onClick?: (value: string) => void;
 }
 
-function FMenu({options, value, onClick}: FMenuProps) {
-  return (
-    <Menu
-      selectable={false}
-      className={styles.Menu}
-      mode="vertical"
-      onClick={(param: any) => onClick && onClick(param.key)}
-    >
-      {
-        (options || [])
-          .map((i: FMenuItem) => (<Menu.Item
-            key={i.value}
-            className={styles.MenuItem + ' ' + (value === i.value ? styles.active : '')}
-          >{i.text || i.value}</Menu.Item>))
-      }
-    </Menu>
-  );
+function FMenu({ options, value, onClick }: FMenuProps) {
+
+  // const items = [
+  //   { label: '菜单项一', key: 'item-1' }, // 菜单项务必填写 key
+  //   { label: '菜单项二', key: 'item-2' },
+  //   {
+  //     label: '子菜单',
+  //     key: 'submenu',
+  //     children: [{ label: '子菜单项', key: 'submenu-item-1' }],
+  //   },
+  // ];
+
+  const items = options.map((o) => {
+    return {
+      key: o.value,
+      label: o.text || o.value,
+    };
+  });
+
+  return (<Menu
+    selectable={false}
+    className={styles.Menu}
+    mode='vertical'
+    onClick={(param: any) => onClick && onClick(param.key)}
+    items={items}
+  />);
 }
 
 export default FMenu;
