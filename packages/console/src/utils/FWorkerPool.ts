237ab7604669537df974b34219fc3d1d114b8045
liu-kai-github <liukaiemailchina@gmail.com>
2023-11-23 14:32:19 +0800

    Update FWorkerPool.ts

--- packages/console/src/utils/FWorkerPool.ts
+++ packages/console/src/utils/FWorkerPool.ts
@@ -1,29 +1,29 @@
-class FWorkerPool {
-
-  private readonly _scriptURL: string;
-  private _worker: Worker | null = null;
-  private _referenceCount: number = 0;
-
-  constructor(scriptURL: string) {
-    this._scriptURL = scriptURL;
-  }
-
-  public getWorker(this: FWorkerPool): Worker {
-    if (!this._worker) {
-      this._worker = new Worker(this._scriptURL);
-    }
-    this._referenceCount++;
-    return this._worker;
-  }
-
-  public terminate(this: FWorkerPool): void {
-    this._referenceCount--;
-
-    if (!!this._worker && this._referenceCount === 0) {
-      this._worker.terminate();
-      this._worker = null;
-    }
-  }
-}
-
-export default FWorkerPool;
+// class FWorkerPool {
+//
+//   private readonly _scriptURL: string;
+//   private _worker: Worker | null = null;
+//   private _referenceCount: number = 0;
+//
+//   constructor(scriptURL: string) {
+//     this._scriptURL = scriptURL;
+//   }
+//
+//   public getWorker(this: FWorkerPool): Worker {
+//     if (!this._worker) {
+//       this._worker = new Worker(this._scriptURL);
+//     }
+//     this._referenceCount++;
+//     return this._worker;
+//   }
+//
+//   public terminate(this: FWorkerPool): void {
+//     this._referenceCount--;
+//
+//     if (!!this._worker && this._referenceCount === 0) {
+//       this._worker.terminate();
+//       this._worker = null;
+//     }
+//   }
+// }
+//
+// export default FWorkerPool;
