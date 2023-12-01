950d33c35c49156a6c4c3ce6ac72612ef2fc867a
liu-kai-github <liukaiemailchina@gmail.com>
2023-12-01 15:02:04 +0800

    Update index.tsx

--- packages/console/src/pages/resource/creatorBatch/index.tsx
+++ packages/console/src/pages/resource/creatorBatch/index.tsx
@@ -9,6 +9,9 @@ import Finish from './Finish';
 import * as AHooks from 'ahooks';
 import { Dispatch } from 'redux';
 import { OnMount_Page_Action, OnUnmount_Page_Action } from '@/models/resourceCreatorBatchPage';
+import { history } from '@@/core/history';
+import { OnChange_IsOpenCartoon_Action } from '@/models/resourceVersionCreatorPage';
+import FPrompt from '@/components/FPrompt';
 
 interface CreatorBatchProps {
   dispatch: Dispatch;
@@ -29,21 +32,32 @@ function CreatorBatch({ dispatch, resourceCreatorBatchPage }: CreatorBatchProps)
     });
   });
 
+  return (<>
+    <FPrompt
+      watch={resourceCreatorBatchPage.resourceListInfo.length > 0}
+      messageText={'还没有保存，现在离开会导致信息丢失'}
+      onOk={(locationHref) => {
+        history.push(locationHref);
+      }}
+    />
 
-  if (resourceCreatorBatchPage.showPage === 'resourceType') {
-    return (<ResourceType />);
-  }
+    {
+      resourceCreatorBatchPage.showPage === 'resourceType' && (<ResourceType />)
+    }
 
+    {
+      resourceCreatorBatchPage.showPage === 'uploadFile' && (<UploadFile />)
+    }
 
-  if (resourceCreatorBatchPage.showPage === 'uploadFile') {
-    return (<UploadFile />);
-  }
+    {
+      resourceCreatorBatchPage.showPage === 'resourceList' && (<ResourceList />)
+    }
 
-  if (resourceCreatorBatchPage.showPage === 'resourceList') {
-    return (<ResourceList />);
-  }
+    {
+      resourceCreatorBatchPage.showPage === 'finish' && (<Finish />)
+    }
 
-  return <Finish />;
+  </>);
 }
 
 export default connect(({ resourceCreatorBatchPage }: ConnectState) => ({
