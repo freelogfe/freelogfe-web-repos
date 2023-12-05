6baf3c54414d2f37ac28f3f5c8f348a6063ff169
liu-kai-github <liukaiemailchina@gmail.com>
2023-12-05 15:26:54 +0800

    Update index.tsx

--- packages/console/src/pages/resource/creatorBatch/UploadFile/index.tsx
+++ packages/console/src/pages/resource/creatorBatch/UploadFile/index.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react';
 import styles from './index.less';
 import FComponentsLib from '@freelog/components-lib';
-import { connect } from 'dva';
-import { ConnectState, ResourceCreatorBatchPageState, ResourceVersionCreatorPageModelState } from '@/models/connect';
-// import { FServiceAPI, FUtil } from '@freelog/tools-lib';
-// import { RcFile } from 'antd/lib/upload/interface';
-// import fReadLocalFiles from '@/components/fReadLocalFiles';
-// import { Modal } from 'antd';
-// import Task from './Task';
-// import * as AHooks from 'ahooks';
-import { Dispatch } from 'redux';
-// import { ChangeAction } from '@/models/resourceCreatorBatchPage';
-// import { getFilesSha1Info } from '@/utils/service';
-// import fObjectSelectorDrawer from '@/components/fObjectSelectorDrawer';
-// import fObjectsSelectorDrawer from '@/components/fObjectsSelectorDrawer';
-// import fMessage from '@/components/fMessage';
+import { FI18n } from '../../../../../../@freelog/tools-lib';
 
 interface UploadFileProps {
   onLocalUpload?(): void;
@@ -26,32 +13,35 @@ function UploadFile({ onLocalUpload, onImportStorage }: UploadFileProps) {
   return (<div className={styles.container2}>
     <div style={{ height: 35 }} />
     <div className={styles.nav}>
-      <div className={styles.left}>批量发行资源</div>
+      <div className={styles.left}>{FI18n.i18nNext.t('brr_title_bulkreleaseresource')}</div>
       <div style={{ width: 10 }} />
       <div className={styles.other}>{'>'}</div>
       <div style={{ width: 7 }} />
-      <div className={styles.other}>上传资源文件</div>
+      <div className={styles.other}>{FI18n.i18nNext.t('brr_submitresource_title')}</div>
     </div>
     <div style={{ height: 35 }} />
     <div className={styles.cards}>
       <div className={styles.localUpload}>
         <FComponentsLib.FIcons.FLocalUpload style={{ fontSize: 60 }} />
         <div style={{ height: 40 }} />
-        <FComponentsLib.FContentText text={'选择本地文件作为发行对象'} type={'additional2'} />
+        <FComponentsLib.FContentText
+          text={FI18n.i18nNext.t('brr_submitresource_upload_msg')}
+          type={'additional2'}
+        />
         <div style={{ height: 40 }} />
         <FComponentsLib.FRectBtn
           type={'primary'}
           onClick={async () => {
             onLocalUpload && onLocalUpload();
           }}
-        >本地上传</FComponentsLib.FRectBtn>
+        >{FI18n.i18nNext.t('brr_submitresource_btn_uploadfromlocal')}</FComponentsLib.FRectBtn>
       </div>
 
       <div className={styles.storageSpace}>
         <FComponentsLib.FIcons.FStorageSpace style={{ fontSize: 60 }} />
         <div style={{ height: 40 }} />
         <FComponentsLib.FContentText
-          text={'选择存储空间对象作为发行对象'}
+          text={FI18n.i18nNext.t('brr_submitresource_importfromstorage_msg')}
           type={'additional2'}
         />
         <div style={{ height: 40 }} />
@@ -60,7 +50,7 @@ function UploadFile({ onLocalUpload, onImportStorage }: UploadFileProps) {
           onClick={async () => {
             onImportStorage && onImportStorage();
           }}
-        >存储空间导入</FComponentsLib.FRectBtn>
+        >{FI18n.i18nNext.t('brr_submitresource_btn_importfromstorage')}</FComponentsLib.FRectBtn>
       </div>
     </div>
 
