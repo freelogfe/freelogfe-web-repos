63f3383ad2173e3cec0abb3b8e131d1e22dd83d6
liu-kai-github <liukaiemailchina@gmail.com>
2023-12-05 15:17:47 +0800

    Update index.tsx

--- packages/console/src/pages/resource/creatorEntry/index.tsx
+++ packages/console/src/pages/resource/creatorEntry/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 import FComponentsLib from '@freelog/components-lib';
 import { Space } from 'antd';
 import { history } from 'umi';
-import { FUtil } from '@freelog/tools-lib';
+import { FI18n, FUtil } from '@freelog/tools-lib';
 
 interface CreatorEntryProps {
 
@@ -13,12 +13,15 @@ function creatorEntry({}: CreatorEntryProps) {
   return (<div className={styles.creatorEntry}>
     <div className={styles.card}>
       <FComponentsLib.FIcons.FPolicy className={styles.icon} />
-      <FComponentsLib.FContentText type={'additional2'} text={'一次发行一个资源，更详细'} />
+      <FComponentsLib.FContentText
+        type={'additional2'}
+        text={FI18n.i18nNext.t('addnewresource_option_single_msg')}
+      />
       <FComponentsLib.FRectBtn
         onClick={() => {
           history.push(FUtil.LinkTo.resourceCreator());
         }}
-      >发行一个资源</FComponentsLib.FRectBtn>
+      >{FI18n.i18nNext.t('addnewresource_option_single_btn')}</FComponentsLib.FRectBtn>
     </div>
     <div className={styles.card}>
       <Space size={15}>
@@ -26,12 +29,15 @@ function creatorEntry({}: CreatorEntryProps) {
         <FComponentsLib.FIcons.FPolicy className={styles.icon} />
         <FComponentsLib.FIcons.FPolicy className={styles.icon} />
       </Space>
-      <FComponentsLib.FContentText type={'additional2'} text={'一次发行多个资源，更便捷'} />
+      <FComponentsLib.FContentText
+        type={'additional2'}
+        text={FI18n.i18nNext.t('addnewresource_option_bulk_msg')}
+      />
       <FComponentsLib.FRectBtn
         onClick={() => {
           history.push(FUtil.LinkTo.resourceCreatorBatch());
         }}
-      >批量发行资源</FComponentsLib.FRectBtn>
+      >{FI18n.i18nNext.t('addnewresource_option_bulk_one')}</FComponentsLib.FRectBtn>
 
     </div>
   </div>);
