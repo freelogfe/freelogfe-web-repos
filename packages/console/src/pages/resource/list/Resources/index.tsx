00fc0c0d6b8bef93fd82163967a61171a2ecd451
liu-kai-github <liukaiemailchina@gmail.com>
2023-12-05 14:44:27 +0800

    Update index.tsx

--- packages/console/src/pages/resource/list/Resources/index.tsx
+++ packages/console/src/pages/resource/list/Resources/index.tsx
@@ -211,7 +211,7 @@ function Resources({ dispatch, resourceListPage }: ResourceProps) {
             <div className={styles.batchHandle}>
               <div className={styles.batchHandleLeft}>
                 <Checkbox
-                  checked={resourceListPage.checkedResourceIDs.length === resourceListPage.resource_List.filter((r) => {
+                  checked={resourceListPage.checkedResourceIDs.length !== 0 && resourceListPage.checkedResourceIDs.length === resourceListPage.resource_List.filter((r) => {
                     return r.status !== 2;
                   }).length}
                   indeterminate={resourceListPage.checkedResourceIDs.length !== 0
