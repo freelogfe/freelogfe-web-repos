1d5a4f197cd999f26cdfd8eb41dfc22c62dc0f5b
liu-kai-github <liukaiemailchina@gmail.com>
2024-01-12 16:02:03 +0800

    Update index.tsx

--- packages/console/src/pages/resource/creatorBatch/index.tsx
+++ packages/console/src/pages/resource/creatorBatch/index.tsx
@@ -1,14 +1,14 @@
-// import * as React from 'react';
-// import styles from './index.less';
-// import { connect } from 'dva';
-// import { ConnectState, ResourceCreatorBatchPageState, ResourceVersionCreatorPageModelState } from '@/models/connect';
-// import ResourceType from './ResourceType';
+import * as React from 'react';
+import styles from './index.less';
+import { connect } from 'dva';
+import { ConnectState, ResourceCreatorBatchPageState } from '@/models/connect';
+import ResourceType from './ResourceType';
 // import UploadFile from './UploadFile';
 // import ResourceList from './ResourceList';
-// import Finish from './Finish';
-// import * as AHooks from 'ahooks';
-// import { Dispatch } from 'redux';
-// import { ChangeAction, OnMount_Page_Action, OnUnmount_Page_Action } from '@/models/resourceCreatorBatchPage';
+import Finish from './Finish';
+import * as AHooks from 'ahooks';
+import { Dispatch } from 'redux';
+import { OnMount_Page_Action, OnUnmount_Page_Action } from '@/models/resourceCreatorBatchPage';
 // import { FServiceAPI, FUtil } from '@freelog/tools-lib';
 // import { RcFile } from 'antd/lib/upload/interface';
 // import { getFilesSha1Info } from '@/utils/service';
@@ -16,121 +16,121 @@
 // import fReadLocalFiles from '@/components/fReadLocalFiles';
 // import fObjectsSelectorDrawer from '@/components/fObjectsSelectorDrawer';
 // import { getTaskHandler } from '@/components/FResourceBatchUpload';
-// import Handle from '@/pages/resource/creatorBatch/Handle';
-//
-// interface CreatorBatchProps {
-//   dispatch: Dispatch;
-//   resourceCreatorBatchPage: ResourceCreatorBatchPageState;
-// }
-//
-// function CreatorBatch({ dispatch, resourceCreatorBatchPage }: CreatorBatchProps) {
-//
-//   const [$dataSource, set$dataSource, get$dataSource] = FUtil.Hook.useGetState<ResourceCreatorBatchPageState['resourceListInfo']>(resourceCreatorBatchPage.resourceListInfo);
-//
-//   React.useEffect(() => {
-//     set$dataSource(resourceCreatorBatchPage.resourceListInfo);
-//   }, [resourceCreatorBatchPage.resourceListInfo]);
-//
-//   AHooks.useMount(() => {
-//     dispatch<OnMount_Page_Action>({
-//       type: 'resourceCreatorBatchPage/onMount_Page',
-//     });
-//   });
-//
-//   AHooks.useUnmount(() => {
-//     dispatch<OnUnmount_Page_Action>({
-//       type: 'resourceCreatorBatchPage/onUnmount_Page',
-//     });
-//   });
-//
-//   async function storageSpaceGotoList(objIDs: string[]) {
-//
-//   }
-//
-//   async function onLocalUpload() {
-//     // console.log('onLocalUpload sdfujsdl;kfjlksdjflkjlkj');
-//     const { data: data_acceptResourceType }: {
-//       data: {
-//         formats: string[];
-//       }
-//     } = await FServiceAPI.Resource.getResourceTypeInfoByCode({
-//       code: resourceCreatorBatchPage.selectedResourceType?.value || '',
-//     });
-//     if (!data_acceptResourceType) {
-//       return;
-//     }
-//
-//     // set$accept();
-//     const files: RcFile[] | null = await fReadLocalFiles({
-//       accept: data_acceptResourceType.formats.join(','),
-//       multiple: true,
-//     });
-//
-//     if (!files) {
-//       return;
-//     }
-//     // console.log('*************************** 88888');
-//     dispatch<ChangeAction>({
-//       type: 'resourceCreatorBatchPage/change',
-//       payload: {
-//         showPage: 'resourceList',
-//         // resourceListInfo: resourceListInfo,
-//       },
-//     });
-//
-//     const handler = await getTaskHandler();
-//     // console.log(handler, 'handler sdifjlskdjflksdjlkfjlk');
-//
-//     handler.addTask(files);
-//
-//   }
-//
-//   async function onImportStorage() {
-//     const objIDs: string[] | null = await fObjectsSelectorDrawer({
-//       resourceTypeCode: resourceCreatorBatchPage.selectedResourceType?.value || '',
-//     });
-//     // console.log(objIDs, 'objIDs');
-//     if (!objIDs) {
-//       return;
-//     }
-//
-//     storageSpaceGotoList(objIDs);
-//   }
-//
-//   return (<>
-//
-//     {
-//       resourceCreatorBatchPage.showPage === 'resourceType' && (<ResourceType />)
-//     }
-//
-//     {
-//       resourceCreatorBatchPage.showPage === 'uploadFile' && (<Handle />)
-//     }
-//
-//     {/*{*/}
-//     {/*  resourceCreatorBatchPage.showPage === 'uploadFile' && (<UploadFile*/}
-//     {/*    onImportStorage={onImportStorage}*/}
-//     {/*    onLocalUpload={onLocalUpload}*/}
-//     {/*  />)*/}
-//     {/*}*/}
-//
-//     {/*{*/}
-//     {/*  resourceCreatorBatchPage.showPage === 'resourceList' && (<ResourceList*/}
-//     {/*    onImportStorage={onImportStorage}*/}
-//     {/*    onLocalUpload={onLocalUpload}*/}
-//     {/*  />)*/}
-//     {/*}*/}
-//
-//     {
-//       resourceCreatorBatchPage.showPage === 'finish' && (<Finish />)
-//     }
-//   </>);
-// }
-//
-// export default connect(({ resourceCreatorBatchPage }: ConnectState) => ({
-//   resourceCreatorBatchPage: resourceCreatorBatchPage,
-// }))(CreatorBatch);
-//
+import Handle from '@/pages/resource/creatorBatch/Handle';
+
+interface CreatorBatchProps {
+  dispatch: Dispatch;
+  resourceCreatorBatchPage: ResourceCreatorBatchPageState;
+}
+
+function CreatorBatch({ dispatch, resourceCreatorBatchPage }: CreatorBatchProps) {
+
+  // const [$dataSource, set$dataSource, get$dataSource] = FUtil.Hook.useGetState<ResourceCreatorBatchPageState['resourceListInfo']>(resourceCreatorBatchPage.resourceListInfo);
+
+  // React.useEffect(() => {
+  //   set$dataSource(resourceCreatorBatchPage.resourceListInfo);
+  // }, [resourceCreatorBatchPage.resourceListInfo]);
+
+  AHooks.useMount(() => {
+    dispatch<OnMount_Page_Action>({
+      type: 'resourceCreatorBatchPage/onMount_Page',
+    });
+  });
+
+  AHooks.useUnmount(() => {
+    dispatch<OnUnmount_Page_Action>({
+      type: 'resourceCreatorBatchPage/onUnmount_Page',
+    });
+  });
+
+  // async function storageSpaceGotoList(objIDs: string[]) {
+  //
+  // }
+
+  // async function onLocalUpload() {
+  //   // console.log('onLocalUpload sdfujsdl;kfjlksdjflkjlkj');
+  //   const { data: data_acceptResourceType }: {
+  //     data: {
+  //       formats: string[];
+  //     }
+  //   } = await FServiceAPI.Resource.getResourceTypeInfoByCode({
+  //     code: resourceCreatorBatchPage.selectedResourceType?.value || '',
+  //   });
+  //   if (!data_acceptResourceType) {
+  //     return;
+  //   }
+  //
+  //   // set$accept();
+  //   const files: RcFile[] | null = await fReadLocalFiles({
+  //     accept: data_acceptResourceType.formats.join(','),
+  //     multiple: true,
+  //   });
+  //
+  //   if (!files) {
+  //     return;
+  //   }
+  //   // console.log('*************************** 88888');
+  //   dispatch<ChangeAction>({
+  //     type: 'resourceCreatorBatchPage/change',
+  //     payload: {
+  //       showPage: 'resourceList',
+  //       // resourceListInfo: resourceListInfo,
+  //     },
+  //   });
+  //
+  //   const handler = await getTaskHandler();
+  //   // console.log(handler, 'handler sdifjlskdjflksdjlkfjlk');
+  //
+  //   handler.addTask(files);
+  //
+  // }
+
+  // async function onImportStorage() {
+  //   const objIDs: string[] | null = await fObjectsSelectorDrawer({
+  //     resourceTypeCode: resourceCreatorBatchPage.selectedResourceType?.value || '',
+  //   });
+  //   // console.log(objIDs, 'objIDs');
+  //   if (!objIDs) {
+  //     return;
+  //   }
+  //
+  //   storageSpaceGotoList(objIDs);
+  // }
+
+  return (<>
+
+    {
+      resourceCreatorBatchPage.showPage === 'resourceType' && (<ResourceType />)
+    }
+
+    {
+      resourceCreatorBatchPage.showPage === 'uploadFile' && (<Handle />)
+    }
+
+    {/*{*/}
+    {/*  resourceCreatorBatchPage.showPage === 'uploadFile' && (<UploadFile*/}
+    {/*    onImportStorage={onImportStorage}*/}
+    {/*    onLocalUpload={onLocalUpload}*/}
+    {/*  />)*/}
+    {/*}*/}
+
+    {/*{*/}
+    {/*  resourceCreatorBatchPage.showPage === 'resourceList' && (<ResourceList*/}
+    {/*    onImportStorage={onImportStorage}*/}
+    {/*    onLocalUpload={onLocalUpload}*/}
+    {/*  />)*/}
+    {/*}*/}
+
+    {
+      resourceCreatorBatchPage.showPage === 'finish' && (<Finish />)
+    }
+  </>);
+}
+
+export default connect(({ resourceCreatorBatchPage }: ConnectState) => ({
+  resourceCreatorBatchPage: resourceCreatorBatchPage,
+}))(CreatorBatch);
+
 // function getARightName(name: string) {
 //   const newName: string = name.replace(new RegExp(/\.[\w-]+$/), '')
 //     .substring(0, 50)
