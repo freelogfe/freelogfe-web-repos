83d937ad224d1dd7a5a6232bbe9f424ca666eb63
liu-kai-github <liukaiemailchina@gmail.com>
2024-01-22 17:00:14 +0800

    udpate

--- packages/www/src/pages/activity/$id/SpringFestival/index.tsx
+++ packages/www/src/pages/activity/$id/SpringFestival/index.tsx
@@ -47,6 +47,16 @@ function SpringFestival({ activityDetailsPage }: SpringFestivalProps) {
     <div className={styles.participation}>
       <div className={styles.h1}>参与方式</div>
 
+      <div className={styles.textContent} style={{ top: 108, left: 270 }}>
+        前往freelog网页端，发布带有“<strong>新春召集令，freelog创作激励计划启动！</strong>”标签的原创资源并成功签约至展示节点，完成一次节点或展品分享后即视为参与此次活动
+      </div>
+      <div className={styles.textContent} style={{ top: 290, left: 182 }}>
+        不限制内容形式，<strong>原创小说/文章、漫画、图片、音乐、播客、视频、游戏/主题/插件开发资源</strong>等均可发布
+      </div>
+      <div className={styles.textContent} style={{ top: 464, left: 330 }}>
+        由于平台尚在测试阶段，活动功能未全面开放。如您要参与活动，需点击下方报名入口获取内测资格。
+      </div>
+
       <Space size={25}>
         <a className={styles.button}>立即报名</a>
         <a className={styles.button}>查看操作教程</a>
