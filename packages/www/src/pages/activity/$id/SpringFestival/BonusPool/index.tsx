cf7cc31251d8f6a0f303915e8c8041a18d73f079
liu-kai-github <liukaiemailchina@gmail.com>
2024-01-24 16:34:17 +0800

    Update index.tsx

--- packages/www/src/pages/activity/$id/SpringFestival/BonusPool/index.tsx
+++ packages/www/src/pages/activity/$id/SpringFestival/BonusPool/index.tsx
@@ -4,12 +4,22 @@ import img_poolTitle from '@/assets/activity/SpringFestival/poolTitle@2x.png';
 import FEnergyBall from '@/components/FEnergyBall';
 import { Space } from 'antd';
 import FComponentsLib from '@freelog/components-lib';
+import { FUtil } from '@freelog/tools-lib';
+import moment, { Moment } from 'moment';
+import { connect } from 'dva';
+import { ActivityDetailsPageModelState, ConnectState } from '@/models/connect';
 
 interface BonusPoolProps {
-
+  activityDetailsPage: ActivityDetailsPageModelState;
 }
 
-function BonusPool({}: BonusPoolProps) {
+function BonusPool({ activityDetailsPage }: BonusPoolProps) {
+  const [$momentTime, set$momentTime, get$momentTime] = FUtil.Hook.useGetState<Moment>(moment());
+
+  const [$updateTime, set$updateTime, get$updateTime] = FUtil.Hook.useGetState<string>(activityDetailsPage.endTime ? moment().isBefore(activityDetailsPage.endTime)
+    ? (get$momentTime().subtract(12, 'hours').format('YYYY-MM-DD') + ' 12:00:00')
+    : activityDetailsPage.endTime.format('YYYY-MM-DD HH:mm:ss') : 'YYYY-MM-DD HH:mm:ss');
+
   return (<div className={styles.pool}>
     <img src={img_poolTitle} style={{ width: 636, opacity: .95 }} alt={''} />
     <FEnergyBall percent={50} />
@@ -32,10 +42,12 @@ function BonusPool({}: BonusPoolProps) {
     <div style={{ height: 60 }} />
     <Space size={20}>
       <FComponentsLib.FContentText type={'negative'} text={'每天12点更新'} />
-      <FComponentsLib.FContentText type={'negative'} text={'最近更新时间：2024/01/28  12:00:00'} />
+      <FComponentsLib.FContentText type={'negative'} text={'最近更新时间：' + $updateTime} />
     </Space>
     <div style={{ height: 60 }} />
   </div>);
 }
 
-export default BonusPool;
+export default connect(({ activityDetailsPage }: ConnectState) => ({
+  activityDetailsPage,
+}))(BonusPool);
