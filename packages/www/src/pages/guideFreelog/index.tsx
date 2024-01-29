d496e87ac8f07a404e2d9704cb9cbea36a5615a8
liu-kai-github <liukaiemailchina@gmail.com>
2024-01-29 11:27:24 +0800

    u的帕特

--- packages/www/src/pages/guideFreelog/index.tsx
+++ packages/www/src/pages/guideFreelog/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import styles from './index.less';
 import FComponentsLib from '@freelog/components-lib';
 import * as AHooks from 'ahooks';
+import moment from 'moment';
+import fCenterMessage from '@/components/fCenterMessage';
+import { FUtil } from '../../../../@freelog/tools-lib';
 
 interface GuideFreelogProps {
 
@@ -9,12 +12,7 @@ interface GuideFreelogProps {
 
 function GuideFreelog({}: GuideFreelogProps) {
 
-  const [cookie, setCookie] = AHooks.useCookieState('guideFreelog', {
-    defaultValue: 'true', // 默认值
-    domain: '.testfreelog.com',
-    path: '/', // cookie 的路径
-    expires: new Date('2099-12-31T23:59:59.000Z'),
-  });
+  const [cookie, setCookie] = AHooks.useCookieState('guideFreelog');
 
   return (<div className={styles.guide}>
     <FComponentsLib.FTitleText type={'h1'} text={'目前您在测试环境，现在去正式环境'} />
@@ -29,19 +27,25 @@ function GuideFreelog({}: GuideFreelogProps) {
       <FComponentsLib.FInput.FPassword
         // className={styles.FPassword}
         placeholder={'输入内部密码'}
-        onPressEnter={((e) => {
-          // console.log(e, 'asdfiosahjdlkfjslkdjflkjl');
-          // console.log(cookie, 'cookieisdjf;lksjdl;kfjsdl;kjflkj');
+        onPressEnter={((value: any) => {
+          if (value.target.value !== moment().format('YYYYMMDD').split('').reverse().join('')) {
+            fCenterMessage({ message: '密码错误' });
+            return;
+          }
           setCookie('false', {
-            value: 'false',
             domain: '.testfreelog.com',
             path: '/', // cookie 的路径
-            expires: new Date('2099-12-31T23:59:59.000Z'),
+            // expires: new Date('2099-12-31T23:59:59.000Z'),
+            expires: moment().add(7, 'days').toDate(),
+            // expires: Date.now() * 2,
+            secure: true,
           });
+          self.location.replace(FUtil.LinkTo.home());
         })}
       />
     </div>
   </div>);
 }
+
 
 export default GuideFreelog;
