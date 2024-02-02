449e0d573a79609ff5df39af424b2ebba1a93812
liu-kai-github <liukaiemailchina@gmail.com>
2024-02-02 14:35:19 +0800

    Update nodeManagerPage.ts

--- packages/console/src/models/nodeManagerPage.ts
+++ packages/console/src/models/nodeManagerPage.ts
@@ -1070,7 +1070,7 @@ const Model: NodeManagerModelType = {
             return {
               id: i.presentableId,
               cover: i.coverImages[0],
-              title: i.presentableTitle,
+              title: i.presentableTitle || i.presentableName,
               resourceName: i.presentableName,
               version: i.version,
               isOnline: i.onlineStatus === 1,
