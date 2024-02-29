72f3d35d72e86568fc2239cd674fbfd916d5aa55
liu-kai-github <liukaiemailchina@gmail.com>
2024-02-29 14:52:18 +0800

    update

--- packages/console/src/components/FUploadCover/FCropperModal/index.tsx
+++ packages/console/src/components/FUploadCover/FCropperModal/index.tsx
@@ -4,42 +4,68 @@ import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 import { Modal, Space } from 'antd';
 import FComponentsLib from '@freelog/components-lib';
-import { FI18n } from '@freelog/tools-lib';
+import { FI18n, FUtil } from '@freelog/tools-lib';
 
 interface FCropperModalProps {
   uploadRef: any;
   imgSrc: string;
 
-  onOk?(info: {
-    h: number;
-    w: number;
-    x: number;
-    y: number;
-    r: number;
-  }): void;
+  // onOk?(info: {
+  //   h: number;
+  //   w: number;
+  //   x: number;
+  //   y: number;
+  //   r: number;
+  // }): void;
 
   onCancel?(): void;
 }
 
-function FCropperModal({ uploadRef, imgSrc, onOk, onCancel }: FCropperModalProps) {
-  const [cropper, setCropper] = React.useState<any>();
+function FCropperModal({
+                         uploadRef,
+                         imgSrc,
+                         // onOk,
+                         onCancel,
+                       }: FCropperModalProps) {
+  const [$cropperInstance, set$cropperInstance, get$cropperInstance] = FUtil.Hook.useGetState<Cropper | null>(null);
+
+  const [$aspectRatioChecked, set$aspectRatioChecked, get$aspectRatioChecked] = FUtil.Hook.useGetState<'free' | 'original' | number>('free');
+
+  React.useEffect(() => {
+    const cropperInstance = get$cropperInstance();
+    if (!cropperInstance) {
+      return;
+    }
+    const aspectRatioChecked = get$aspectRatioChecked();
+    if (aspectRatioChecked === 'free') {
+      cropperInstance.enable();
+      cropperInstance.setAspectRatio(0);
+    } else if (aspectRatioChecked === 'original') {
+      cropperInstance.setAspectRatio(0);
+      cropperInstance.disable();
+    } else {
+      cropperInstance.enable();
+      cropperInstance.setAspectRatio(aspectRatioChecked);
+    }
+
+  }, [$aspectRatioChecked]);
 
   return (<Modal
     open={!!imgSrc}
-    width={950}
+    width={1000}
     title={<FComponentsLib.FTitleText text={'上传资源图片'} type='popup' />}
     destroyOnClose
     bodyStyle={{ padding: '20px 30px' }}
     onOk={() => {
-      const info = cropper.getData();
+      // const info = cropper.getData();
       // console.log(info, '##SDfsiodlk');
-      onOk && onOk({
-        h: info.height,
-        w: info.width,
-        x: info.x,
-        y: info.y,
-        r: info.rotate,
-      });
+      // onOk && onOk({
+      //   h: info.height,
+      //   w: info.width,
+      //   x: info.x,
+      //   y: info.y,
+      //   r: info.rotate,
+      // });
     }}
     onCancel={() => {
       onCancel && onCancel();
@@ -49,12 +75,18 @@ function FCropperModal({ uploadRef, imgSrc, onOk, onCancel }: FCropperModalProps
   >
     <div className={styles.content}>
       <div className={styles.contentLeft}>
-        <FComponentsLib.FTitleText text={'图片选框'} type='h3' />
+        <FComponentsLib.FTitleText
+          text={'图片选框'}
+          type='h4'
+        />
         <div style={{ height: 10 }} />
+        {/*{console.log($aspectRatioChecked, '$aspectRatioChecked sdifjsdlkfjlkdsjflkjlkj')}*/}
         <Cropper
-          style={{ width: 560, height: 420 }}
-          aspectRatio={4 / 3}
-          rotatable={true}
+          style={{ width: 512, height: 384 }}
+          // aspectRatio={($aspectRatioChecked === 'free' || $aspectRatioChecked === 'original')
+          //   ? 1
+          //   : $aspectRatioChecked}
+          // rotatable={true}
           preview={'.' + styles.imgPreview}
           src={imgSrc}
           viewMode={2}
@@ -65,9 +97,13 @@ function FCropperModal({ uploadRef, imgSrc, onOk, onCancel }: FCropperModalProps
           autoCropArea={1}
           checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
           onInitialized={(instance) => {
-            setCropper(instance);
+            // setCropper(instance);
+            set$cropperInstance(instance);
           }}
+          // autoCapitalize={}
           guides={true}
+          disabled={true}
+          width={100}
           // onChange={(c) => {
           //   console.log(c, 'c9803iojsodlkfsjdl');
           // }}
@@ -114,13 +150,172 @@ function FCropperModal({ uploadRef, imgSrc, onOk, onCancel }: FCropperModalProps
             }}
           >重新选择</FComponentsLib.FTextBtn>
         </Space>
+
+        <div style={{ height: 15 }} />
+        <FComponentsLib.FContentText
+          text={'资源封面是资源对外展示的窗口，清晰美观的封面更容易被打开；'}
+          type='additional2'
+        />
+        <div style={{ height: 5 }} />
+        <FComponentsLib.FContentText
+          text={'只支持JPG/PNG/GIF，GIF文件不能动画化，大小不超过5M，建议宽高不小于800px。'}
+          type='additional2'
+        />
       </div>
+      <div style={{ width: 5 }} />
+      <div className={styles.contentMiddle}>
+        <FComponentsLib.FTitleText
+          text={'尺寸选择'}
+          type='h4'
+        />
+        <div style={{ height: 10 }} />
+        <div className={styles.checkBox}>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 'free' ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked('free');
+            }}
+          >
+            <span>自由尺寸</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 'original' ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked('original');
+            }}
+          >
+            <span>原图尺寸</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 2 / 3 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(2 / 3);
+            }}
+          >
+            <div
+              className={styles.rect}
+              style={{
+                width: 10,
+                height: 15,
+              }}
+            />
+            <span>2 : 3</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 3 / 2 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(3 / 2);
+            }}
+          >
+            <div
+              className={styles.rect}
+              style={{
+                width: 15,
+                height: 10,
+              }}
+            />
+            <span>3 : 2</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 3 / 4 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(3 / 4);
+            }}
+          >
+            <div
+              className={styles.rect}
+              style={{
+                width: 9,
+                height: 12,
+              }}
+            />
+            <span>3 : 4</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 4 / 3 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(4 / 3);
+            }}
+          >
+            <div
+              className={styles.rect}
+              style={{
+                width: 12,
+                height: 9,
+              }}
+            />
+            <span>4 : 3</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 5 / 7 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(5 / 7);
+            }}>
+            <div
+              className={styles.rect}
+              style={{
+                width: 10,
+                height: 14,
+              }}
+            />
+            <span>5 : 7</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 7 / 5 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(7 / 5);
+            }}>
+            <div
+              className={styles.rect}
+              style={{
+                width: 14,
+                height: 10,
+              }}
+            />
+            <span>7 : 5</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 9 / 16 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(9 / 16);
+            }}>
+            <div
+              className={styles.rect}
+              style={{
+                width: 9,
+                height: 16,
+              }}
+            />
+            <span>9 : 16</span>
+          </div>
+          <div
+            className={[styles.checkBoxItem, $aspectRatioChecked === 16 / 9 ? styles.checked : ''].join(' ')}
+            onClick={() => {
+              set$aspectRatioChecked(16 / 9);
+            }}>
+            <div
+              className={styles.rect}
+              style={{
+                width: 16,
+                height: 9,
+              }}
+            />
+            <span>16 : 9</span>
+          </div>
+        </div>
+      </div>
+      <div style={{ width: 30 }} />
       <div className={styles.contentRight}>
-        <FComponentsLib.FTitleText text={'资源卡片预览'} type='h3' />
+        <FComponentsLib.FTitleText
+          text={'资源卡片预览'}
+          type='h4'
+        />
         <div style={{ height: 10 }} />
 
         <div className={styles.previewCard}>
-          <div className={styles.imgPreview} />
+          <div className={styles.imgPreviewBox}>
+            <div className={styles.imgPreview} />
+          </div>
           <div style={{ height: 10 }} />
           <FComponentsLib.FContentText text={'资源名称'} type='highlight' />
           <div style={{ height: 8 }} />
@@ -131,12 +326,6 @@ function FCropperModal({ uploadRef, imgSrc, onOk, onCancel }: FCropperModalProps
           <div style={{ height: 12 }} />
           <FComponentsLib.F_Contract_And_Policy_Labels data={[{ text: '授权策略', dot: '' }]} />
         </div>
-
-        <div style={{ height: 20 }} />
-        <FComponentsLib.FContentText text={'只支持JPG/PNG/GIF，GIF文件不能动画化，大小不超过5M，建议尺寸为800X600；'} type='additional2' />
-        <div style={{ height: 10 }} />
-        <FComponentsLib.FContentText text={'未上传封面时，使用系统默认封面。'} type='additional2' />
-
       </div>
     </div>
   </Modal>);
--- packages/console/src/components/FUploadCover/index.tsx
+++ packages/console/src/components/FUploadCover/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 import { RcFile } from 'antd/lib/upload/interface';
 import { Upload } from 'antd';
 import FCropperModal from './FCropperModal';
-import { FServiceAPI, FI18n } from '@freelog/tools-lib';
+import { FServiceAPI, FI18n, FUtil } from '@freelog/tools-lib';
 import fMessage from '@/components/fMessage';
 
 interface FUploadCoverProps {
@@ -26,63 +26,82 @@ const initStates: FUploadCoverStates = {
 
 function FUploadCover({ children, onUploadSuccess, onError }: FUploadCoverProps) {
   const ref = React.useRef<any>(null);
-  const [naturalFile, setNaturalFile] = React.useState<FUploadCoverStates['naturalFile']>(initStates['naturalFile']);
-  const [image, setImage] = React.useState<FUploadCoverStates['image']>(initStates['image']);
+  const [$naturalFile, set$naturalFile, get$naturalFile] = FUtil.Hook.useGetState<FUploadCoverStates['naturalFile']>(initStates['naturalFile']);
+  const [$image, set$image, get$image] = FUtil.Hook.useGetState<FUploadCoverStates['image']>(initStates['image']);
 
-  function beforeUpload(file: RcFile) {
-    if (file.type !== 'image/gif' && file.type !== 'image/png' && file.type !== 'image/jpeg') {
-      onError && onError(FI18n.i18nNext.t('limit_resource_image_format'));
-      return false;
-    }
+  // function beforeUpload(file: RcFile) {
+  //   if (file.type !== 'image/gif' && file.type !== 'image/png' && file.type !== 'image/jpeg') {
+  //     onError && onError(FI18n.i18nNext.t('limit_resource_image_format'));
+  //     return false;
+  //   }
+  //
+  //   if (file.size > 5 * 1024 * 1024) {
+  //     onError && onError(FI18n.i18nNext.t('limit_resource_image_size'));
+  //     return false;
+  //   }
+  //
+  //   // setNaturalFile(file);
+  //
+  //   const reader = new FileReader();
+  //   reader.onload = () => {
+  //     set$image(reader.result as any);
+  //   };
+  //   reader.readAsDataURL(file);
+  //   return false;
+  // }
 
-    if (file.size > 5 * 1024 * 1024) {
-      onError && onError(FI18n.i18nNext.t('limit_resource_image_size'));
-      return false;
-    }
-
-    setNaturalFile(file);
-
-    const reader = new FileReader();
-    reader.onload = () => {
-      setImage(reader.result as any);
-    };
-    reader.readAsDataURL(file);
-    return false;
-  }
-
-  async function upload(cropArea: { x: number, y: number, r: number, w: number, h: number }) {
-    if (!image || !naturalFile) {
-      return;
-    }
-    const { ret, errCode, data, msg } = await FServiceAPI.Storage.uploadImage({
-      file: naturalFile,
-    });
-
-    if (ret !== 0 || errCode !== 0) {
-      fMessage(msg, 'error');
-      return;
-    }
-
-    const img = new Image();
-    img.src = data.url;
-
-    img.onload = () => {
-      const hash: string = `#x=${cropArea.x}&y=${cropArea.y}&r=${cropArea.r}&w=${cropArea.w}&h=${cropArea.h}&width=${img.width}&height=${img.height}`;
-      const url: string = data.url + hash;
-      // console.log(url, 'url2222222');
-      onUploadSuccess && onUploadSuccess(url);
-
-      setNaturalFile(initStates['naturalFile']);
-      setImage(initStates['image']);
-    };
-  }
+  // async function upload(cropArea: { x: number, y: number, r: number, w: number, h: number }) {
+  //   if (!image || !naturalFile) {
+  //     return;
+  //   }
+  //   const { ret, errCode, data, msg } = await FServiceAPI.Storage.uploadImage({
+  //     file: naturalFile,
+  //   });
+  //
+  //   if (ret !== 0 || errCode !== 0) {
+  //     fMessage(msg, 'error');
+  //     return;
+  //   }
+  //
+  //   const img = new Image();
+  //   img.src = data.url;
+  //
+  //   img.onload = () => {
+  //     const hash: string = `#x=${cropArea.x}&y=${cropArea.y}&r=${cropArea.r}&w=${cropArea.w}&h=${cropArea.h}&width=${img.width}&height=${img.height}`;
+  //     const url: string = data.url + hash;
+  //     // console.log(url, 'url2222222');
+  //     onUploadSuccess && onUploadSuccess(url);
+  //
+  //     setNaturalFile(initStates['naturalFile']);
+  //     setImage(initStates['image']);
+  //   };
+  // }
 
   return (<div className={styles.styles}>
 
     <Upload
       // accept={'image/gif,image/png,.jpg'}
       accept={'.gif,.png,.jpg,.jpeg,.jpe'}
-      beforeUpload={beforeUpload}
+      beforeUpload={(file: RcFile) => {
+        if (file.type !== 'image/gif' && file.type !== 'image/png' && file.type !== 'image/jpeg') {
+          onError && onError(FI18n.i18nNext.t('limit_resource_image_format'));
+          return false;
+        }
+
+        if (file.size > 5 * 1024 * 1024) {
+          onError && onError(FI18n.i18nNext.t('limit_resource_image_size'));
+          return false;
+        }
+
+        // setNaturalFile(file);
+
+        const reader = new FileReader();
+        reader.onload = () => {
+          set$image(reader.result as any);
+        };
+        reader.readAsDataURL(file);
+        return false;
+      }}
       multiple={false}
       showUploadList={false}
     >
@@ -92,13 +111,13 @@ function FUploadCover({ children, onUploadSuccess, onError }: FUploadCoverProps)
     </Upload>
     <FCropperModal
       uploadRef={ref}
-      imgSrc={image}
-      onOk={(info) => {
-        upload(info);
-      }}
+      imgSrc={$image}
+      // onOk={(info) => {
+      //   upload(info);
+      // }}
       onCancel={() => {
-        setNaturalFile(initStates['naturalFile']);
-        setImage(initStates['image']);
+        // setNaturalFile(initStates['naturalFile']);
+        // setImage(initStates['image']);
       }}
     />
   </div>);
