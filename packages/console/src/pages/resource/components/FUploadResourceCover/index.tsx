f9e0ef706c9ee4bf0a941030abdd9b3ab552faa1
liu-kai-github <liukaiemailchina@gmail.com>
2024-02-29 16:24:08 +0800

    update

--- packages/console/src/pages/resource/components/FUploadResourceCover/index.tsx
+++ packages/console/src/pages/resource/components/FUploadResourceCover/index.tsx
@@ -1,51 +1,51 @@
-import * as React from 'react';
-import styles from './index.less';
-import FUploadCover from '@/components/FUploadCover';
-import fMessage from '@/components/fMessage';
-import FCoverImage from '@/components/FCoverImage';
-import { FI18n } from '@freelog/tools-lib';
-import FComponentsLib from '@freelog/components-lib';
-
-interface FUploadResourceCoverProps {
-  value?: string;
-  onChange?: (value: string) => void;
-}
-
-function FUploadResourceCover({ value, onChange }: FUploadResourceCoverProps) {
-
-  return (<div className={styles.cover}>
-    {
-      !value
-        ? (<FUploadCover
-          onUploadSuccess={(url) => onChange && onChange(url)}
-          onError={(err) => {
-            fMessage(err, 'error');
-          }}
-        >
-          <a className={styles.FUploadImageChildren}>
-            <FComponentsLib.FIcons.FCloudUpload />
-            <span>{FI18n.i18nNext.t('upload_image')}</span>
-          </a>
-        </FUploadCover>)
-        : (<div className={styles.FUploadImageChildren}>
-          <FCoverImage src={value} width={200} />
-        </div>)
-    }
-    <div className={styles.coverTip}>
-      {value && <FUploadCover
-        onUploadSuccess={(url) => onChange && onChange(url)}
-        onError={(err) => {
-          fMessage(err, 'error');
-        }}
-      >
-        <a className={styles.ReUpload}>{FI18n.i18nNext.t('replace_resource_image')}</a>
-      </FUploadCover>}
-      <div style={{ height: 15 }} />
-      <FComponentsLib.FContentText type='additional2' text={FI18n.i18nNext.t('rules_resource_image')} />
-      <div style={{ height: 5 }} />
-      <FComponentsLib.FContentText type='additional2' text={'未上传封面时，默认使用系统封面。'} />
-    </div>
-  </div>);
-}
-
-export default FUploadResourceCover;
+// import * as React from 'react';
+// import styles from './index.less';
+// import FUploadCover from '@/components/FUploadCover';
+// import fMessage from '@/components/fMessage';
+// import FCoverImage from '@/components/FCoverImage';
+// import { FI18n } from '@freelog/tools-lib';
+// import FComponentsLib from '@freelog/components-lib';
+//
+// interface FUploadResourceCoverProps {
+//   value?: string;
+//   onChange?: (value: string) => void;
+// }
+//
+// function FUploadResourceCover({ value, onChange }: FUploadResourceCoverProps) {
+//
+//   return (<div className={styles.cover}>
+//     {
+//       !value
+//         ? (<FUploadCover
+//           onUploadSuccess={(url) => onChange && onChange(url)}
+//           onError={(err) => {
+//             fMessage(err, 'error');
+//           }}
+//         >
+//           <a className={styles.FUploadImageChildren}>
+//             <FComponentsLib.FIcons.FCloudUpload />
+//             <span>{FI18n.i18nNext.t('upload_image')}</span>
+//           </a>
+//         </FUploadCover>)
+//         : (<div className={styles.FUploadImageChildren}>
+//           <FCoverImage src={value} width={200} />
+//         </div>)
+//     }
+//     <div className={styles.coverTip}>
+//       {value && <FUploadCover
+//         onUploadSuccess={(url) => onChange && onChange(url)}
+//         onError={(err) => {
+//           fMessage(err, 'error');
+//         }}
+//       >
+//         <a className={styles.ReUpload}>{FI18n.i18nNext.t('replace_resource_image')}</a>
+//       </FUploadCover>}
+//       <div style={{ height: 15 }} />
+//       <FComponentsLib.FContentText type='additional2' text={FI18n.i18nNext.t('rules_resource_image')} />
+//       <div style={{ height: 5 }} />
+//       <FComponentsLib.FContentText type='additional2' text={'未上传封面时，默认使用系统封面。'} />
+//     </div>
+//   </div>);
+// }
+//
+// export default FUploadResourceCover;
