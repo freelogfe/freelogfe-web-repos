a10919aaeb4ae3de1fdd283f10aa3846f46c6ae3
liu-kai-github <liukaiemailchina@gmail.com>
2024-03-20 10:43:07 +0800

    Update index.tsx

--- packages/console/src/components/fResourcePropertyEditor/index.tsx
+++ packages/console/src/components/fResourcePropertyEditor/index.tsx
@@ -31,10 +31,14 @@ function fResourcePropertyEditor({
                                    valueAcceptNull = false,
                                  }: fResourcePropertyEditorProps): Promise<ReturnData> {
   return new Promise<ReturnData>((resolve) => {
-    const drawerRoot: HTMLDivElement | null = self.document.getElementById('drawer-root') as HTMLDivElement;
-    const rootDiv: HTMLDivElement = self.document.createElement('div');
-    drawerRoot.appendChild(rootDiv);
-    const root = ReactDOM.createRoot(rootDiv);
+    // const drawerRoot: HTMLDivElement | null = self.document.getElementById('drawer-root') as HTMLDivElement;
+    // const rootDiv: HTMLDivElement = self.document.createElement('div');
+    // drawerRoot.appendChild(rootDiv);
+    // const root = ReactDOM.createRoot(rootDiv);
+    const divRoot = self.document.body;
+    const div = self.document.createElement('div') as HTMLDivElement;
+    divRoot.appendChild(div);
+    const root = ReactDOM.createRoot(div);
     return root.render(<FResourcePropertyEditorDrawer
       defaultData={defaultData || null}
       disabledKeys={disabledKeys}
