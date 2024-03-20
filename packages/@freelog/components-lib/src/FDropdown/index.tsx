a22050621e4e52d9f0480da1c4924716c69d8067
liu-kai-github <liukaiemailchina@gmail.com>
2024-03-20 15:28:06 +0800

    Update index.tsx

--- packages/@freelog/components-lib/src/FDropdown/index.tsx
+++ packages/@freelog/components-lib/src/FDropdown/index.tsx
@@ -4,15 +4,17 @@ import {Dropdown} from 'antd';
 import {DropDownProps} from "antd/lib/dropdown";
 
 interface FDropdownProps extends DropDownProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 function FDropdown({children, overlay, overlayClassName, ...props}: FDropdownProps) {
-  return (<Dropdown
-    {...props}
-    overlay={overlay}
-    overlayClassName={styles.dropdown + ' ' + overlayClassName}
-  ><div className={styles.children}>{children}</div></Dropdown>);
+    return (<Dropdown
+        {...props}
+        overlay={overlay}
+        overlayClassName={styles.dropdown + ' ' + overlayClassName}
+    >
+        <div className={styles.children}>{children}</div>
+    </Dropdown>);
 }
 
 export default FDropdown;
