80f949b01a10a20a4aea5d861d0db2526fbe85ca
liu-kai-github <liukaiemailchina@gmail.com>
2024-03-26 10:44:58 +0800

    Update resourceVersionCreatorPage.ts

--- packages/console/src/models/resourceVersionCreatorPage.ts
+++ packages/console/src/models/resourceVersionCreatorPage.ts
@@ -1233,7 +1233,9 @@ const Model: ResourceVersionCreatorModelType = {
                 };
               }),
               customProperties: draftData.customProperties,
-              customConfigurations: draftData.customConfigurations,
+              customConfigurations: resourceVersionCreatorPage.resourceTypeConfig.isSupportOptionalConfig
+                ? draftData.customConfigurations
+                : [],
             },
           });
         }
